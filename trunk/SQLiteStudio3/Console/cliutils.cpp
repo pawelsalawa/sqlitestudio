274783e886cb8d85fe9987e12a3a660d3480ca68
boogie <boogie@38d8252a-f1d7-4ff0-a8a6-9bdb9c17b6a5>
2014-02-27 18:16:50 +0000

    Implemented cols&rows for terminal under unix.

--- trunk/SQLiteStudio3/Console/cliutils.cpp
+++ trunk/SQLiteStudio3/Console/cliutils.cpp
@@ -1,23 +1,35 @@
 #include "cliutils.h"
 #include <QtGlobal>
-#ifdef Q_OS_WIN32
+
+#if defined(Q_OS_WIN32)
 #include <windows.h>
+#elif defined(Q_OS_UNIX)
+#include <sys/ioctl.h>
+#include <unistd.h>
 #endif
 
 int getCliColumns()
 {
-#ifdef Q_OS_WIN32
+#if defined(Q_OS_WIN32)
     CONSOLE_SCREEN_BUFFER_INFO data;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &data);
     return data.dwSize.X;
+#elif defined(Q_OS_UNIX)
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    return w.ws_col;
 #endif
 }
 
 int getCliRows()
 {
-#ifdef Q_OS_WIN32
+#if defined(Q_OS_WIN32)
     CONSOLE_SCREEN_BUFFER_INFO data;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &data);
     return data.dwSize.Y;
+#elif defined(Q_OS_UNIX)
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    return w.ws_row;
 #endif
 }
