66a47f0a55f6a6494eb7d4ffd7da2e3caf08face
boogie <pawelsalawa@gmail.com>
2014-03-19 20:00:00 +0000

    Removed deprecated DbSqlite3 plugin

--- DbSqlite3/dbsqlite3instance.cpp
+++
@@ -1,6 +0,0 @@
-#include "dbsqlite3instance.h"
-
-DbSqlite3Instance::DbSqlite3Instance(const QString& name, const QString& path, const QHash<QString, QVariant>& connOptions, const QString &driverName, const QString &type)
-    : DbQt3<Sqlite3>(name, path, connOptions, driverName, type)
-{
-}
