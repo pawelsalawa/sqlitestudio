7bc115a9d4b30199f8be52332794b53dbe67a0a1
boogie <boogie@38d8252a-f1d7-4ff0-a8a6-9bdb9c17b6a5>
2014-08-27 21:01:23 +0000

    Compilation fix.

--- trunk/SQLiteStudio3/coreSQLiteStudio/services/impl/pluginmanagerimpl.cpp
+++ trunk/SQLiteStudio3/coreSQLiteStudio/services/impl/pluginmanagerimpl.cpp
@@ -27,11 +27,11 @@ void PluginManagerImpl::init()
         pluginDirs += envDirs.split(PATH_LIST_SEPARATOR);
 
 #ifdef PLUGINS_DIR
-    pluginDirs += PLUGINS_DIR;
+    pluginDirs += STRINGIFY(PLUGINS_DIR);
 #endif
 
 #ifdef SYS_PLUGINS_DIR
-    pluginDirs += SYS_PLUGINS_DIR;
+    pluginDirs += STRINGIFY(SYS_PLUGINS_DIR);
 #endif
 
     scanPlugins();
