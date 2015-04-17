9768c0907de66fbd1e6c6eb49eb7c42b3c78d5e3
boogie <pawelsalawa@gmail.com>
2015-04-17 20:39:55 +0000

    CsvSerializer enhanced to support multi-char separators.

--- SQLiteStudio3/coreSQLiteStudio/csvformat.cpp
+++ SQLiteStudio3/coreSQLiteStudio/csvformat.cpp
@@ -11,3 +11,8 @@ CsvFormat::CsvFormat(const QString& columnSeparator, const QString& rowSeparator
     columnSeparator(columnSeparator), rowSeparator(rowSeparator)
 {
 }
+
+CsvFormat::CsvFormat(const QString& columnSeparator, const QString& rowSeparator, bool strictRowSeparator, bool strictColumnSeparator) :
+    columnSeparator(columnSeparator), rowSeparator(rowSeparator), strictColumnSeparator(strictColumnSeparator), strictRowSeparator(strictRowSeparator)
+{
+}
