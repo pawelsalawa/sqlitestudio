f9e25b7408bf10ed8429d20ac26d107648b22b4c
boogie <boogie@38d8252a-f1d7-4ff0-a8a6-9bdb9c17b6a5>
2015-05-05 21:58:38 +0000

    Merged linux build script from 3.0.

---
+++ trunk/scripts/linux_build_portable.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+if [ "$1" == "" ]; then
+    QMAKE=`which qmake`
+    if [ $QMAKE == "" ]; then
+	echo "Cannot find qmake"
+    else
+	read -p "Is this correct qmake (y/N) $QMAKE : " yn
+	case $yn in
+	    [Yy]* ) break;;
+	    * ) echo "Please pass path to correct qmake as argument to this script."; exit;;
+	esac
+    fi
+else
+    QMAKE=$1
+fi
+
+cdir=`pwd`
+cpu_cores=`grep -c ^processor /proc/cpuinfo`
+absolute_path=`realpath $0`
+this_dir=`dirname $absolute_path`
+parent_dir=`dirname $this_dir`
+
+read -p "Number of CPU cores to use for compiling (hit enter to use $cpu_cores): " new_cpu_cores
+case $new_cpu_cores in
+    "" ) break;;
+    * ) cpu_cores=$new_cpu_cores; break;;
+esac
+
+if [ -d $parent_dir/output ]; then
+	read -p "Directory $parent_dir/output already exists. The script will delete and recreate it. Is that okay? (y/N) : " yn
+	case $yn in
+	    [Yy]* ) break;;
+	    * ) echo "Aborted."; exit;;
+	esac
+fi
+
+cd $parent_dir
+mkdir output output/build output/build/Plugins
+
+cd output/build
+$QMAKE CONFIG+=portable ../../SQLiteStudio3
+make -j $cpu_cores
+
+cd Plugins
+$QMAKE CONFIG+=portable ../../SQLiteStudio3
+make -j $cpu_cores
+
+cd $cdir
