c0529f3ed0f4a6abcb32b341b9d36ed15ab8a4b0
boogie <boogie@38d8252a-f1d7-4ff0-a8a6-9bdb9c17b6a5>
2015-05-08 20:02:38 +0000

    .desc command in CLI initially implemented.

--- trunk/SQLiteStudio3/sqlitestudiocli/commands/clicommanddesc.h
+++ trunk/SQLiteStudio3/sqlitestudiocli/commands/clicommanddesc.h
@@ -3,6 +3,9 @@
 
 #include "clicommand.h"
 
+class SqliteCreateTable;
+class SqliteCreateVirtualTable;
+
 class CliCommandDesc : public CliCommand
 {
         Q_OBJECT
@@ -13,6 +16,11 @@ class CliCommandDesc : public CliCommand
         QString shortHelp() const;
         QString fullHelp() const;
         void defineSyntax();
+
+    private:
+        void printTable(SqliteCreateTable* table);
+        void printVirtualTable(SqliteCreateVirtualTable* table);
+        void printHorizontalLine(int lgt3rd);
 };
 
 #endif // CLICOMMANDDESC_H
