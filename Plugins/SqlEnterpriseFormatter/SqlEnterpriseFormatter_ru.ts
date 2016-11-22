7a8ae226f5506a85a59a68d07c59ccfa118e3040
boogie <pawelsalawa@gmail.com>
2016-11-22 21:47:04 +0000

    Translation synchronization (Tue Nov 22 21:47:03 GMT 2016)

--- Plugins/SqlEnterpriseFormatter/SqlEnterpriseFormatter_ru.ts
+++ Plugins/SqlEnterpriseFormatter/SqlEnterpriseFormatter_ru.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU">
+<TS version="2.0" language="ru_RU">
 <context>
     <name>QObject</name>
     <message>
@@ -147,7 +147,7 @@
     <message>
         <location filename="sqlenterpriseformatter.ui" line="452"/>
         <source>No space between SQL function name and opening parenthesis</source>
-        <translation>Не ставить пробел между именем функции SQL и открывающкй скобкой</translation>
+        <translation>Не ставить пробел между именем функции SQL и открывающей скобкой</translation>
     </message>
     <message>
         <location filename="sqlenterpriseformatter.ui" line="462"/>
