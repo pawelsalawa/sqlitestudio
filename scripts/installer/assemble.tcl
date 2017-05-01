af128def68f5e5f8c484c65a14ff5fe246dc42ba
boogie <pawelsalawa@gmail.com>
2017-05-01 23:04:20 +0000

    Installer adopted for Linux

--- scripts/installer/assemble.tcl
+++ scripts/installer/assemble.tcl
@@ -62,10 +62,41 @@ switch $::OS {
 	}
 	default {
 		set mainPkgFiles {
+			lib/libcoreSQLiteStudio.so
+			lib/libguiSQLiteStudio.so
+			lib/libsqlite3.so
+			sqlitestudio
+			sqlitestudiocli
 		}
 		set qtPkgFiles {
+			iconengines/libqsvgicon.so
+			imageformats/libqgif.so
+			imageformats/libqicns.so
+			imageformats/libqico.so
+			imageformats/ibqjpeg.so
+			imageformats/libqsvg.so
+			imageformats/libqtga.so
+			imageformats/libqtiff.so
+			platforms/libqxcb.so
+			printsupport/libcupsprintersupport.so
+			lib/libQt5Concurrent.so
+			lib/libQt5Core.so
+			lib/libQt5DBus.so
+			lib/libQt5Gui.so
+			lib/libQt5Network.so
+			lib/libQt5PrintSupport.so
+			lib/libQt5Script.so
+			lib/libQt5Svg.so
+			lib/libQt5Widgets.so
+			lib/libQt5Xml.so
+			lib/libicudata.so
+			lib/libicui18n.so
+			lib/libicuuc.so.56
 		}
 		array set pluginDeps {
+			DbSqlite2 {
+				lib/libsqlite.so
+			}
 		}
 	}
 }
@@ -74,34 +105,6 @@ proc defineGlobalVars {} {
 	lassign $::argv ::targetDir
 	set ::portableDir [file normalize ../../output/portable]
 	set ::releaseDate [clock format [clock seconds] -format "%Y-%m-%d"]
-
-	switch $::OS {
-		"macosx" {
-			set ::finalExecutable "SQLiteStudio.app"
-			set ::wizardStyle "Mac"
-			set ::initialTargetDir "/Applications/SQLiteStudio.app"
-			set ::libExt "dylib"
-			set ::updateArch "macosx"
-			set ::startApp "SQLiteStudio.app"
-		}
-		"win32" {
-			set ::finalExecutable "SQLiteStudio.exe"
-			set ::wizardStyle "Modern"
-			set ::initialTargetDir "C:\\Program Files\\SQLiteStudio"
-			set ::libExt "dll"
-			set ::updateArch "windows"
-			set ::startApp "SQLiteStudio.exe"
-			set qtCoreFile "$::portableDir/SQLiteStudio/Qt5Core.dll"
-		}
-		default {
-			set ::finalExecutable "sqlitestudio"
-			set ::wizardStyle "Modern"
-			set ::initialTargetDir "/opt/SQLiteStudio"
-			set ::updateArch "linux"
-			set ::startApp "SQLiteStudio"
-			set ::libExt "so"
-		}
-	}
 	
 	# Version
 	set verFile "../../SQLiteStudio3/coreSQLiteStudio/sqlitestudio.cpp"
@@ -112,6 +115,38 @@ proc defineGlobalVars {} {
 	set ::sqliteStudioVersion [toVersion $ver]
 	set data [read $fd]
 	close $fd
+
+	switch $::OS {
+		"macosx" {
+			set ::finalExecutable "SQLiteStudio.app"
+			set ::wizardStyle "Mac"
+			set ::initialTargetDir "/Applications/SQLiteStudio.app"
+			set ::libExt "dylib"
+			set ::updateArch "macosx"
+			set ::libPref ""
+			set ::output [file normalize $::targetDir/InstallSQLiteStudio-${::sqliteStudioVersion}]
+		}
+		"win32" {
+			set ::finalExecutable "SQLiteStudio.exe"
+			set ::wizardStyle "Modern"
+			set ::initialTargetDir "C:\\Program Files\\SQLiteStudio"
+			set ::libExt "dll"
+			set ::updateArch "windows"
+			set ::libPref ""
+			set qtCoreFile "$::portableDir/SQLiteStudio/Qt5Core.dll"
+			set ::output [file normalize $::targetDir/InstallSQLiteStudio-${::sqliteStudioVersion}.exe]
+		}
+		default {
+			set ::finalExecutable "sqlitestudio"
+			set ::wizardStyle "Modern"
+			set ::initialTargetDir "/opt/SQLiteStudio"
+			set ::updateArch "linux"
+			set ::libExt "so"
+			set ::libPref "lib"
+			set qtCoreFile "$::portableDir/SQLiteStudio/lib/libQt5Core.so"
+			set ::output [file normalize $::targetDir/InstallSQLiteStudio-${::sqliteStudioVersion}]
+		}
+	}
 	
 	# Qt version
 	set fd [open $qtCoreFile r]
@@ -122,8 +157,7 @@ proc defineGlobalVars {} {
 	######################################################################################### VER +1
 	#set ::qtVer 6.0.0
 
-	# Output binary
-	set ::output [file normalize $::targetDir/InstallSQLiteStudio-${::sqliteStudioVersion}.exe]
+	set ::startApp $::finalExecutable
 
 	# Repository
 	set ::repoDir [file normalize $::targetDir/REPO]
@@ -159,13 +193,33 @@ proc archiveContentsOf {dir intoFile} {
 	cd $cdir
 }
 
+proc copyFileWithLinks {fromDir toDir file} {
+	file mkdir [file dirname $toDir/$file]
+	switch -- $::OS {
+		"macosx" {
+			file copy -force $fromDir/$file $toDir/$file
+		}
+		"win32" {
+			file copy -force $fromDir/$file $toDir/$file
+		}
+		default {
+			set fd [file dirname $fromDir/$file]
+			set td [file dirname $toDir/$file]
+			set fileOnly [lindex [file split $file] end]
+			foreach f [glob -nocomplain -tails -directory $fd "${fileOnly}*"] {
+				file copy -force $fd/$f $td/$f
+			}
+		}
+	}
+}
+
 proc copyMainPkg {targetDir} {
 	set pkgDir $targetDir/packages/pl.com.salsoft.sqlitestudio
 	
 	puts "Copying core app files."
 	file mkdir $pkgDir $pkgDir/meta $pkgDir/data
 	foreach f $::mainPkgFiles {
-		file copy -force $::portableDir/SQLiteStudio/$f $pkgDir/data/
+		copyFileWithLinks $::portableDir/SQLiteStudio $pkgDir/data $f
 	}
 	file copy ../../SQLiteStudio3/sqlitestudio/package.xml $pkgDir/meta/
 
@@ -181,7 +235,7 @@ proc copyQtPkg {targetDir} {
 	puts "Copying Qt."
 	file mkdir $pkgDir $pkgDir/meta $pkgDir/data
 	foreach f $::qtPkgFiles {
-		file copy -force $::portableDir/SQLiteStudio/$f $pkgDir/data/
+		copyFileWithLinks $::portableDir/SQLiteStudio $pkgDir/data $f
 	}
 	file copy ../../SQLiteStudio3/qt_package.xml $pkgDir/meta/package.xml
 
@@ -220,12 +274,13 @@ proc readPluginVersion {path} {
 proc collectPlugins {} {
 	set mask "*.$::libExt"
 	set toRemove [expr {[string length $mask] - 1}]
+	set prefToRemove [string length $::libPref]
 
 	set files [glob -tails -directory ../../output/portable/SQLiteStudio/plugins $mask]
 	set ::plugins [list]
 	foreach f $files {
-		set plugin [string range $f 0 end-$toRemove]
-		set ver [readPluginVersion ../../Plugins/$plugin/${plugin}.json]
+		set plugin [string range $f $prefToRemove end-$toRemove]
+		set ver [readPluginVersion ../../Plugins/$plugin/[string tolower $plugin].json]
 		######################################################################################### VER +1
 		#set ver 9.9.9
 		lappend ::plugins [dict create name $plugin version $ver]
@@ -254,10 +309,10 @@ proc copyPluginPkg {pluginDict targetDir} {
 	
 	puts "Copying plugin: $name ($ver)"
 	file mkdir $pkgDir $pkgDir/meta $pkgDir/data $pkgDir/data/plugins
-	file copy -force $::portableDir/SQLiteStudio/plugins/${name}.$::libExt $pkgDir/data/plugins/
+	file copy -force $::portableDir/SQLiteStudio/plugins/$::libPref${name}.$::libExt $pkgDir/data/plugins/
 	if {[info exists ::pluginDeps($name)]} {
 		foreach f $::pluginDeps($name) {
-			file copy -force $::portableDir/SQLiteStudio/$f $pkgDir/data/
+			copyFileWithLinks $::portableDir/SQLiteStudio $pkgDir/data $f
 		}
 	}
 	file copy $packageXml $pkgDir/meta/
