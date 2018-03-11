ba2cf48976e90bedfb80f97f03c7a548f365d5e5
Paweł Salawa <pawelsalawa@gmail.com>
2018-03-11 14:04:45 +0100

    #3293 Fixed deault shortcut for code assistant under MacOSX from Cmd-Space to Ctrl-Space, so it does not conflict with Spotlight

--- SQLiteStudio3/guiSQLiteStudio/sqleditor.h
+++ SQLiteStudio3/guiSQLiteStudio/sqleditor.h
@@ -19,6 +19,12 @@ class SqlEditor;
 class SearchTextDialog;
 class SearchTextLocator;
 
+#ifdef Q_OS_OSX
+#  define COMPLETE_REQ_KEY Qt::META
+#else
+#  define COMPLETE_REQ_KEY Qt::CTRL
+#endif
+
 CFG_KEY_LIST(SqlEditor, QObject::tr("SQL editor input field"),
     CFG_KEY_ENTRY(CUT,             QKeySequence::Cut,                 QObject::tr("Cut selected text"))
     CFG_KEY_ENTRY(COPY,            QKeySequence::Copy,                QObject::tr("Copy selected text"))
@@ -34,7 +40,7 @@ CFG_KEY_LIST(SqlEditor, QObject::tr("SQL editor input field"),
     CFG_KEY_ENTRY(FIND_PREV,       QKeySequence::FindPrevious,        QObject::tr("Find previous"))
     CFG_KEY_ENTRY(REPLACE,         QKeySequence::Replace,             QObject::tr("Replace in text"))
     CFG_KEY_ENTRY(DELETE_LINE,     Qt::CTRL + Qt::Key_D,              QObject::tr("Delete current line"))
-    CFG_KEY_ENTRY(COMPLETE,        Qt::CTRL + Qt::Key_Space,          QObject::tr("Request code assistant"))
+    CFG_KEY_ENTRY(COMPLETE,        COMPLETE_REQ_KEY + Qt::Key_Space,          QObject::tr("Request code assistant"))
     CFG_KEY_ENTRY(FORMAT_SQL,      Qt::CTRL + Qt::Key_T,              QObject::tr("Format contents"))
     CFG_KEY_ENTRY(MOVE_BLOCK_DOWN, Qt::ALT + Qt::Key_Down,            QObject::tr("Move selected block of text one line down"))
     CFG_KEY_ENTRY(MOVE_BLOCK_UP,   Qt::ALT + Qt::Key_Up,              QObject::tr("Move selected block of text one line up"))
