79ac70ff3b9d3a1b80b741c27b65d8cbabaa5639
Pawel Salawa <pawelsalawa@gmail.com>
2018-07-19 01:10:54 +0200

    Updated version upgrade script for SQLCipher

--- Plugins/DbSqliteCipher/add_prefix.tcl
+++ Plugins/DbSqliteCipher/add_prefix.tcl
@@ -12,5 +12,13 @@ proc process {f} {
   close $fd
 }
 
+if {[file exists sqlite3.c]} {
+    file rename -force sqlite3.c sqlcipher.c
+}
+
+if {[file exists sqlite3.h]} {
+    file rename -force sqlite3.h sqlcipher.h
+}
+
 process sqlcipher.c
 process sqlcipher.h
