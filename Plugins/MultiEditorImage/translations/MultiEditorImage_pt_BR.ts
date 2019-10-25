928340a75c2fe5ef8bc38dce648c0c30f9d102e4
Paweł Salawa <pawelsalawa@gmail.com>
2019-10-25 14:19:12 +0200

    New translations MultiEditorImage.ts (Portuguese, Brazilian)

--- Plugins/MultiEditorImage/translations/MultiEditorImage_pt_BR.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_pt_BR.ts
@@ -1,81 +1,80 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt_BR">
-<context>
+<TS version="2.1" language="pt-BR" sourcelanguage="en">
+  <context>
     <name>MultiEditorImage</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="26"/>
-        <source>Load from file</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="26"/>
+      <source>Load from file</source>
+      <translation type="unfinished">Load from file</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="27"/>
-        <source>Store in a file</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="27"/>
+      <source>Store in a file</source>
+      <translation type="unfinished">Store in a file</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="28"/>
-        <source>Zoom in by 25%</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="28"/>
+      <source>Zoom in by 25%</source>
+      <translation type="unfinished">Zoom in by 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="29"/>
-        <source>Zoom out by 25%</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="29"/>
+      <source>Zoom out by 25%</source>
+      <translation type="unfinished">Zoom out by 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="30"/>
-        <source>Reset zoom</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="30"/>
+      <source>Reset zoom</source>
+      <translation type="unfinished">Reset zoom</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="100"/>
-        <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="100"/>
+      <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
+      <translation type="unfinished">Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="101"/>
-        <source>Open image</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="101"/>
+      <source>Open image</source>
+      <translation type="unfinished">Open image</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="110"/>
-        <source>Could not open file %1 for reading.</source>
-        <oldsource>Could not nopen file %1 for reading.</oldsource>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="110"/>
+      <source>Could not open file %1 for reading.</source>
+      <translation type="unfinished">Could not open file %1 for reading.</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="130"/>
-        <source>All files (*)</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="130"/>
+      <source>All files (*)</source>
+      <translation type="unfinished">All files (*)</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="132"/>
-        <source>Save image</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="132"/>
+      <source>Save image</source>
+      <translation type="unfinished">Save image</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="143"/>
-        <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="143"/>
+      <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
+      <translation type="unfinished">Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="154"/>
-        <source>Could not nopen file %1 for writting.</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="154"/>
+      <source>Could not nopen file %1 for writting.</source>
+      <translation type="unfinished">Could not nopen file %1 for writting.</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="159"/>
-        <source>Could not write image into the file %1</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="159"/>
+      <source>Could not write image into the file %1</source>
+      <translation type="unfinished">Could not write image into the file %1</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="250"/>
-        <source>Image</source>
-        <translation type="unfinished"></translation>
+      <location filename="../multieditorimage.cpp" line="250"/>
+      <source>Image</source>
+      <translation type="unfinished">Image</translation>
     </message>
-</context>
+  </context>
 </TS>
