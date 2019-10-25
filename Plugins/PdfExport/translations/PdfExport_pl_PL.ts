ff7f85b275d5073f2c1b79383789fbf40475b39d
Paweł Salawa <pawelsalawa@gmail.com>
2019-10-25 14:56:24 +0200

    New translations PdfExport.ts (Polish)

--- Plugins/PdfExport/translations/PdfExport_pl_PL.ts
+++ Plugins/PdfExport/translations/PdfExport_pl_PL.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../pdfexport.cpp" line="28"/>
       <source>SQLiteStudio v%1</source>
-      <translation type="unfinished">SQLiteStudio v%1</translation>
+      <translation>SQLiteStudio v%1</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="61"/>
@@ -205,7 +205,7 @@
       <location filename="../pdfexport.ui" line="132"/>
       <location filename="../pdfexport.ui" line="156"/>
       <source>mm</source>
-      <translation type="unfinished">mm</translation>
+      <translation>mm</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="139"/>
