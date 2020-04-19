5820f9e869abb3bc2d2771d4b251f5dccf43441f
Paweł Salawa <pawelsalawa@gmail.com>
2020-04-19 19:33:45 +0200

    New translations PdfExport.ts (Turkish)

--- Plugins/PdfExport/translations/PdfExport_tr_TR.ts
+++ Plugins/PdfExport/translations/PdfExport_tr_TR.ts
@@ -44,126 +44,126 @@
     <message>
       <location filename="../pdfexport.cpp" line="126"/>
       <source>Global table constraints</source>
-      <translation type="unfinished">Global table constraints</translation>
+      <translation>Global tablo constraints</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="215"/>
       <source>Exported database: %1</source>
-      <translation type="unfinished">Exported database: %1</translation>
+      <translation>Çıkartılan veritabanı: %1</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="223"/>
       <source>Index: %1</source>
-      <translation type="unfinished">Index: %1</translation>
+      <translation>Indeks: %1</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="225"/>
       <source>Property</source>
       <comment>index header</comment>
-      <translation type="unfinished">Property</translation>
+      <translation>Özellik</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="225"/>
       <source>Value</source>
       <comment>index header</comment>
-      <translation type="unfinished">Value</translation>
+      <translation>Değer</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="228"/>
       <source>Indexed table</source>
-      <translation type="unfinished">Indexed table</translation>
+      <translation>İndekslenen tablo</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="229"/>
       <source>Unique index</source>
-      <translation type="unfinished">Unique index</translation>
+      <translation>Unique index</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="229"/>
       <source>Yes</source>
-      <translation type="unfinished">Yes</translation>
+      <translation>Evet</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="229"/>
       <source>No</source>
-      <translation type="unfinished">No</translation>
+      <translation>Hayır</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="231"/>
       <source>Collation</source>
-      <translation type="unfinished">Collation</translation>
+      <translation>Collation</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="231"/>
       <source>Sort order</source>
-      <translation type="unfinished">Sort order</translation>
+      <translation>Sıralama düzeni</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="247"/>
       <source>Partial index condition</source>
-      <translation type="unfinished">Partial index condition</translation>
+      <translation>Partial indeks koşulu</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="261"/>
       <source>Trigger: %1</source>
-      <translation type="unfinished">Trigger: %1</translation>
+      <translation>Trigger: %1</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="263"/>
       <source>Property</source>
       <comment>trigger header</comment>
-      <translation type="unfinished">Property</translation>
+      <translation>Özellik</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="263"/>
       <source>Value</source>
       <comment>trigger header</comment>
-      <translation type="unfinished">Value</translation>
+      <translation>Değer</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="265"/>
       <source>Activation time</source>
-      <translation type="unfinished">Activation time</translation>
+      <translation>Aktivasyon zamanı</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="268"/>
       <source>For action</source>
-      <translation type="unfinished">For action</translation>
+      <translation>İşlemi için</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="272"/>
       <source>On view</source>
-      <translation type="unfinished">On view</translation>
+      <translation>View</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="274"/>
       <source>On table</source>
-      <translation type="unfinished">On table</translation>
+      <translation>Tablo</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="279"/>
       <source>Activation condition</source>
-      <translation type="unfinished">Activation condition</translation>
+      <translation>Aktivasyon koşulu</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="285"/>
       <source>Code executed</source>
-      <translation type="unfinished">Code executed</translation>
+      <translation>Çalıştırılan kod</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="297"/>
       <source>View: %1</source>
-      <translation type="unfinished">View: %1</translation>
+      <translation>View: %1</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="298"/>
       <source>Query:</source>
-      <translation type="unfinished">Query:</translation>
+      <translation>Sorgu:</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="654"/>
       <source>Document generated with SQLiteStudio v%1</source>
-      <translation type="unfinished">Document generated with SQLiteStudio v%1</translation>
+      <translation>SQLiteStudio ile üretilen döküman üzerinde v%1</translation>
     </message>
   </context>
   <context>
@@ -171,32 +171,32 @@
     <message>
       <location filename="../pdfexport.ui" line="20"/>
       <source>Size and layout</source>
-      <translation type="unfinished">Size and layout</translation>
+      <translation>Ölçü ve düzen</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="36"/>
       <source>Page size:</source>
-      <translation type="unfinished">Page size:</translation>
+      <translation>Sayfa boyutu:</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="63"/>
       <source>Right margin:</source>
-      <translation type="unfinished">Right margin:</translation>
+      <translation>Sağ kenar boşluğu:</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="70"/>
       <source>Left margin:</source>
-      <translation type="unfinished">Left margin:</translation>
+      <translation>Sol kenar boşluğu:</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="77"/>
       <source>Cell padding:</source>
-      <translation type="unfinished">Cell padding:</translation>
+      <translation>Hücre boşluğu:</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="84"/>
       <source>Limit characters in single cell:</source>
-      <translation type="unfinished">Limit characters in single cell:</translation>
+      <translation>Bir hücredeki karakter limiti:</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="91"/>
@@ -205,52 +205,52 @@
       <location filename="../pdfexport.ui" line="132"/>
       <location filename="../pdfexport.ui" line="156"/>
       <source>mm</source>
-      <translation type="unfinished">mm</translation>
+      <translation>mm</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="139"/>
       <source>Bottom margin:</source>
-      <translation type="unfinished">Bottom margin:</translation>
+      <translation>Alt kenar boşluğu:</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="163"/>
       <source>Top margin:</source>
-      <translation type="unfinished">Top margin:</translation>
+      <translation>Üst kenar boşluğu:</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="183"/>
       <source>Font</source>
-      <translation type="unfinished">Font</translation>
+      <translation>Yazı tipi</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="206"/>
       <source>Colors</source>
-      <translation type="unfinished">Colors</translation>
+      <translation>Renkler</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="212"/>
       <source>Headers background:</source>
-      <translation type="unfinished">Headers background:</translation>
+      <translation>Başlık arkaplanı:</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="235"/>
       <source>NULL value color:</source>
-      <translation type="unfinished">NULL value color:</translation>
+      <translation>NULL değer rengi:</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="261"/>
       <source>Other settings</source>
-      <translation type="unfinished">Other settings</translation>
+      <translation>Diğer ayarlar</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="267"/>
       <source>Print row numbers for data</source>
-      <translation type="unfinished">Print row numbers for data</translation>
+      <translation>Veriler için satır numarasını yazdır</translation>
     </message>
     <message>
       <location filename="../pdfexport.ui" line="277"/>
       <source>Print page numbers</source>
-      <translation type="unfinished">Print page numbers</translation>
+      <translation>Sayfa numaralarını yazdır</translation>
     </message>
   </context>
 </TS>
