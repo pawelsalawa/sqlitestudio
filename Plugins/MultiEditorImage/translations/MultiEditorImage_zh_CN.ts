ded744cf17c74e51ff5db51b3a3872373f49a0ff
Paweł Salawa <pawelsalawa@gmail.com>
2020-04-28 13:57:07 +0200

    Fixed typo in message.

--- Plugins/MultiEditorImage/translations/MultiEditorImage_zh_CN.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_zh_CN.ts
@@ -1,80 +1,84 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh-CN" sourcelanguage="en">
-  <context>
+<context>
     <name>MultiEditorImage</name>
     <message>
-      <location filename="../multieditorimage.cpp" line="26"/>
-      <source>Load from file</source>
-      <translation>从文件加载</translation>
+        <location filename="../multieditorimage.cpp" line="26"/>
+        <source>Load from file</source>
+        <translation>从文件加载</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="27"/>
-      <source>Store in a file</source>
-      <translation>保存为文件</translation>
+        <location filename="../multieditorimage.cpp" line="27"/>
+        <source>Store in a file</source>
+        <translation>保存为文件</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="28"/>
-      <source>Zoom in by 25%</source>
-      <translation>放大25%</translation>
+        <location filename="../multieditorimage.cpp" line="28"/>
+        <source>Zoom in by 25%</source>
+        <translation>放大25%</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="29"/>
-      <source>Zoom out by 25%</source>
-      <translation>缩小25%</translation>
+        <location filename="../multieditorimage.cpp" line="29"/>
+        <source>Zoom out by 25%</source>
+        <translation>缩小25%</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="30"/>
-      <source>Reset zoom</source>
-      <translation>重置缩放</translation>
+        <location filename="../multieditorimage.cpp" line="30"/>
+        <source>Reset zoom</source>
+        <translation>重置缩放</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="100"/>
-      <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
-      <translation>图片 (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;所有文件 (*)</translation>
+        <location filename="../multieditorimage.cpp" line="100"/>
+        <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
+        <translation>图片 (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;所有文件 (*)</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="101"/>
-      <source>Open image</source>
-      <translation>打开图片</translation>
+        <location filename="../multieditorimage.cpp" line="101"/>
+        <source>Open image</source>
+        <translation>打开图片</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="110"/>
-      <source>Could not open file %1 for reading.</source>
-      <translation>无法以读模式打开文件 %1。</translation>
+        <location filename="../multieditorimage.cpp" line="110"/>
+        <source>Could not open file %1 for reading.</source>
+        <translation>无法以读模式打开文件 %1。</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="130"/>
-      <source>All files (*)</source>
-      <translation>所有文件 (*)</translation>
+        <location filename="../multieditorimage.cpp" line="130"/>
+        <source>All files (*)</source>
+        <translation>所有文件 (*)</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="132"/>
-      <source>Save image</source>
-      <translation>保存图片</translation>
+        <location filename="../multieditorimage.cpp" line="132"/>
+        <source>Save image</source>
+        <translation>保存图片</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="143"/>
-      <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
-      <translation>试图以与原始格式 (%2) 不同的格式 (%1) 保存图像，但应用程序转换失败。 图像将以未改变的格式 (%3) 保存在给定名称下 (%4)</translation>
+        <location filename="../multieditorimage.cpp" line="143"/>
+        <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
+        <translation>试图以与原始格式 (%2) 不同的格式 (%1) 保存图像，但应用程序转换失败。 图像将以未改变的格式 (%3) 保存在给定名称下 (%4)</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="154"/>
-      <source>Could not nopen file %1 for writting.</source>
-      <translation>无法以写模式打开文件 %1：</translation>
+        <location filename="../multieditorimage.cpp" line="154"/>
+        <source>Could not open file %1 for writting.</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="159"/>
-      <source>Could not write image into the file %1</source>
-      <translation>无法将图像写入文件 %1</translation>
+        <source>Could not nopen file %1 for writting.</source>
+        <translation type="vanished">无法以写模式打开文件 %1：</translation>
     </message>
-  </context>
-  <context>
+    <message>
+        <location filename="../multieditorimage.cpp" line="159"/>
+        <source>Could not write image into the file %1</source>
+        <translation>无法将图像写入文件 %1</translation>
+    </message>
+</context>
+<context>
     <name>MultiEditorImagePlugin</name>
     <message>
-      <location filename="../multieditorimage.cpp" line="250"/>
-      <source>Image</source>
-      <translation>图片</translation>
+        <location filename="../multieditorimage.cpp" line="250"/>
+        <source>Image</source>
+        <translation>图片</translation>
     </message>
-  </context>
+</context>
 </TS>
