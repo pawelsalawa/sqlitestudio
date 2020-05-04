0731ad045b0cfc861b9a67aff4eca005752b052e
Paweł Salawa <pawelsalawa@gmail.com>
2020-05-04 22:51:15 +0200

    New translations DbSqliteCipher.ts (Spanish)

--- Plugins/DbSqliteCipher/translations/DbSqliteCipher_es_ES.ts
+++ Plugins/DbSqliteCipher/translations/DbSqliteCipher_es_ES.ts
@@ -6,53 +6,53 @@
     <message>
       <location filename="../dbsqlitecipher.cpp" line="43"/>
       <source>Password (key)</source>
-      <translation type="unfinished">Password (key)</translation>
+      <translation>Contraseña (clave)</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="44"/>
       <source>Leave empty to create or connect to decrypted database.</source>
-      <translation type="unfinished">Leave empty to create or connect to decrypted database.</translation>
+      <translation>Dejar en blanco para crear o conectarse a una base de datos desencriptada.</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="45"/>
       <source>Encryption password</source>
-      <translation type="unfinished">Encryption password</translation>
+      <translation>Contraseña de encriptado</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="50"/>
       <source>Cipher</source>
-      <translation type="unfinished">Cipher</translation>
+      <translation>Cifrado</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="51"/>
       <source>Must be the same as the one used when creating the database. %1 is the default one.</source>
-      <translation type="unfinished">Must be the same as the one used when creating the database. %1 is the default one.</translation>
+      <translation>Debe ser igual que aquel usado al crear la base de datos. %1 es el predeterminado.</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="59"/>
       <source>KDF iterations</source>
-      <translation type="unfinished">KDF iterations</translation>
+      <translation>Iteraciones de KDF</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="63"/>
       <location filename="../dbsqlitecipher.cpp" line="72"/>
       <source>Must be the same as the one used when creating the database. %1 is the default.</source>
-      <translation type="unfinished">Must be the same as the one used when creating the database. %1 is the default.</translation>
+      <translation>Debe ser igual que aquel usado al crear la base de datos. %1 es el predeterminado.</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="68"/>
       <source>Cipher page size</source>
-      <translation type="unfinished">Cipher page size</translation>
+      <translation>Tamaño de página del cifrado</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="77"/>
       <source>1.1 compatibility</source>
-      <translation type="unfinished">1.1 compatibility</translation>
+      <translation>Compatibilidad 1.1</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="79"/>
       <source>Enabling this option disables HMAC checks introduced in SQLCipher 2.0, thus making the connection compatible with SQLCipher 1.1.x.</source>
-      <translation type="unfinished">Enabling this option disables HMAC checks introduced in SQLCipher 2.0, thus making the connection compatible with SQLCipher 1.1.x.</translation>
+      <translation>Activar esta opción desactiva las comprobaciones HMAC introducidas en SQLCipher 2.0, haciendo compatible la conexión con SQLCipher 1.1.x.</translation>
     </message>
   </context>
 </TS>
