198d62b6015d06338a4e3d3541272b7e9f8cafac
Paweł Salawa <pawelsalawa@gmail.com>
2020-09-19 10:26:43 +0200

    New translations DbSqliteCipher.ts (Hebrew)

--- Plugins/DbSqliteCipher/translations/DbSqliteCipher_he_IL.ts
+++ Plugins/DbSqliteCipher/translations/DbSqliteCipher_he_IL.ts
@@ -26,12 +26,12 @@
     <message>
       <location filename="../dbsqlitecipher.cpp" line="51"/>
       <source>Must be the same as the one used when creating the database. %1 is the default one.</source>
-      <translation type="unfinished">Must be the same as the one used when creating the database. %1 is the default one.</translation>
+      <translation>חייב להיות זהה לזה ששימש ביצירת מסד נתונים. ברירת המחדל היא %1.</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="59"/>
       <source>KDF iterations</source>
-      <translation type="unfinished">KDF iterations</translation>
+      <translation>חִזְרוּרי KDF</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="63"/>
@@ -42,17 +42,17 @@
     <message>
       <location filename="../dbsqlitecipher.cpp" line="68"/>
       <source>Cipher page size</source>
-      <translation type="unfinished">Cipher page size</translation>
+      <translation>גודל דף צופן</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="77"/>
       <source>1.1 compatibility</source>
-      <translation type="unfinished">1.1 compatibility</translation>
+      <translation>תאימות 1.1</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="79"/>
       <source>Enabling this option disables HMAC checks introduced in SQLCipher 2.0, thus making the connection compatible with SQLCipher 1.1.x.</source>
-      <translation type="unfinished">Enabling this option disables HMAC checks introduced in SQLCipher 2.0, thus making the connection compatible with SQLCipher 1.1.x.</translation>
+      <translation>הפעלת אפשרות זו מבטלת בדיקות HMAC שהוצגו ב- SQLCipher 2.0, ובכך הופך את החיבור לתואם ל- SQLCipher 1.1.x.</translation>
     </message>
   </context>
 </TS>
