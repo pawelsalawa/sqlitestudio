26158bec10ef1e7818c4c39a54a6923ab76fa9df
Paweł Salawa <pawelsalawa@gmail.com>
2020-09-20 21:25:48 +0200

    New translations SqlEnterpriseFormatter.ts (Hebrew)

--- Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_he_IL.ts
+++ Plugins/SqlEnterpriseFormatter/translations/SqlEnterpriseFormatter_he_IL.ts
@@ -86,7 +86,7 @@
     <message>
       <location filename="../sqlenterpriseformatter.ui" line="250"/>
       <source>Put each column constraint in CREATE TABLE into new line</source>
-      <translation type="unfinished">Put each column constraint in CREATE TABLE into new line</translation>
+      <translation>הכנסת כל אילוץ עמודה ב 'יצירת טבלה' לשורה חדשה</translation>
     </message>
     <message>
       <location filename="../sqlenterpriseformatter.ui" line="260"/>
