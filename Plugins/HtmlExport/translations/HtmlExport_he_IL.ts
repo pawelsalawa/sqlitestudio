286f7f05f7ffed91827854d134c3867cc2265cf1
Paweł Salawa <pawelsalawa@gmail.com>
2021-04-17 16:51:08 +0200

    New translations HtmlExport.ts (Hebrew)

--- Plugins/HtmlExport/translations/HtmlExport_he_IL.ts
+++ Plugins/HtmlExport/translations/HtmlExport_he_IL.ts
@@ -1,173 +1,173 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="he" sourcelanguage="en">
-<context>
+  <context>
     <name>HtmlExport</name>
     <message>
-        <location filename="../htmlexport.cpp" line="46"/>
-        <source>SQL query results</source>
-        <translation>תוצאות שאילתת SQL</translation>
+      <location filename="../htmlexport.cpp" line="46"/>
+      <source>SQL query results</source>
+      <translation>תוצאות שאילתת SQL</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="78"/>
-        <location filename="../htmlexport.cpp" line="155"/>
-        <source>no type</source>
-        <translation>ללא סוג</translation>
+      <location filename="../htmlexport.cpp" line="78"/>
+      <location filename="../htmlexport.cpp" line="155"/>
+      <source>no type</source>
+      <translation>ללא סוג</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="115"/>
-        <location filename="../htmlexport.cpp" line="243"/>
-        <source>Exported table: %1</source>
-        <translation>טבלה מיוצת: %1</translation>
+      <location filename="../htmlexport.cpp" line="115"/>
+      <location filename="../htmlexport.cpp" line="243"/>
+      <source>Exported table: %1</source>
+      <translation>טבלה מיוצת: %1</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="127"/>
-        <location filename="../htmlexport.cpp" line="255"/>
-        <source>Table: %1</source>
-        <translation>טבלה: %1</translation>
+      <location filename="../htmlexport.cpp" line="127"/>
+      <location filename="../htmlexport.cpp" line="255"/>
+      <source>Table: %1</source>
+      <translation>טבלה: %1</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="255"/>
-        <source>virtual</source>
-        <translation>מְדֻמֶּה</translation>
+      <location filename="../htmlexport.cpp" line="255"/>
+      <source>virtual</source>
+      <translation>מְדֻמֶּה</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="308"/>
-        <source>Exported database: %1</source>
-        <translation>מסד נתונים מיוצא: %1</translation>
+      <location filename="../htmlexport.cpp" line="308"/>
+      <source>Exported database: %1</source>
+      <translation>מסד נתונים מיוצא: %1</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="324"/>
-        <source>Index: %1</source>
-        <translation>מִפְתֵּחַ: %1</translation>
+      <location filename="../htmlexport.cpp" line="324"/>
+      <source>Index: %1</source>
+      <translation>מִפְתֵּחַ: %1</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="330"/>
-        <source>For table:</source>
-        <translation>עבור טבלה:</translation>
+      <location filename="../htmlexport.cpp" line="330"/>
+      <source>For table:</source>
+      <translation>עבור טבלה:</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="337"/>
-        <source>Unique:</source>
-        <translation>ייחודי:</translation>
+      <location filename="../htmlexport.cpp" line="337"/>
+      <source>Unique:</source>
+      <translation>ייחודי:</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="338"/>
-        <source>Yes</source>
-        <translation>כן</translation>
+      <location filename="../htmlexport.cpp" line="338"/>
+      <source>Yes</source>
+      <translation>כן</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="338"/>
-        <source>No</source>
-        <translation>לא</translation>
+      <location filename="../htmlexport.cpp" line="338"/>
+      <source>No</source>
+      <translation>לא</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="344"/>
-        <source>Column</source>
-        <translation>עמודה</translation>
+      <location filename="../htmlexport.cpp" line="344"/>
+      <source>Column</source>
+      <translation>עמודה</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="345"/>
-        <source>Collating</source>
-        <translation>ארגון</translation>
+      <location filename="../htmlexport.cpp" line="345"/>
+      <source>Collating</source>
+      <translation>ארגון</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="346"/>
-        <source>Sort order</source>
-        <translation>סדר מיון</translation>
+      <location filename="../htmlexport.cpp" line="346"/>
+      <source>Sort order</source>
+      <translation>סדר מיון</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="380"/>
-        <source>Trigger: %1</source>
-        <translation>מַזְנֵק: %1</translation>
+      <location filename="../htmlexport.cpp" line="380"/>
+      <source>Trigger: %1</source>
+      <translation>מַזְנֵק: %1</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="386"/>
-        <source>Activated:</source>
-        <translation>מופעל:</translation>
+      <location filename="../htmlexport.cpp" line="386"/>
+      <source>Activated:</source>
+      <translation>מופעל:</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="394"/>
-        <source>Action:</source>
-        <translation>פעולה:</translation>
+      <location filename="../htmlexport.cpp" line="394"/>
+      <source>Action:</source>
+      <translation>פעולה:</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="401"/>
-        <source>On view:</source>
-        <translation>אין מצג:</translation>
+      <location filename="../htmlexport.cpp" line="401"/>
+      <source>On view:</source>
+      <translation>אין מצג:</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="403"/>
-        <source>On table:</source>
-        <translation>אין טבלה:</translation>
+      <location filename="../htmlexport.cpp" line="403"/>
+      <source>On table:</source>
+      <translation>אין טבלה:</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="414"/>
-        <source>Activate condition:</source>
-        <translation>שפעול תנאי:</translation>
+      <location filename="../htmlexport.cpp" line="414"/>
+      <source>Activate condition:</source>
+      <translation>שפעול תנאי:</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="421"/>
-        <source>Code executed:</source>
-        <translation>הקוד בוצע:</translation>
+      <location filename="../htmlexport.cpp" line="421"/>
+      <source>Code executed:</source>
+      <translation>הקוד בוצע:</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="455"/>
-        <source>View: %1</source>
-        <translation>מצג: %1</translation>
+      <location filename="../htmlexport.cpp" line="455"/>
+      <source>View: %1</source>
+      <translation>מצג: %1</translation>
     </message>
     <message>
-        <location filename="../htmlexport.cpp" line="480"/>
-        <source>Document generated by SQLiteStudio v%1 on %2</source>
-        <translation>מסמך חולל על ידי SQLiteStudio v%1 ב %2</translation>
+      <location filename="../htmlexport.cpp" line="480"/>
+      <source>Document generated by SQLiteStudio v%1 on %2</source>
+      <translation>מסמך חולל על ידי SQLiteStudio v%1 ב %2</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>HtmlExportConfig</name>
     <message>
-        <location filename="../htmlexport.ui" line="36"/>
-        <source>Maximum number of characters per cell:</source>
-        <translation>מספר תווים מזערי בכל תא:</translation>
+      <location filename="../htmlexport.ui" line="36"/>
+      <source>Maximum number of characters per cell:</source>
+      <translation>מספר תווים מזערי בכל תא:</translation>
     </message>
     <message>
-        <location filename="../htmlexport.ui" line="43"/>
-        <source>Include data types in first row</source>
-        <translation>הכללת סוגי נתונים בשורה ראשונה</translation>
+      <location filename="../htmlexport.ui" line="43"/>
+      <source>Include data types in first row</source>
+      <translation>הכללת סוגי נתונים בשורה ראשונה</translation>
     </message>
     <message>
-        <location filename="../htmlexport.ui" line="53"/>
-        <source>Column names as first row</source>
-        <translation>שמות עמודות בשורה ראשונה</translation>
+      <location filename="../htmlexport.ui" line="53"/>
+      <source>Column names as first row</source>
+      <translation>שמות עמודות בשורה ראשונה</translation>
     </message>
     <message>
-        <location filename="../htmlexport.ui" line="63"/>
-        <source>Row numbers as first column</source>
-        <translation>מספר שורה כעמודה ראשונה</translation>
+      <location filename="../htmlexport.ui" line="63"/>
+      <source>Row numbers as first column</source>
+      <translation>מספר שורה כעמודה ראשונה</translation>
     </message>
     <message>
-        <location filename="../htmlexport.ui" line="73"/>
-        <source>Output format</source>
-        <translation>תבנית פלט</translation>
+      <location filename="../htmlexport.ui" line="73"/>
+      <source>Output format</source>
+      <translation>תבנית פלט</translation>
     </message>
     <message>
-        <location filename="../htmlexport.ui" line="79"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation>תבנות מסמך (שורה חדשה, הזחה)</translation>
+      <location filename="../htmlexport.ui" line="79"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>תבנות מסמך (שורה חדשה, הזחה)</translation>
     </message>
     <message>
-        <location filename="../htmlexport.ui" line="92"/>
-        <source>Compress (everything in one line)</source>
-        <translation>דחיסה (הכל בשורה אחת)</translation>
+      <location filename="../htmlexport.ui" line="92"/>
+      <source>Compress (everything in one line)</source>
+      <translation>דחיסה (הכל בשורה אחת)</translation>
     </message>
     <message>
-        <location filename="../htmlexport.ui" line="108"/>
-        <source>&lt;p&gt;When enabled, HTML characters such as &amp;lt;, &amp;gt; and &amp;amp; are not escaped in exported values. This allows you for example to export hyper-link enabled documents, but it also may result in incorrect HTML document (unmatched pairs of &amp;lt; and &amp;gt; characters). Be warned.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;כאשר אפשרות זו מופעלת, תווי HTML כגון &amp;lt;, &amp;gt; ו- &amp; לא יחמקו ביצוא ערכים. דבר מאפשר לייצא מסמכים המופעלים באמצעות היפר-קישור, אך זה עלול להסתיים במסמך HTML שגוי (זוגות תווים &amp;lt; and &amp;gt; לא תואמים). לתשומת לב. &lt;/p&gt;</translation>
+      <location filename="../htmlexport.ui" line="108"/>
+      <source>&lt;p&gt;When enabled, HTML characters such as &amp;lt;, &amp;gt; and &amp;amp; are not escaped in exported values. This allows you for example to export hyper-link enabled documents, but it also may result in incorrect HTML document (unmatched pairs of &amp;lt; and &amp;gt; characters). Be warned.&lt;/p&gt;</source>
+      <translation>&lt;p&gt;כאשר אפשרות זו מופעלת, תווי HTML כגון &amp;lt;, &amp;gt; ו- &amp; לא יחמקו ביצוא ערכים. דבר מאפשר לייצא מסמכים המופעלים באמצעות היפר-קישור, אך זה עלול להסתיים במסמך HTML שגוי (זוגות תווים &amp;lt; and &amp;gt; לא תואמים). לתשומת לב. &lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../htmlexport.ui" line="111"/>
-        <source>Don&apos;t escape HTML characters</source>
-        <translation>לא לחמוק מתווי HTML</translation>
+      <location filename="../htmlexport.ui" line="111"/>
+      <source>Don&apos;t escape HTML characters</source>
+      <translation>לא לחמוק מתווי HTML</translation>
     </message>
-</context>
+  </context>
 </TS>
