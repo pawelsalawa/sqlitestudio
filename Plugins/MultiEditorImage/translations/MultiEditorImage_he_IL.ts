6b91e35bc60587d1ade9eae593565af1e47ca603
Paweł Salawa <pawelsalawa@gmail.com>
2021-04-17 16:58:31 +0200

    New translations MultiEditorImage.ts (Hebrew)

--- Plugins/MultiEditorImage/translations/MultiEditorImage_he_IL.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_he_IL.ts
@@ -29,42 +29,42 @@
       <translation>איפוס מיקוד</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="100"/>
+      <location filename="../multieditorimage.cpp" line="104"/>
       <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
       <translation>תמונות (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);כל הקבצים (*)</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="101"/>
+      <location filename="../multieditorimage.cpp" line="105"/>
       <source>Open image</source>
       <translation>פתיחת תמונה</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="110"/>
+      <location filename="../multieditorimage.cpp" line="114"/>
       <source>Could not open file %1 for reading.</source>
       <translation>לא ניתן לפתוח קובץ%1 לקריאה.</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="130"/>
+      <location filename="../multieditorimage.cpp" line="134"/>
       <source>All files (*)</source>
       <translation>כל הקבצים (*)</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="132"/>
+      <location filename="../multieditorimage.cpp" line="136"/>
       <source>Save image</source>
       <translation>שמירת תמונה</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="143"/>
+      <location filename="../multieditorimage.cpp" line="155"/>
       <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
       <translation>נעשה ניסה לשמירת תמונה בתבנית (%1) השונה מהמקור (%2), אך היישום לא הצליח להמיר אותה. התמונה ללא שינוי בתבנית (%3) תישמר בשם (%4)</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="154"/>
+      <location filename="../multieditorimage.cpp" line="166"/>
       <source>Could not open file %1 for writting.</source>
       <translation>לא ניתן לפתוח קובץ%1 לכתיבה.</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="159"/>
+      <location filename="../multieditorimage.cpp" line="171"/>
       <source>Could not write image into the file %1</source>
       <translation>לא ניתן לכתוב תמונה לתוך קובץ %1</translation>
     </message>
@@ -72,7 +72,7 @@
   <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-      <location filename="../multieditorimage.cpp" line="250"/>
+      <location filename="../multieditorimage.cpp" line="262"/>
       <source>Image</source>
       <translation>תמונה</translation>
     </message>
