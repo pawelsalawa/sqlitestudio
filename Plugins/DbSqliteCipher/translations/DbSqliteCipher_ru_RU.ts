4eaabca58e75f390f4a9f950d57eed2bdb949e66
Paweł Salawa <pawelsalawa@gmail.com>
2021-04-17 16:57:11 +0200

    New translations DbSqliteCipher.ts (Russian)

--- Plugins/DbSqliteCipher/translations/DbSqliteCipher_ru_RU.ts
+++ Plugins/DbSqliteCipher/translations/DbSqliteCipher_ru_RU.ts
@@ -1,62 +1,36 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ru" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteCipher</name>
     <message>
-        <location filename="../dbsqlitecipher.cpp" line="30"/>
-        <source>Password (key)</source>
-        <translation>Пароль (ключ)</translation>
+      <location filename="../dbsqlitecipher.cpp" line="30"/>
+      <source>Password (key)</source>
+      <translation>Пароль (ключ)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitecipher.cpp" line="31"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation>Оставьте пустым для создания или подключения к расшифрованной базе данных.</translation>
+      <location filename="../dbsqlitecipher.cpp" line="31"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Оставьте пустым для создания или подключения к расшифрованной базе данных</translation>
     </message>
     <message>
-        <location filename="../dbsqlitecipher.cpp" line="32"/>
-        <source>Encryption password</source>
-        <translation>Пароль для шифрования</translation>
+      <location filename="../dbsqlitecipher.cpp" line="32"/>
+      <source>Encryption password</source>
+      <translation>Пароль для шифрования</translation>
     </message>
     <message>
-        <location filename="../dbsqlitecipher.cpp" line="37"/>
-        <source>Cipher configuration (optional)</source>
-        <translation type="unfinished"></translation>
+      <location filename="../dbsqlitecipher.cpp" line="37"/>
+      <source>Cipher configuration (optional)</source>
+      <translation type="unfinished">Cipher configuration (optional)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitecipher.cpp" line="38"/>
-        <source>PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitecipher.cpp" line="38"/>
+      <source>PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLCipher for details.</source>
-        <translation type="unfinished"></translation>
+      <translation type="unfinished">PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
+They will be executed upon each opening of the database.
+See documentation for SQLCipher for details.</translation>
     </message>
-    <message>
-        <source>Cipher</source>
-        <translation type="vanished">Шифр</translation>
-    </message>
-    <message>
-        <source>Must be the same as the one used when creating the database. %1 is the default one.</source>
-        <translation type="vanished">Должен совпадать с использованным при создании базы данных. По умолчанию используется %1.</translation>
-    </message>
-    <message>
-        <source>KDF iterations</source>
-        <translation type="vanished">Количество итераций KDF</translation>
-    </message>
-    <message>
-        <source>Must be the same as the one used when creating the database. %1 is the default.</source>
-        <translation type="vanished">Должно совпадать с использованным при создании базы данных. По умолчанию используется %1.</translation>
-    </message>
-    <message>
-        <source>Cipher page size</source>
-        <translation type="vanished">Размер страницы шифра</translation>
-    </message>
-    <message>
-        <source>1.1 compatibility</source>
-        <translation type="vanished">Совместимость с 1.1</translation>
-    </message>
-    <message>
-        <source>Enabling this option disables HMAC checks introduced in SQLCipher 2.0, thus making the connection compatible with SQLCipher 1.1.x.</source>
-        <translation type="vanished">Включение этой опции отключает проверки HMAC, реализованные в SQLCipher 2.0, обеспечивая совместимость подключения с SQLCipher 1.1.x.</translation>
-    </message>
-</context>
+  </context>
 </TS>
