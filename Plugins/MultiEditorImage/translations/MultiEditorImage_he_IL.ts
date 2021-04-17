5831d91a6af6b02aaa5f7366880e8e08c040228e
Paweł Salawa <pawelsalawa@gmail.com>
2021-04-17 16:51:06 +0200

    New translations MultiEditorImage.ts (Hebrew)

--- Plugins/MultiEditorImage/translations/MultiEditorImage_he_IL.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_he_IL.ts
@@ -1,80 +1,80 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="he" sourcelanguage="en">
-<context>
+  <context>
     <name>MultiEditorImage</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="26"/>
-        <source>Load from file</source>
-        <translation>טעינה מקובץ</translation>
+      <location filename="../multieditorimage.cpp" line="26"/>
+      <source>Load from file</source>
+      <translation>טעינה מקובץ</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="27"/>
-        <source>Store in a file</source>
-        <translation>אחסון בקובץ</translation>
+      <location filename="../multieditorimage.cpp" line="27"/>
+      <source>Store in a file</source>
+      <translation>אחסון בקובץ</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="28"/>
-        <source>Zoom in by 25%</source>
-        <translation>קרוב מיקוד ב 25%</translation>
+      <location filename="../multieditorimage.cpp" line="28"/>
+      <source>Zoom in by 25%</source>
+      <translation>קרוב מיקוד ב 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="29"/>
-        <source>Zoom out by 25%</source>
-        <translation>רחוק מיקוד ב 25%</translation>
+      <location filename="../multieditorimage.cpp" line="29"/>
+      <source>Zoom out by 25%</source>
+      <translation>רחוק מיקוד ב 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="30"/>
-        <source>Reset zoom</source>
-        <translation>איפוס מיקוד</translation>
+      <location filename="../multieditorimage.cpp" line="30"/>
+      <source>Reset zoom</source>
+      <translation>איפוס מיקוד</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="104"/>
-        <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
-        <translation>תמונות (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);כל הקבצים (*)</translation>
+      <location filename="../multieditorimage.cpp" line="100"/>
+      <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
+      <translation>תמונות (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);כל הקבצים (*)</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="105"/>
-        <source>Open image</source>
-        <translation>פתיחת תמונה</translation>
+      <location filename="../multieditorimage.cpp" line="101"/>
+      <source>Open image</source>
+      <translation>פתיחת תמונה</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="114"/>
-        <source>Could not open file %1 for reading.</source>
-        <translation>לא ניתן לפתוח קובץ%1 לקריאה.</translation>
+      <location filename="../multieditorimage.cpp" line="110"/>
+      <source>Could not open file %1 for reading.</source>
+      <translation>לא ניתן לפתוח קובץ%1 לקריאה.</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="134"/>
-        <source>All files (*)</source>
-        <translation>כל הקבצים (*)</translation>
+      <location filename="../multieditorimage.cpp" line="130"/>
+      <source>All files (*)</source>
+      <translation>כל הקבצים (*)</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="136"/>
-        <source>Save image</source>
-        <translation>שמירת תמונה</translation>
+      <location filename="../multieditorimage.cpp" line="132"/>
+      <source>Save image</source>
+      <translation>שמירת תמונה</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="155"/>
-        <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
-        <translation>נעשה ניסה לשמירת תמונה בתבנית (%1) השונה מהמקור (%2), אך היישום לא הצליח להמיר אותה. התמונה ללא שינוי בתבנית (%3) תישמר בשם (%4)</translation>
+      <location filename="../multieditorimage.cpp" line="143"/>
+      <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
+      <translation>נעשה ניסה לשמירת תמונה בתבנית (%1) השונה מהמקור (%2), אך היישום לא הצליח להמיר אותה. התמונה ללא שינוי בתבנית (%3) תישמר בשם (%4)</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="166"/>
-        <source>Could not open file %1 for writting.</source>
-        <translation>לא ניתן לפתוח קובץ%1 לכתיבה.</translation>
+      <location filename="../multieditorimage.cpp" line="154"/>
+      <source>Could not open file %1 for writting.</source>
+      <translation>לא ניתן לפתוח קובץ%1 לכתיבה.</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="171"/>
-        <source>Could not write image into the file %1</source>
-        <translation>לא ניתן לכתוב תמונה לתוך קובץ %1</translation>
+      <location filename="../multieditorimage.cpp" line="159"/>
+      <source>Could not write image into the file %1</source>
+      <translation>לא ניתן לכתוב תמונה לתוך קובץ %1</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="262"/>
-        <source>Image</source>
-        <translation>תמונה</translation>
+      <location filename="../multieditorimage.cpp" line="250"/>
+      <source>Image</source>
+      <translation>תמונה</translation>
     </message>
-</context>
+  </context>
 </TS>
