f86a0e17b4431f247412c0c6c5d2c50fb165c54d
Paweł Salawa <pawelsalawa@gmail.com>
2021-04-17 16:58:06 +0200

    New translations MultiEditorImage.ts (Russian)

--- Plugins/MultiEditorImage/translations/MultiEditorImage_ru_RU.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_ru_RU.ts
@@ -29,42 +29,42 @@
       <translation>Сбросить масштаб</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="100"/>
+      <location filename="../multieditorimage.cpp" line="104"/>
       <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
       <translation>Изображения (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;Все файлы (*)</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="101"/>
+      <location filename="../multieditorimage.cpp" line="105"/>
       <source>Open image</source>
       <translation>Открыть изображение</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="110"/>
+      <location filename="../multieditorimage.cpp" line="114"/>
       <source>Could not open file %1 for reading.</source>
       <translation>Невозможно открыть файл %1 для чтения.</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="130"/>
+      <location filename="../multieditorimage.cpp" line="134"/>
       <source>All files (*)</source>
       <translation>Все файлы (*)</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="132"/>
+      <location filename="../multieditorimage.cpp" line="136"/>
       <source>Save image</source>
       <translation>Сохранить изображение</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="143"/>
+      <location filename="../multieditorimage.cpp" line="155"/>
       <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
       <translation>Осуществлена попытка сохранить изображение в формате (%1), отличном от исходного (%2), однако приложению не удалось произвести конвертацию. Изображение в исходном формате (%3) будет сохранено с указанным именем (%4)</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="154"/>
+      <location filename="../multieditorimage.cpp" line="166"/>
       <source>Could not open file %1 for writting.</source>
       <translation type="unfinished">Could not open file %1 for writting.</translation>
     </message>
     <message>
-      <location filename="../multieditorimage.cpp" line="159"/>
+      <location filename="../multieditorimage.cpp" line="171"/>
       <source>Could not write image into the file %1</source>
       <translation>Невозможно записать изображение в файл %1</translation>
     </message>
@@ -72,7 +72,7 @@
   <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-      <location filename="../multieditorimage.cpp" line="250"/>
+      <location filename="../multieditorimage.cpp" line="262"/>
       <source>Image</source>
       <translation>Изображение</translation>
     </message>
