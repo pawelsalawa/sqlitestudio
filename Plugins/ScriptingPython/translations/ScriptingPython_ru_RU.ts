ea7163af0e61335e14ec9a86f5732b69c6691cd2
Paweł Salawa <pawelsalawa@gmail.com>
2021-05-24 23:20:23 +0200

    New translations ScriptingPython.ts (Russian)

--- Plugins/ScriptingPython/translations/ScriptingPython_ru_RU.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_ru_RU.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru" sourcelanguage="en">
-<context>
+<TS version="2.1" language="ru" sourcelanguage="en">
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../scriptingpython.cpp" line="466"/>
-        <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-        <translation>Некорректное использование функции %1. Ожидается аргументов: %2, передано аргументов: %3.</translation>
+      <location filename="../scriptingpython.cpp" line="466"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation type="unfinished">Invalid use of %1 function. Expected %2 arguments, but got %3.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="475"/>
-        <source>Unknown error from function %1.</source>
-        <translation>Неизвестная ошибка при выполнении функции %1.</translation>
+      <location filename="../scriptingpython.cpp" line="475"/>
+      <source>Unknown error from function %1.</source>
+      <translation type="unfinished">Unknown error from function %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="516"/>
-        <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-        <translation>Невозможно сформировать строковое представление объекта Python, переданного в виде аргумента в функцию %1.</translation>
+      <location filename="../scriptingpython.cpp" line="516"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation type="unfinished">Could not calculate string representation of the Python object passed as argument to the function %1.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="532"/>
-        <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-        <translation>Не найден контекст выполнения для функции %1. Это ошибка модуля Python. Пожалуйста, сообщите о ней.</translation>
+      <location filename="../scriptingpython.cpp" line="532"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation type="unfinished">Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</translation>
     </message>
     <message>
-        <location filename="../scriptingpython.cpp" line="557"/>
-        <source>Error from Python function %1: %2</source>
-        <translation>Ошибка при выполнении функции Python %1: %2</translation>
+      <location filename="../scriptingpython.cpp" line="557"/>
+      <source>Error from Python function %1: %2</source>
+      <translation type="unfinished">Error from Python function %1: %2</translation>
     </message>
-</context>
+  </context>
 </TS>
