8b0109505eba9a167d9920ef70a2e822ffc2b6ce
Paweł Salawa <pawelsalawa@gmail.com>
2021-07-22 08:48:23 +0200

    New translations sqlitestudio.ts (Hebrew)

--- SQLiteStudio3/sqlitestudio/translations/sqlitestudio_he_IL.ts
+++ SQLiteStudio3/sqlitestudio/translations/sqlitestudio_he_IL.ts
@@ -6,22 +6,22 @@
     <message>
       <location filename="../main.cpp" line="51"/>
       <source>GUI interface to SQLiteStudio, a SQLite manager.</source>
-      <translation>ממשק משתמש גרפי ל- SQLiteStudio, תכנת ניהול SQLite.</translation>
+      <translation>מנשק משתמש גרפי SQLiteStudio, תכנת ניהול SQLite.</translation>
     </message>
     <message>
       <location filename="../main.cpp" line="55"/>
       <source>Enables debug messages in console (accessible with F12).</source>
-      <translation type="unfinished">Enables debug messages in console (accessible with F12).</translation>
+      <translation>אפשור הודעות ניפוי תקלים בעמדת־הבקרה (נגיש באמצעות מקש F12).</translation>
     </message>
     <message>
       <location filename="../main.cpp" line="56"/>
       <source>Redirects debug messages into standard output (forces debug mode).</source>
-      <translation type="unfinished">Redirects debug messages into standard output (forces debug mode).</translation>
+      <translation>הפנית הודעות ניפוי תקלים לפלט תקני (אילוץ מצב ניפוי תקלים).</translation>
     </message>
     <message>
       <location filename="../main.cpp" line="57"/>
       <source>Redirects debug messages into given file (forces debug mode).</source>
-      <translation type="unfinished">Redirects debug messages into given file (forces debug mode).</translation>
+      <translation>הפנית הודעות ניפוי תקלים לקובץ נתון (אילוץ מצב ניפוי תקלים).</translation>
     </message>
     <message>
       <location filename="../main.cpp" line="57"/>
@@ -51,17 +51,17 @@
     <message>
       <location filename="../main.cpp" line="61"/>
       <source>Enables debugging of SQLiteStudio&apos;s query executor.</source>
-      <translation type="unfinished">Enables debugging of SQLiteStudio&apos;s query executor.</translation>
+      <translation>אפשור ניפוי תקלים מריץ השאילתות של SQLiteStudio.</translation>
     </message>
     <message>
       <location filename="../main.cpp" line="62"/>
       <source>Lists plugins installed in the SQLiteStudio and quits.</source>
-      <translation>הצגת מתקעים מותקנים ב- SQLiteStudio ויציאה.</translation>
+      <translation>הצגת רשימת מתקעים מותקנים ב־SQLiteStudio ויציאה.</translation>
     </message>
     <message>
       <location filename="../main.cpp" line="63"/>
       <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-      <translation>הפניה לקובץ התצורה הראשי. פרטים נוספים, ניתן למצוא במדריך למשתמש בעמוד הוויקי.</translation>
+      <translation>הפניה לקובץ התצורה הראשי. פרטים נוספים, ניתן למצוא במדריך למשתמש בעמוד הויקי.</translation>
     </message>
     <message>
       <location filename="../main.cpp" line="63"/>
