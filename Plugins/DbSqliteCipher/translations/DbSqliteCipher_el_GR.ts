d2ab820368acb597518643837f676b8b43516844
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-27 16:35:57 +0200

    New translations DbSqliteCipher.ts (Greek)

--- Plugins/DbSqliteCipher/translations/DbSqliteCipher_el_GR.ts
+++ Plugins/DbSqliteCipher/translations/DbSqliteCipher_el_GR.ts
@@ -6,29 +6,29 @@
     <message>
       <location filename="../dbsqlitecipher.cpp" line="30"/>
       <source>Password (key)</source>
-      <translation>Password (key)</translation>
+      <translation type="unfinished">Password (key)</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="31"/>
       <source>Leave empty to create or connect to decrypted database.</source>
-      <translation>Leave empty to create or connect to decrypted database.</translation>
+      <translation type="unfinished">Leave empty to create or connect to decrypted database.</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="32"/>
       <source>Encryption password</source>
-      <translation>Encryption password</translation>
+      <translation type="unfinished">Encryption password</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="37"/>
       <source>Cipher configuration (optional)</source>
-      <translation>Cipher configuration (optional)</translation>
+      <translation type="unfinished">Cipher configuration (optional)</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="38"/>
       <source>PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLCipher for details.</source>
-      <translation>PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
+      <translation type="unfinished">PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLCipher for details.</translation>
     </message>
