8b0154de0474fa0b31e7d2d2c029639f56fbea70
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-27 11:55:55 +0200

    New translations ScriptingPython.ts (Chinese Simplified)

--- Plugins/ScriptingPython/translations/ScriptingPython_zh_CN.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_zh_CN.ts
@@ -6,27 +6,27 @@
     <message>
       <location filename="../scriptingpython.cpp" line="466"/>
       <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-      <translation type="unfinished">Invalid use of %1 function. Expected %2 arguments, but got %3.</translation>
+      <translation>无效的 %1 函数用法。预期是 %2 参数，但实际是 %3。</translation>
     </message>
     <message>
       <location filename="../scriptingpython.cpp" line="475"/>
       <source>Unknown error from function %1.</source>
-      <translation type="unfinished">Unknown error from function %1.</translation>
+      <translation>函数 %1 的未知错误。</translation>
     </message>
     <message>
       <location filename="../scriptingpython.cpp" line="516"/>
       <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-      <translation type="unfinished">Could not calculate string representation of the Python object passed as argument to the function %1.</translation>
+      <translation>无法计算作为参数传递给 %1 函数的 Python 对象所表示的字符串。</translation>
     </message>
     <message>
       <location filename="../scriptingpython.cpp" line="532"/>
       <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-      <translation type="unfinished">Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</translation>
+      <translation>找不到函数 %1 的执行环境。这是 Python 插件的 bug，请报告该问题。</translation>
     </message>
     <message>
       <location filename="../scriptingpython.cpp" line="557"/>
       <source>Error from Python function %1: %2</source>
-      <translation type="unfinished">Error from Python function %1: %2</translation>
+      <translation>Python 函数 %1 错误：%2</translation>
     </message>
   </context>
 </TS>
