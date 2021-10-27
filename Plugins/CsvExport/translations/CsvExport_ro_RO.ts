f74185aa15d163ef221dec4e6351215d77148916
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-27 16:34:09 +0200

    New translations CsvExport.ts (Romanian)

--- Plugins/CsvExport/translations/CsvExport_ro_RO.ts
+++ Plugins/CsvExport/translations/CsvExport_ro_RO.ts
@@ -6,52 +6,52 @@
     <message>
       <location filename="../CsvExport.ui" line="20"/>
       <source>Column names in first row</source>
-      <translation>Column names in first row</translation>
+      <translation type="unfinished">Column names in first row</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="30"/>
       <source>Column separator:</source>
-      <translation>Column separator:</translation>
+      <translation type="unfinished">Column separator:</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="41"/>
       <source>, (comma)</source>
-      <translation>, (comma)</translation>
+      <translation type="unfinished">, (comma)</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="46"/>
       <source>; (semicolon)</source>
-      <translation>; (semicolon)</translation>
+      <translation type="unfinished">; (semicolon)</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="51"/>
       <source>\t (tab)</source>
-      <translation>\t (tab)</translation>
+      <translation type="unfinished">\t (tab)</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="56"/>
       <source>  (whitespace)</source>
-      <translation>  (whitespace)</translation>
+      <translation type="unfinished">  (whitespace)</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="61"/>
       <source>Custom:</source>
-      <translation>Custom:</translation>
+      <translation type="unfinished">Custom:</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="82"/>
       <source>Export NULL values as:</source>
-      <translation>Export NULL values as:</translation>
+      <translation type="unfinished">Export NULL values as:</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="89"/>
       <source>Empty string</source>
-      <translation>Empty string</translation>
+      <translation type="unfinished">Empty string</translation>
     </message>
     <message>
       <location filename="../csvexport.cpp" line="42"/>
       <source>Enter the custom separator character.</source>
-      <translation>Enter the custom separator character.</translation>
+      <translation type="unfinished">Enter the custom separator character.</translation>
     </message>
   </context>
 </TS>
