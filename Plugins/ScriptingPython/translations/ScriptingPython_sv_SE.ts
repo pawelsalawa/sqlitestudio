9f04f8c8d97004f089b802c639d4b7dfc1ae5250
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-27 16:42:51 +0200

    New translations ScriptingPython.ts (Swedish)

--- Plugins/ScriptingPython/translations/ScriptingPython_sv_SE.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_sv_SE.ts
@@ -6,27 +6,27 @@
     <message>
       <location filename="../scriptingpython.cpp" line="466"/>
       <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
-      <translation>Invalid use of %1 function. Expected %2 arguments, but got %3.</translation>
+      <translation type="unfinished">Invalid use of %1 function. Expected %2 arguments, but got %3.</translation>
     </message>
     <message>
       <location filename="../scriptingpython.cpp" line="475"/>
       <source>Unknown error from function %1.</source>
-      <translation>Unknown error from function %1.</translation>
+      <translation type="unfinished">Unknown error from function %1.</translation>
     </message>
     <message>
       <location filename="../scriptingpython.cpp" line="516"/>
       <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
-      <translation>Could not calculate string representation of the Python object passed as argument to the function %1.</translation>
+      <translation type="unfinished">Could not calculate string representation of the Python object passed as argument to the function %1.</translation>
     </message>
     <message>
       <location filename="../scriptingpython.cpp" line="532"/>
       <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
-      <translation>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</translation>
+      <translation type="unfinished">Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</translation>
     </message>
     <message>
       <location filename="../scriptingpython.cpp" line="557"/>
       <source>Error from Python function %1: %2</source>
-      <translation>Error from Python function %1: %2</translation>
+      <translation type="unfinished">Error from Python function %1: %2</translation>
     </message>
   </context>
 </TS>
