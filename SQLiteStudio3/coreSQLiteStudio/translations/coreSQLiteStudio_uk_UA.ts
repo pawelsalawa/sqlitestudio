591f45fa9dd20eab2233fe256d9797ac39d45bdd
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-27 14:46:57 +0200

    New translations coreSQLiteStudio.ts (Ukrainian)

--- SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_uk_UA.ts
+++ SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_uk_UA.ts
@@ -402,7 +402,7 @@ Tables, indexes, triggers and views copied to database %3 will remain.</source>
     <message>
       <location filename="../services/importmanager.cpp" line="96"/>
       <source>Imported data to the table &apos;%1&apos; successfully. Number of imported rows: %2</source>
-      <translation type="unfinished">Imported data to the table &apos;%1&apos; successfully. Number of imported rows: %2</translation>
+      <translation>Imported data to the table &apos;%1&apos; successfully. Number of imported rows: %2</translation>
     </message>
   </context>
   <context>
@@ -1001,7 +1001,7 @@ The view will remain as it is.</source>
     <message>
       <location filename="../plugins/scriptingqtdbproxy.cpp" line="48"/>
       <source>No database available in current context, while called JavaScript&apos;s %1 command.</source>
-      <translation type="unfinished">No database available in current context, while called JavaScript&apos;s %1 command.</translation>
+      <translation>No database available in current context, while called JavaScript&apos;s %1 command.</translation>
     </message>
     <message>
       <location filename="../plugins/scriptingqtdbproxy.cpp" line="65"/>
@@ -1047,7 +1047,7 @@ The view will remain as it is.</source>
     <message>
       <location filename="../services/updatemanager.cpp" line="92"/>
       <source>Could not check for updates (%1).</source>
-      <translation type="unfinished">Could not check for updates (%1).</translation>
+      <translation>Could not check for updates (%1).</translation>
     </message>
   </context>
 </TS>
