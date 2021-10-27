f312e7cc45fd01ad159b96b1db8a379ffb8f5ac3
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-27 16:42:20 +0200

    New translations DbSqliteWx.ts (Czech)

--- Plugins/DbSqliteWx/translations/DbSqliteWx_cs_CZ.ts
+++ Plugins/DbSqliteWx/translations/DbSqliteWx_cs_CZ.ts
@@ -6,39 +6,39 @@
     <message>
       <location filename="../dbsqlitewx.cpp" line="30"/>
       <source>Password (key)</source>
-      <translation>Password (key)</translation>
+      <translation type="unfinished">Password (key)</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="31"/>
       <source>Leave empty to create or connect to decrypted database.</source>
-      <translation>Leave empty to create or connect to decrypted database.</translation>
+      <translation type="unfinished">Leave empty to create or connect to decrypted database.</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="32"/>
       <source>Encryption password</source>
-      <translation>Encryption password</translation>
+      <translation type="unfinished">Encryption password</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="38"/>
       <source>Cipher</source>
-      <translation>Cipher</translation>
+      <translation type="unfinished">Cipher</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="39"/>
       <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-      <translation>Cipher determines encryption algorithm used to encrypt the database.</translation>
+      <translation type="unfinished">Cipher determines encryption algorithm used to encrypt the database.</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="47"/>
       <source>Cipher configuration (optional)</source>
-      <translation>Cipher configuration (optional)</translation>
+      <translation type="unfinished">Cipher configuration (optional)</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="48"/>
       <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-      <translation>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <translation type="unfinished">PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</translation>
     </message>
