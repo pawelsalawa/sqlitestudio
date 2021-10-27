bba35fdd7f26241b5ca7ee61a28dd3f7817bb5f2
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-27 16:39:28 +0200

    New translations Printing.ts (Slovak)

--- Plugins/Printing/translations/Printing_sk_SK.ts
+++ Plugins/Printing/translations/Printing_sk_SK.ts
@@ -6,27 +6,27 @@
     <message>
       <location filename="../printing.cpp" line="35"/>
       <source>Print data</source>
-      <translation>Print data</translation>
+      <translation type="unfinished">Print data</translation>
     </message>
     <message>
       <location filename="../printing.cpp" line="37"/>
       <source>Print query</source>
-      <translation>Print query</translation>
+      <translation type="unfinished">Print query</translation>
     </message>
     <message>
       <location filename="../printing.cpp" line="74"/>
       <source>No data to print.</source>
-      <translation>No data to print.</translation>
+      <translation type="unfinished">No data to print.</translation>
     </message>
     <message>
       <location filename="../printing.cpp" line="82"/>
       <source>Printing data.</source>
-      <translation>Printing data.</translation>
+      <translation type="unfinished">Printing data.</translation>
     </message>
     <message>
       <location filename="../printing.cpp" line="108"/>
       <source>Printing query.</source>
-      <translation>Printing query.</translation>
+      <translation type="unfinished">Printing query.</translation>
     </message>
   </context>
   <context>
@@ -34,7 +34,7 @@
     <message>
       <location filename="../printingexport.cpp" line="30"/>
       <source>Printing</source>
-      <translation>Printing</translation>
+      <translation type="unfinished">Printing</translation>
     </message>
   </context>
 </TS>
