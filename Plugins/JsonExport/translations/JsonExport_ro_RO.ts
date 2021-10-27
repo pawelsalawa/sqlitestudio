4a716a2b1c8171dc05493f8ce1d67c6db0051d9b
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-27 14:40:11 +0200

    New translations JsonExport.ts (Romanian)

--- Plugins/JsonExport/translations/JsonExport_ro_RO.ts
+++ Plugins/JsonExport/translations/JsonExport_ro_RO.ts
@@ -6,17 +6,17 @@
     <message>
       <location filename="../jsonexport.ui" line="20"/>
       <source>Output format</source>
-      <translation type="unfinished">Output format</translation>
+      <translation>Output format</translation>
     </message>
     <message>
       <location filename="../jsonexport.ui" line="26"/>
       <source>Format document (new lines, indentation)</source>
-      <translation type="unfinished">Format document (new lines, indentation)</translation>
+      <translation>Format document (new lines, indentation)</translation>
     </message>
     <message>
       <location filename="../jsonexport.ui" line="42"/>
       <source>Compress (everything in one line)</source>
-      <translation type="unfinished">Compress (everything in one line)</translation>
+      <translation>Compress (everything in one line)</translation>
     </message>
   </context>
 </TS>
