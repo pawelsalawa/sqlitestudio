f6d3b7625b4c7f744a09a76e2370077790580914
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-28 14:04:37 +0200

    New translations CsvExport.ts (Chinese Simplified)

--- Plugins/CsvExport/translations/CsvExport_zh_CN.ts
+++ Plugins/CsvExport/translations/CsvExport_zh_CN.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../CsvExport.ui" line="20"/>
       <source>Column names in first row</source>
-      <translation>第一行显示列名</translation>
+      <translation>第一行是列名</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="30"/>
@@ -16,7 +16,7 @@
     <message>
       <location filename="../CsvExport.ui" line="41"/>
       <source>, (comma)</source>
-      <translation>，（逗号）</translation>
+      <translation>,（逗号）</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="46"/>
@@ -26,7 +26,7 @@
     <message>
       <location filename="../CsvExport.ui" line="51"/>
       <source>\t (tab)</source>
-      <translation>\t（tab位）</translation>
+      <translation>\t（制表符）</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="56"/>
@@ -41,7 +41,7 @@
     <message>
       <location filename="../CsvExport.ui" line="82"/>
       <source>Export NULL values as:</source>
-      <translation>NULL导出为：</translation>
+      <translation>NULL 值导出为：</translation>
     </message>
     <message>
       <location filename="../CsvExport.ui" line="89"/>
@@ -51,7 +51,7 @@
     <message>
       <location filename="../csvexport.cpp" line="42"/>
       <source>Enter the custom separator character.</source>
-      <translation>输入自定义分隔符 。</translation>
+      <translation>请输入自定义的分隔符。</translation>
     </message>
   </context>
 </TS>
