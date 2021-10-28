08fcadf6f995a0868006d8371bb94d8acfef1802
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-28 14:04:44 +0200

    New translations DbSqliteCipher.ts (Chinese Simplified)

--- Plugins/DbSqliteCipher/translations/DbSqliteCipher_zh_CN.ts
+++ Plugins/DbSqliteCipher/translations/DbSqliteCipher_zh_CN.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../dbsqlitecipher.cpp" line="30"/>
       <source>Password (key)</source>
-      <translation>密码 (密钥)</translation>
+      <translation>密码（密钥）</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="31"/>
       <source>Leave empty to create or connect to decrypted database.</source>
-      <translation>留空以创建或连接到解密的数据库。</translation>
+      <translation>留空则创建或连接到已解密的数据库。</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="32"/>
@@ -21,16 +21,16 @@
     <message>
       <location filename="../dbsqlitecipher.cpp" line="37"/>
       <source>Cipher configuration (optional)</source>
-      <translation>Cipher配置 (可选)</translation>
+      <translation>加密算法配置（可选）</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="38"/>
       <source>PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLCipher for details.</source>
-      <translation>自定义 SQLCiper 配置的 PRAGMA 语句，如KDF 迭代、遗留模式等。
-它们将在数据库的每次打开时执行。
-了解 SQLCipher 文档的详细信息。</translation>
+      <translation>用于自定义 SQLCiper 配置的 PRAGMA 语句，如 KDF 迭代、传统模式等。
+将在每次打开数据库时执行。
+细节请参考 SQLCipher 文档。</translation>
     </message>
   </context>
 </TS>
