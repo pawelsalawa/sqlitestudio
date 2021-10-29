33227149b287d1a90931c904b7d26dcf0b9f99c5
Paweł Salawa <pawelsalawa@gmail.com>
2021-10-29 10:09:50 +0200

    New translations sqlitestudiocli.ts (Chinese Simplified)

--- SQLiteStudio3/sqlitestudiocli/translations/sqlitestudiocli_zh_CN.ts
+++ SQLiteStudio3/sqlitestudiocli/translations/sqlitestudiocli_zh_CN.ts
@@ -433,20 +433,20 @@ The COLUMNS mode is similar to FIXED mode, except it tries to be smart and make 
 ATTENTION! The COLUMNS mode reads all the results from the query at once in order to evaluate column widhts, therefore it is dangerous to use this mode when working with huge result sets. Keep in mind that this mode will load entire result set into memory.
 
 The ROW mode is recommended if you need to see whole values and you don&apos;t expect many rows to be displayed, because this mode displays a line of output per each column, so you&apos;ll get 10 lines for single row with 10 columns, then if you have 10 of such rows, you will get 100 lines of output (+1 extra line per each row, to separate rows from each other).</source>
-      <translation type="unfinished">When called without argument, tells the current output format for a query results. When the &lt;mode&gt; is passed, the mode is changed to the given one. Supported modes are:
-- CLASSIC - columns are separated by a comma, not aligned,
-- FIXED   - columns have equal and fixed width, they always fit into terminal window width, but the data in columns can be cut off,
-- COLUMNS - like FIXED, but smarter (do not use with huge result sets, see details below),
-- ROW     - each column from the row is displayed in new line, so the full data is displayed.
+      <translation>调用不提供参数时，列出当前的查询结果输出格式。传入 &lt;mode&gt; 时，按提供的参数更改模式。支持的模式如下：
+- CLASSIC - 经典：列使用逗号分隔，不对齐。
+- FIXED   - 固定：所有列使用同等的列宽，始终按终端窗口宽度适配，可能裁剪列中的数据。
+- COLUMNS - 列：类似 FIXED，但更智能（不适合大型结果集，详见下文）。
+- ROW     - 行：每个行的每个列都显示为一行，因此能显示完整的数据。
 
-The CLASSIC mode is recommended if you want to see all the data, but you don&apos;t want to waste lines for each column. Each row will display full data for every column, but this also means, that columns will not be aligned to each other in next rows. The CLASSIC mode also doesn&apos;t respect the width of your terminal (console) window, so if values in columns are wider than the window, the row will be continued in next lines.
+CLASSIC 模式适合查看完整的数据而不为每个列浪费行数。每行输出会为每个列显示完整的数据，因此各输出行的列可能不会对齐。CLASSIC 模式不关心终端（控制台）窗口的宽度，因此如果列的值比窗口宽度还长，会自动折行到下一行显示。
 
-The FIXED mode is recommended if you want a readable output and you don&apos;t care about long data values. Columns will be aligned, making the output a nice table. The width of columns is calculated from width of the console window and a number of columns.
+FIXED 模式提供更美观的数据展示，但不提供长数据的完整值。各列会彼此对齐，使输出形成一个美观的表格。列宽度根据控制台窗口的宽度及列数计算。
 
-The COLUMNS mode is similar to FIXED mode, except it tries to be smart and make columns with shorter values more thin, while columns with longer values get more space. First to shrink are columns with longest headers (so the header names are to be cut off as first), then columns with the longest values are shrinked, up to the moment when all columns fit into terminal window.
-ATTENTION! The COLUMNS mode reads all the results from the query at once in order to evaluate column widhts, therefore it is dangerous to use this mode when working with huge result sets. Keep in mind that this mode will load entire result set into memory.
+COLUMNS 模式类似 FIXED 模式，但会智能将短值的列更窄，长值的列更长。首先收缩最长标题的列（因此列名称会被裁切），然后是最长值的列，直至所有列的总计宽度适合终端窗口。
+注意！COLUMNS 模式需一次性读取查询的全部结果以评估列的宽度，因此对大型数据集使用此模式存在风险，它会将整个结果集加载到内存！
 
-The ROW mode is recommended if you need to see whole values and you don&apos;t expect many rows to be displayed, because this mode displays a line of output per each column, so you&apos;ll get 10 lines for single row with 10 columns, then if you have 10 of such rows, you will get 100 lines of output (+1 extra line per each row, to separate rows from each other).</translation>
+ROW 模式适合没有太多行且想要查看完整值的需求。该模式会为每个列输出一行，所以只有 1 个数据行但有 10 个数据列的数据会输出 10 行；而如果有 10 个数据行，输出会产生 100 行（及每个数据行之间还会有一行间隔）。</translation>
     </message>
   </context>
   <context>
