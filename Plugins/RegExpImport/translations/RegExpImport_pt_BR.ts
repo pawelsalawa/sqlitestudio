27bb7eefb8466d3c109c3418fa83a4bf8a2ddbea
Paweł Salawa <pawelsalawa@gmail.com>
2021-11-19 00:03:04 +0100

    New translations RegExpImport.ts (Portuguese, Brazilian)

--- Plugins/RegExpImport/translations/RegExpImport_pt_BR.ts
+++ Plugins/RegExpImport/translations/RegExpImport_pt_BR.ts
@@ -6,32 +6,32 @@
     <message>
       <location filename="../regexpimport.cpp" line="37"/>
       <source>Text files (*.txt);;All files (*)</source>
-      <translation type="unfinished">Text files (*.txt);;All files (*)</translation>
+      <translation>Arquivos de texto (*.txt);;Todos os arquivos (*)</translation>
     </message>
     <message>
       <location filename="../regexpimport.cpp" line="53"/>
       <source>Cannot read file %1</source>
-      <translation type="unfinished">Cannot read file %1</translation>
+      <translation>Não é possível ler o arquivo %1</translation>
     </message>
     <message>
       <location filename="../regexpimport.cpp" line="161"/>
       <source>Enter the regular expression pattern.</source>
-      <translation type="unfinished">Enter the regular expression pattern.</translation>
+      <translation>Digite o padrão de expressão regular.</translation>
     </message>
     <message>
       <location filename="../regexpimport.cpp" line="169"/>
       <source>Invalid pattern: %1</source>
-      <translation type="unfinished">Invalid pattern: %1</translation>
+      <translation>Padrão inválido: %1</translation>
     </message>
     <message>
       <location filename="../regexpimport.cpp" line="189"/>
       <source>Requested capture index %1 is out of range.</source>
-      <translation type="unfinished">Requested capture index %1 is out of range.</translation>
+      <translation>O índice de captura solicitado %1 está fora do alcance.</translation>
     </message>
     <message>
       <location filename="../regexpimport.cpp" line="196"/>
       <source>&lt;p&gt;Requested capture group name &apos;%1&apos;, but it&apos;s not defined in the pattern: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</source>
-      <translation type="unfinished">&lt;p&gt;Requested capture group name &apos;%1&apos;, but it&apos;s not defined in the pattern: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</translation>
+      <translation>&lt;p&gt;Nome do grupo de captura requisitado &apos;%1&apos;, mas&apos;não definido no padrão: &lt;pre&gt;%2&lt;/pre&gt;&lt;/p&gt;</translation>
     </message>
   </context>
   <context>
@@ -39,48 +39,48 @@
     <message>
       <location filename="../regexpimport.ui" line="20"/>
       <source>Capture groups</source>
-      <translation type="unfinished">Capture groups</translation>
+      <translation>Grupos de captura</translation>
     </message>
     <message>
       <location filename="../regexpimport.ui" line="26"/>
       <source>Treat all RegExp capture groups as columns</source>
-      <translation type="unfinished">Treat all RegExp capture groups as columns</translation>
+      <translation>Tratar todos os grupos de captura RegExp como colunas</translation>
     </message>
     <message>
       <location filename="../regexpimport.ui" line="39"/>
       <source>Import only following groups:</source>
-      <translation type="unfinished">Import only following groups:</translation>
+      <translation>Importar apenas os seguintes grupos:</translation>
     </message>
     <message>
       <location filename="../regexpimport.ui" line="52"/>
       <source>&lt;p&gt;Enter comma separated list of capture group indexes. The 0 index refers to the entire matched string.&lt;/p&gt;
 &lt;p&gt;If you used named groups in the pattern, you can use names instead of indexes. You can mix indexes and names in this list.&lt;/p&gt;</source>
-      <translation type="unfinished">&lt;p&gt;Enter comma separated list of capture group indexes. The 0 index refers to the entire matched string.&lt;/p&gt;
-&lt;p&gt;If you used named groups in the pattern, you can use names instead of indexes. You can mix indexes and names in this list.&lt;/p&gt;</translation>
+      <translation>&lt;p&gt;Insira uma lista separada por vírgulas de índices de grupo de captura. O índice 0 refere-se a toda a string correspondente.&lt;/p&gt;
+&lt;p&gt;Se você usou grupos nomeados no padrão, você pode usar nomes ao invés de indexes. Você pode misturar índices e nomes nesta lista.&lt;/p&gt;</translation>
     </message>
     <message>
       <location filename="../regexpimport.ui" line="56"/>
       <source>Example: 1, 3, 4</source>
-      <translation type="unfinished">Example: 1, 3, 4</translation>
+      <translation>Exemplo: 1, 3, 4</translation>
     </message>
     <message>
       <location filename="../regexpimport.ui" line="69"/>
       <source>Pattern:</source>
-      <translation type="unfinished">Pattern:</translation>
+      <translation>Padrão:</translation>
     </message>
     <message>
       <location filename="../regexpimport.ui" line="76"/>
       <source>&lt;p&gt;Use Regular Expression groups to enclose parts of the expression that you want to import. If you want to use a group, that you don&apos;t want to import, then use &quot;import only following groups&quot; option below.
 
 You can use named groups and refer to them in group list below. To name a group use: &lt;pre&gt;(?&amp;lt;myGroupName&amp;gt;\s+\d+\s+)&lt;/pre&gt;&lt;/p&gt;</source>
-      <translation type="unfinished">&lt;p&gt;Use Regular Expression groups to enclose parts of the expression that you want to import. If you want to use a group, that you don&apos;t want to import, then use &quot;import only following groups&quot; option below.
+      <translation>&lt;p&gt;Use grupos de expressão regular para incluir partes da expressão que você deseja importar. Se você quiser usar um grupo, que você usa&apos;t para importar, em seguida, use &quot;importar apenas seguindo grupos&quot; opção abaixo.
 
-You can use named groups and refer to them in group list below. To name a group use: &lt;pre&gt;(?&amp;lt;myGroupName&amp;gt;\s+\d+\s+)&lt;/pre&gt;&lt;/p&gt;</translation>
+Você pode usar grupos nomeados e se referir a eles na lista de grupos abaixo. Para nomear um grupo, use: &lt;pre&gt;(?&amp;lt;myGroupName&amp;gt;\s+\d+\s+)&lt;/pre&gt;&lt;/p&gt;</translation>
     </message>
     <message>
       <location filename="../regexpimport.ui" line="81"/>
       <source>Example: (\d+)\s+((\d+)\w+)\s+(\w+)</source>
-      <translation type="unfinished">Example: (\d+)\s+((\d+)\w+)\s+(\w+)</translation>
+      <translation>Exemplo: (\d+)\s+((\d+)\w+)\s+(\w+)</translation>
     </message>
   </context>
 </TS>
