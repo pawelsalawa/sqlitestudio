1093a549459476be0c0040856b055fa617d41294
Paweł Salawa <pawelsalawa@gmail.com>
2021-11-18 23:48:25 +0100

    New translations DbSqliteCipher.ts (Portuguese, Brazilian)

--- Plugins/DbSqliteCipher/translations/DbSqliteCipher_pt_BR.ts
+++ Plugins/DbSqliteCipher/translations/DbSqliteCipher_pt_BR.ts
@@ -6,31 +6,31 @@
     <message>
       <location filename="../dbsqlitecipher.cpp" line="30"/>
       <source>Password (key)</source>
-      <translation type="unfinished">Password (key)</translation>
+      <translation>Senha (chave)</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="31"/>
       <source>Leave empty to create or connect to decrypted database.</source>
-      <translation type="unfinished">Leave empty to create or connect to decrypted database.</translation>
+      <translation>Deixe em branco para criar ou conectar ao banco de dados descriptografado.</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="32"/>
       <source>Encryption password</source>
-      <translation type="unfinished">Encryption password</translation>
+      <translation>Senha criptografada</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="37"/>
       <source>Cipher configuration (optional)</source>
-      <translation type="unfinished">Cipher configuration (optional)</translation>
+      <translation>Configuração de cifra (opcional)</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="38"/>
       <source>PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLCipher for details.</source>
-      <translation type="unfinished">PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
-They will be executed upon each opening of the database.
-See documentation for SQLCipher for details.</translation>
+      <translation>Declarações PRAGMA para personalizar a configuração SQLCipher, como iterações KDF, modo legado, etc.
+Eles serão executados após cada abertura do banco de dados.
+Consulte a documentação de SQLCipher para obter detalhes.</translation>
     </message>
   </context>
 </TS>
