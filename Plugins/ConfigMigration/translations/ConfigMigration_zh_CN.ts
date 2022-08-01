dc7a9b00bdbb2c65508ac775d7f96f7e77cacb25
Paweł Salawa <pawelsalawa@gmail.com>
2022-08-01 17:54:18 +0200

    New translations ConfigMigration.ts (Chinese Simplified)

--- Plugins/ConfigMigration/translations/ConfigMigration_zh_CN.ts
+++ Plugins/ConfigMigration/translations/ConfigMigration_zh_CN.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../configmigration.cpp" line="36"/>
       <source>A configuration from old SQLiteStudio 2.x.x has been detected. Would you like to migrate old settings into the current version? &lt;a href=&quot;%1&quot;&gt;Click here to do that&lt;/a&gt;.</source>
-      <translation>检测到旧的 SQLiteStudio 2.x.x 的配置。你想要将旧的设置迁移到当前版本吗？&lt;a href=&quot;%1&quot;&gt;点击这里进行迁移&lt;/a&gt;。</translation>
+      <translation>检测到旧的 SQLiteStudio 2.x.x 的配置。 你想要将旧的设置迁移到当前版本吗？ &lt;a href=&quot;%1&quot;&gt;点击这里进行迁移&lt;/a&gt;。</translation>
     </message>
     <message>
       <location filename="../configmigration.cpp" line="136"/>
@@ -84,32 +84,32 @@
     <message>
       <location filename="../configmigrationwizard.cpp" line="121"/>
       <source>Could not commit migrated data into new configuration file: %1</source>
-      <translation>无法将迁移的数据提交到新的配置文件：%1</translation>
+      <translation>无法将迁移的数据提交到新的配置文件： %1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="165"/>
       <source>Could not read bug reports history from old configuration file in order to migrate it: %1</source>
-      <translation>无法从旧的配置文件读取错误报告历史记录来进行迁移：%1</translation>
+      <translation>无法从旧的配置文件读取错误报告历史记录来进行迁移： %1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="182"/>
       <source>Could not insert a bug reports history entry into new configuration file: %1</source>
-      <translation>无法将错误报告历史记录插入到新的配置文件：%1</translation>
+      <translation>无法将错误报告历史记录插入到新的配置文件： %1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="203"/>
       <source>Could not read database list from old configuration file in order to migrate it: %1</source>
-      <translation>无法从旧的配置文件读取数据库列表来进行迁移：%1</translation>
+      <translation>无法从旧的配置文件读取数据库列表来进行迁移： %1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="217"/>
       <source>Could not query for available order for containing group in new configuration file in order to migrate the database list: %1</source>
-      <translation>无法在新配置文件中查询包含组的可用顺序以迁移数据库列表：%1</translation>
+      <translation>无法在新配置文件中查询包含组的可用顺序以迁移数据库列表: %1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="228"/>
       <source>Could not create containing group in new configuration file in order to migrate the database list: %1</source>
-      <translation>无法在新配置文件中创建包含组以迁移数据库列表：%1</translation>
+      <translation>无法在新配置文件中创建包含组以迁移数据库列表: %1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="249"/>
@@ -119,7 +119,7 @@
     <message>
       <location filename="../configmigrationwizard.cpp" line="261"/>
       <source>Could not query for available order for next database in new configuration file in order to migrate the database list: %1</source>
-      <translation>无法在新配置文件中查询下一个数据库的可用顺序以迁移数据库列表：%1</translation>
+      <translation>无法在新配置文件中查询下一个数据库的可用顺序以迁移数据库列表:%1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="272"/>
@@ -129,22 +129,22 @@
     <message>
       <location filename="../configmigrationwizard.cpp" line="290"/>
       <source>Could not read function list from old configuration file in order to migrate it: %1</source>
-      <translation>无法从旧配置文件读取函数列表来进行迁移：%1</translation>
+      <translation>无法从旧配置文件读取函数列表来进行迁移： %1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="325"/>
       <source>Could not read SQL queries history from old configuration file in order to migrate it: %1</source>
-      <translation>无法从旧配置文件读取 SQL 查询历史来进行迁移：%1</translation>
+      <translation>无法从旧配置文件读取 SQL 查询历史来进行迁移： %1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="332"/>
       <source>Could not read next ID for SQL queries history in new configuration file: %1</source>
-      <translation>无法在新配置文件中读取 SQL 查询历史的下一个 ID：%1</translation>
+      <translation>无法在新配置文件中读取 SQL 查询历史的下一个ID： %1</translation>
     </message>
     <message>
       <location filename="../configmigrationwizard.cpp" line="348"/>
       <source>Could not insert SQL history entry into new configuration file: %1</source>
-      <translation>无法将 SQL 历史条目插入到新的配置文件：%1</translation>
+      <translation>无法将 SQL 历史条目插入到新的配置文件： %1</translation>
     </message>
   </context>
 </TS>
