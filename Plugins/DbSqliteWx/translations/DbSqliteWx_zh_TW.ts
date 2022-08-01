dd214b5120fa80cd148457597e46185c9841b048
Paweł Salawa <pawelsalawa@gmail.com>
2022-08-01 17:54:54 +0200

    New translations DbSqliteWx.ts (Chinese Traditional)

--- Plugins/DbSqliteWx/translations/DbSqliteWx_zh_TW.ts
+++ Plugins/DbSqliteWx/translations/DbSqliteWx_zh_TW.ts
@@ -6,41 +6,41 @@
     <message>
       <location filename="../dbsqlitewx.cpp" line="30"/>
       <source>Password (key)</source>
-      <translation>密碼 (金鑰)</translation>
+      <translation type="unfinished">Password (key)</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="31"/>
       <source>Leave empty to create or connect to decrypted database.</source>
-      <translation>留空則建立或連線到已解密的資料庫。</translation>
+      <translation type="unfinished">Leave empty to create or connect to decrypted database.</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="32"/>
       <source>Encryption password</source>
-      <translation>加密密碼</translation>
+      <translation type="unfinished">Encryption password</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="38"/>
       <source>Cipher</source>
-      <translation>加密演算法</translation>
+      <translation type="unfinished">Cipher</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="39"/>
       <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-      <translation>用來加密該資料庫的加密演算法。</translation>
+      <translation type="unfinished">Cipher determines encryption algorithm used to encrypt the database.</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="47"/>
       <source>Cipher configuration (optional)</source>
-      <translation>加密演算法設定檔 (可選)</translation>
+      <translation type="unfinished">Cipher configuration (optional)</translation>
     </message>
     <message>
       <location filename="../dbsqlitewx.cpp" line="48"/>
       <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-      <translation>用於自訂 SQLite3 Multiple Ciphers 設定檔的 PRAGMA 語句，如 KDF 迭代、傳統模式等。
-將在每次開啟資料庫時執行。
-細節請參考 SQLite3 Multiple Ciphers 文件。</translation>
+      <translation type="unfinished">PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+They will be executed upon each opening of the database.
+See documentation for SQLite3 Multiple Ciphers for details.</translation>
     </message>
   </context>
 </TS>
