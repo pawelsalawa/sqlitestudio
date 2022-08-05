80c791bb6b6e0cae9aff789c3f234b80a2d1d27c
Paweł Salawa <pawelsalawa@gmail.com>
2022-08-05 14:50:03 +0200

    New translations XmlExport.ts (Chinese Traditional)

--- Plugins/XmlExport/translations/XmlExport_zh_TW.ts
+++ Plugins/XmlExport/translations/XmlExport_zh_TW.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../xmlexport.cpp" line="38"/>
       <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
-      <translation type="unfinished">Enter the namespace to use (for example: http://my.namespace.org)</translation>
+      <translation>輸入要使用的名稱空間 (例如：http://my.namespace.org)</translation>
     </message>
   </context>
   <context>
@@ -14,57 +14,57 @@
     <message>
       <location filename="../XmlExport.ui" line="20"/>
       <source>Output format</source>
-      <translation type="unfinished">Output format</translation>
+      <translation>輸出格式</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="26"/>
       <source>Format document (new lines, indentation)</source>
-      <translation type="unfinished">Format document (new lines, indentation)</translation>
+      <translation>格式化的文字 (新行，縮排)</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="42"/>
       <source>Compress (everything in one line)</source>
-      <translation type="unfinished">Compress (everything in one line)</translation>
+      <translation>壓縮 (單行檔案)</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="58"/>
       <source>Special characters escaping</source>
-      <translation type="unfinished">Special characters escaping</translation>
+      <translation>特殊字元轉義</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="64"/>
       <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
-      <translation type="unfinished">&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</translation>
+      <translation>&lt;p&gt;&amp;&amp; 符號將用於較短的值，CDATA 將用於較長的值。這僅用於需要字元轉義的值。其他值將按原樣匯出。&lt;/p&gt;</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="67"/>
       <source>Use CDATA and ampersands</source>
-      <translation type="unfinished">Use CDATA and ampersands</translation>
+      <translation>使用 CDATA 及 &amp;&amp; 符號</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="83"/>
       <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
-      <translation type="unfinished">&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</translation>
+      <translation>&lt;p&gt;每個需要字元轉義的值都將包裹在 CDATA 塊中。&lt;/p&gt;</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="86"/>
       <source>Always use CDATA</source>
-      <translation type="unfinished">Always use CDATA</translation>
+      <translation>總是使用 CDATA</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="99"/>
       <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
-      <translation type="unfinished">&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</translation>
+      <translation>&lt;p&gt;每個需要轉義的字元都將使用 &amp;&amp; 符號及序列代替。不會使用 CDATA 塊。&lt;/p&gt;</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="102"/>
       <source>Always use ampersand</source>
-      <translation type="unfinished">Always use ampersand</translation>
+      <translation>總是使用 &amp;&amp; 符號</translation>
     </message>
     <message>
       <location filename="../XmlExport.ui" line="118"/>
       <source>Define XML namespace</source>
-      <translation type="unfinished">Define XML namespace</translation>
+      <translation>定義 XML 名稱空間</translation>
     </message>
   </context>
 </TS>
