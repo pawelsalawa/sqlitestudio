b066860e936075931f85a911bb319eba0f7c8359
Paweł Salawa <pawelsalawa@gmail.com>
2022-08-05 14:35:11 +0200

    New translations DbSqliteCipher.ts (Chinese Traditional)

--- Plugins/DbSqliteCipher/translations/DbSqliteCipher_zh_TW.ts
+++ Plugins/DbSqliteCipher/translations/DbSqliteCipher_zh_TW.ts
@@ -6,31 +6,31 @@
     <message>
       <location filename="../dbsqlitecipher.cpp" line="30"/>
       <source>Password (key)</source>
-      <translation type="unfinished">Password (key)</translation>
+      <translation>密碼 (金鑰)</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="31"/>
       <source>Leave empty to create or connect to decrypted database.</source>
-      <translation type="unfinished">Leave empty to create or connect to decrypted database.</translation>
+      <translation>留空則建立或連線到已解密的資料庫。</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="32"/>
       <source>Encryption password</source>
-      <translation type="unfinished">Encryption password</translation>
+      <translation>加密密碼</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="37"/>
       <source>Cipher configuration (optional)</source>
-      <translation type="unfinished">Cipher configuration (optional)</translation>
+      <translation>加密演算法設定檔 (可選)</translation>
     </message>
     <message>
       <location filename="../dbsqlitecipher.cpp" line="38"/>
       <source>PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLCipher for details.</source>
-      <translation type="unfinished">PRAGMA statements to customize SQLCipher configuration, such as KDF iterations, legacy mode, etc.
-They will be executed upon each opening of the database.
-See documentation for SQLCipher for details.</translation>
+      <translation>用於自訂 SQLCiper 設定檔的 PRAGMA 語句，如 KDF 迭代、傳統模式等。
+將在每次開啟資料庫時執行。
+細節請參考 SQLCipher 文件。</translation>
     </message>
   </context>
 </TS>
