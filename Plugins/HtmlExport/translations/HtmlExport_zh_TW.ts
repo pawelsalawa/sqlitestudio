62cb39d3f792f785bc824e3804bcf36feba612c0
Paweł Salawa <pawelsalawa@gmail.com>
2022-08-05 14:35:10 +0200

    New translations HtmlExport.ts (Chinese Traditional)

--- Plugins/HtmlExport/translations/HtmlExport_zh_TW.ts
+++ Plugins/HtmlExport/translations/HtmlExport_zh_TW.ts
@@ -6,120 +6,120 @@
     <message>
       <location filename="../htmlexport.cpp" line="46"/>
       <source>SQL query results</source>
-      <translation type="unfinished">SQL query results</translation>
+      <translation>SQL 查詢結果</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="78"/>
       <location filename="../htmlexport.cpp" line="155"/>
       <source>no type</source>
-      <translation type="unfinished">no type</translation>
+      <translation>無型別</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="115"/>
       <location filename="../htmlexport.cpp" line="243"/>
       <source>Exported table: %1</source>
-      <translation type="unfinished">Exported table: %1</translation>
+      <translation>匯出的表：%1</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="127"/>
       <location filename="../htmlexport.cpp" line="255"/>
       <source>Table: %1</source>
-      <translation type="unfinished">Table: %1</translation>
+      <translation>表：%1</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="255"/>
       <source>virtual</source>
-      <translation type="unfinished">virtual</translation>
+      <translation>虛擬</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="308"/>
       <source>Exported database: %1</source>
-      <translation type="unfinished">Exported database: %1</translation>
+      <translation>匯出的資料庫：%1</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="324"/>
       <source>Index: %1</source>
-      <translation type="unfinished">Index: %1</translation>
+      <translation>索引: %1</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="330"/>
       <source>For table:</source>
-      <translation type="unfinished">For table:</translation>
+      <translation>表：</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="337"/>
       <source>Unique:</source>
-      <translation type="unfinished">Unique:</translation>
+      <translation>唯一：</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="338"/>
       <source>Yes</source>
-      <translation type="unfinished">Yes</translation>
+      <translation>是</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="338"/>
       <source>No</source>
-      <translation type="unfinished">No</translation>
+      <translation>否</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="344"/>
       <source>Column</source>
-      <translation type="unfinished">Column</translation>
+      <translation>列</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="345"/>
       <source>Collating</source>
-      <translation type="unfinished">Collating</translation>
+      <translation>字元序</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="346"/>
       <source>Sort order</source>
-      <translation type="unfinished">Sort order</translation>
+      <translation>排序方式</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="380"/>
       <source>Trigger: %1</source>
-      <translation type="unfinished">Trigger: %1</translation>
+      <translation>觸發器：%1</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="386"/>
       <source>Activated:</source>
-      <translation type="unfinished">Activated:</translation>
+      <translation>啟用時間：</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="394"/>
       <source>Action:</source>
-      <translation type="unfinished">Action:</translation>
+      <translation>操作：</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="401"/>
       <source>On view:</source>
-      <translation type="unfinished">On view:</translation>
+      <translation>於檢視：</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="403"/>
       <source>On table:</source>
-      <translation type="unfinished">On table:</translation>
+      <translation>於表：</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="414"/>
       <source>Activate condition:</source>
-      <translation type="unfinished">Activate condition:</translation>
+      <translation>啟用條件：</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="421"/>
       <source>Code executed:</source>
-      <translation type="unfinished">Code executed:</translation>
+      <translation>執行程式碼：</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="455"/>
       <source>View: %1</source>
-      <translation type="unfinished">View: %1</translation>
+      <translation>檢視：%1</translation>
     </message>
     <message>
       <location filename="../htmlexport.cpp" line="480"/>
       <source>Document generated by SQLiteStudio v%1 on %2</source>
-      <translation type="unfinished">Document generated by SQLiteStudio v%1 on %2</translation>
+      <translation>由 SQLiteStudio v%1 在 %2 生成的文件</translation>
     </message>
   </context>
   <context>
@@ -127,47 +127,47 @@
     <message>
       <location filename="../htmlexport.ui" line="36"/>
       <source>Maximum number of characters per cell:</source>
-      <translation type="unfinished">Maximum number of characters per cell:</translation>
+      <translation>每個單元格的最大字元數：</translation>
     </message>
     <message>
       <location filename="../htmlexport.ui" line="43"/>
       <source>Include data types in first row</source>
-      <translation type="unfinished">Include data types in first row</translation>
+      <translation>第一行包含資料型別</translation>
     </message>
     <message>
       <location filename="../htmlexport.ui" line="53"/>
       <source>Column names as first row</source>
-      <translation type="unfinished">Column names as first row</translation>
+      <translation>列名作為首行</translation>
     </message>
     <message>
       <location filename="../htmlexport.ui" line="63"/>
       <source>Row numbers as first column</source>
-      <translation type="unfinished">Row numbers as first column</translation>
+      <translation>行號作為首列</translation>
     </message>
     <message>
       <location filename="../htmlexport.ui" line="73"/>
       <source>Output format</source>
-      <translation type="unfinished">Output format</translation>
+      <translation>輸出格式</translation>
     </message>
     <message>
       <location filename="../htmlexport.ui" line="79"/>
       <source>Format document (new lines, indentation)</source>
-      <translation type="unfinished">Format document (new lines, indentation)</translation>
+      <translation>格式化的文件 (新行，縮排)</translation>
     </message>
     <message>
       <location filename="../htmlexport.ui" line="92"/>
       <source>Compress (everything in one line)</source>
-      <translation type="unfinished">Compress (everything in one line)</translation>
+      <translation>壓縮 (單行檔案)</translation>
     </message>
     <message>
       <location filename="../htmlexport.ui" line="108"/>
       <source>&lt;p&gt;When enabled, HTML characters such as &amp;lt;, &amp;gt; and &amp;amp; are not escaped in exported values. This allows you for example to export hyper-link enabled documents, but it also may result in incorrect HTML document (unmatched pairs of &amp;lt; and &amp;gt; characters). Be warned.&lt;/p&gt;</source>
-      <translation>&lt;p&gt;When enabled, HTML characters such as &amp;lt;, &amp;gt; and &amp; are not escaped in exported values. This allows you for example to export hyper-link enabled documents, but it also may result in incorrect HTML document (unmatched pairs of &amp;lt; and &amp;gt; characters). Be warned.&lt;/p&gt;</translation>
+      <translation>&lt;p&gt;如果啟用，匯出值中不會轉義諸如 &amp;lt;、&amp;gt; 和 &amp; 等 HTML 字元。請小心，這使您能匯出有超連結的文件，但也可能導致不正確的 HTML 文件 (不配對的 &amp;lt; 和 &amp;gt; 字元)。&lt;/p&gt;</translation>
     </message>
     <message>
       <location filename="../htmlexport.ui" line="111"/>
       <source>Don&apos;t escape HTML characters</source>
-      <translation type="unfinished">Don&apos;t escape HTML characters</translation>
+      <translation>不轉義 HTML 字元</translation>
     </message>
   </context>
 </TS>
