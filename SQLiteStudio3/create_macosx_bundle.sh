dfda5f6286d7358dfdc18d34c2f491ee1c9745a6
Paweł Salawa <pawelsalawa@gmail.com>
2022-09-01 21:36:40 +0200

    #513 Added minimal python distro package

--- SQLiteStudio3/create_macosx_bundle.sh
+++ SQLiteStudio3/create_macosx_bundle.sh
@@ -38,6 +38,9 @@ cp -RP styles/* SQLiteStudio.app/Contents/PlugIns/styles
 
 cp -RP lib*SQLiteStudio*.dylib SQLiteStudio.app/Contents/Frameworks
 
+cp -RP libpython*.dylib SQLiteStudio.app/Contents/Frameworks
+cp -RP python*.zip SQLiteStudio.app/Contents/Frameworks
+
 # CLI paths
 qtcore_path=`otool -L sqlitestudiocli | awk '/QtCore/ {print $1;}'`
 new_qtcore_path="@rpath/QtCore.framework/Versions/5/QtCore"
