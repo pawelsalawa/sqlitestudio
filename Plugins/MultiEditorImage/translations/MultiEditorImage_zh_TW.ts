6083bc4bd4e8b63ec5d2fe9d648d51956d281573
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-24 23:25:43 +0100

    New translations MultiEditorImage.ts (Chinese Traditional)

--- Plugins/MultiEditorImage/translations/MultiEditorImage_zh_TW.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_zh_TW.ts
@@ -1,80 +1,80 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh-TW" sourcelanguage="en">
-<context>
+  <context>
     <name>MultiEditorImage</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="25"/>
-        <source>Load from file</source>
-        <translation>從檔案載入</translation>
+      <location filename="../multieditorimage.cpp" line="25"/>
+      <source>Load from file</source>
+      <translation>從檔案載入</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="26"/>
-        <source>Store in a file</source>
-        <translation>儲存為檔案</translation>
+      <location filename="../multieditorimage.cpp" line="26"/>
+      <source>Store in a file</source>
+      <translation>儲存為檔案</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="27"/>
-        <source>Zoom in by 25%</source>
-        <translation>放大 25%</translation>
+      <location filename="../multieditorimage.cpp" line="27"/>
+      <source>Zoom in by 25%</source>
+      <translation>放大 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="28"/>
-        <source>Zoom out by 25%</source>
-        <translation>縮小 25%</translation>
+      <location filename="../multieditorimage.cpp" line="28"/>
+      <source>Zoom out by 25%</source>
+      <translation>縮小 25%</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="29"/>
-        <source>Reset zoom</source>
-        <translation>重置縮放</translation>
+      <location filename="../multieditorimage.cpp" line="29"/>
+      <source>Reset zoom</source>
+      <translation>重置縮放</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="103"/>
-        <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
-        <translation>圖片 (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;所有檔案 (*)</translation>
+      <location filename="../multieditorimage.cpp" line="103"/>
+      <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
+      <translation>圖片 (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;所有檔案 (*)</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="104"/>
-        <source>Open image</source>
-        <translation>開啟圖片</translation>
+      <location filename="../multieditorimage.cpp" line="104"/>
+      <source>Open image</source>
+      <translation>開啟圖片</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="113"/>
-        <source>Could not open file %1 for reading.</source>
-        <translation>無法以讀模式開啟檔案 %1。</translation>
+      <location filename="../multieditorimage.cpp" line="113"/>
+      <source>Could not open file %1 for reading.</source>
+      <translation>無法以讀模式開啟檔案 %1。</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="133"/>
-        <source>All files (*)</source>
-        <translation>所有檔案 (*)</translation>
+      <location filename="../multieditorimage.cpp" line="133"/>
+      <source>All files (*)</source>
+      <translation>所有檔案 (*)</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="135"/>
-        <source>Save image</source>
-        <translation>儲存圖片</translation>
+      <location filename="../multieditorimage.cpp" line="135"/>
+      <source>Save image</source>
+      <translation>儲存圖片</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="154"/>
-        <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
-        <translation>試圖以與原始格式 (%2) 不同的格式 (%1) 儲存影象，但應用程式轉換失敗。影象將以未改變的格式 (%3) 儲存在給定名稱下 (%4)</translation>
+      <location filename="../multieditorimage.cpp" line="154"/>
+      <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
+      <translation>試圖以與原始格式 (%2) 不同的格式 (%1) 儲存影象，但應用程式轉換失敗。影象將以未改變的格式 (%3) 儲存在給定名稱下 (%4)</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="165"/>
-        <source>Could not open file %1 for writting.</source>
-        <translation>無法以寫模式開啟檔案 %1。</translation>
+      <location filename="../multieditorimage.cpp" line="165"/>
+      <source>Could not open file %1 for writting.</source>
+      <translation>無法以寫模式開啟檔案 %1。</translation>
     </message>
     <message>
-        <location filename="../multieditorimage.cpp" line="170"/>
-        <source>Could not write image into the file %1</source>
-        <translation>無法將影象寫入檔案 %1</translation>
+      <location filename="../multieditorimage.cpp" line="170"/>
+      <source>Could not write image into the file %1</source>
+      <translation>無法將影象寫入檔案 %1</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MultiEditorImagePlugin</name>
     <message>
-        <location filename="../multieditorimage.cpp" line="263"/>
-        <source>Image</source>
-        <translation>圖片</translation>
+      <location filename="../multieditorimage.cpp" line="263"/>
+      <source>Image</source>
+      <translation>圖片</translation>
     </message>
-</context>
+  </context>
 </TS>
