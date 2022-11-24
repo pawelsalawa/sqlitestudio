7e1f99467fe42fa12a7d4dd060a34808e918ddbe
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-24 23:42:23 +0100

    New translations ScriptingTcl.ts (Turkish)

--- Plugins/ScriptingTcl/translations/ScriptingTcl_tr_TR.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_tr_TR.ts
@@ -1,31 +1,23 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="tr" sourcelanguage="en">
-<context>
+  <context>
     <name>ScriptingTcl</name>
     <message>
-        <location filename="../scriptingtcl.cpp" line="429"/>
-        <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
-        <translation>Tcl&apos;s &apos;%1&apos; komutu sonucunda, uygun bir veritabanı bulunmuyor.</translation>
+      <location filename="../scriptingtcl.cpp" line="411"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation>Tcl&apos;s &apos;%1&apos; komutu sonucunda, uygun bir veritabanı bulunmuyor.</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="451"/>
-        <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
-        <translation type="unfinished"></translation>
+      <location filename="../scriptingtcl.cpp" line="433"/>
+      <source>Invalid &apos;%1&apos; command sytax. Should be: %2</source>
+      <translation>Geçersiz &apos;%1&apos; komut söz dizimi. %2 şeklinde olmalı</translation>
     </message>
     <message>
-        <location filename="../scriptingtcl.cpp" line="463"/>
-        <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
-        <translation type="unfinished"></translation>
+      <location filename="../scriptingtcl.cpp" line="445"/>
+      <location filename="../scriptingtcl.cpp" line="576"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation>Tcl&apos;s&apos; &apos;%1&apos;komutunda hata komut: %2</translation>
     </message>
-    <message>
-        <source>Invalid &apos;%1&apos; command sytax. Should be: %2</source>
-        <translation type="vanished">Geçersiz &apos;%1&apos; komut söz dizimi. %2 şeklinde olmalı</translation>
-    </message>
-    <message>
-        <location filename="../scriptingtcl.cpp" line="594"/>
-        <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
-        <translation>Tcl&apos;s&apos; &apos;%1&apos;komutunda hata komut: %2</translation>
-    </message>
-</context>
+  </context>
 </TS>
