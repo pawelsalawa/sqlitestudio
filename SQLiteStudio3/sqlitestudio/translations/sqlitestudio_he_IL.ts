9786aaf31c2fd90a844fd541578816876ceac949
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-24 23:47:31 +0100

    New translations sqlitestudio.ts (Hebrew)

--- SQLiteStudio3/sqlitestudio/translations/sqlitestudio_he_IL.ts
+++ SQLiteStudio3/sqlitestudio/translations/sqlitestudio_he_IL.ts
@@ -1,87 +1,87 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="he" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="48"/>
-        <source>GUI interface to SQLiteStudio, a SQLite manager.</source>
-        <translation>מנשק משתמש גרפי SQLiteStudio, תכנת ניהול SQLite.</translation>
+      <location filename="../main.cpp" line="51"/>
+      <source>GUI interface to SQLiteStudio, a SQLite manager.</source>
+      <translation>מנשק משתמש גרפי SQLiteStudio, תכנת ניהול SQLite.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation>אפשור הודעות ניפוי תקלים בעמדת־הבקרה (נגיש באמצעות מקש F12).</translation>
+      <location filename="../main.cpp" line="55"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>אפשור הודעות ניפוי תקלים בעמדת־הבקרה (נגיש באמצעות מקש F12).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="53"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation>הפנית הודעות ניפוי תקלים לפלט תקני (אילוץ מצב ניפוי תקלים).</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>הפנית הודעות ניפוי תקלים לפלט תקני (אילוץ מצב ניפוי תקלים).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="54"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation>הפנית הודעות ניפוי תקלים לקובץ נתון (אילוץ מצב ניפוי תקלים).</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>הפנית הודעות ניפוי תקלים לקובץ נתון (אילוץ מצב ניפוי תקלים).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="54"/>
-        <source>log file</source>
-        <translation>קובץ יומן</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>log file</source>
+      <translation>קובץ יומן</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="55"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation>אפשור הודעות ניפוי תקלים ניתוח תחביר Lemon לסייען קוד SQL.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>אפשור הודעות ניפוי תקלים ניתוח תחביר Lemon לסייען קוד SQL.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation>אפשור ניפוי תקלים לכל שאילתת SQL שנשלחת למסד נתונים כלשהו.</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>אפשור ניפוי תקלים לכל שאילתת SQL שנשלחת למסד נתונים כלשהו.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation>הגבלת הודעות שאילתות SQL למסד הנתונים הנתון בלבד.</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>הגבלת הודעות שאילתות SQL למסד הנתונים הנתון בלבד.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>database</source>
-        <translation>מסד נתונים</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>database</source>
+      <translation>מסד נתונים</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Enables debugging of SQLiteStudio&apos;s query executor.</source>
-        <translation>אפשור ניפוי תקלים מריץ השאילתות של SQLiteStudio.</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Enables debugging of SQLiteStudio&apos;s query executor.</source>
+      <translation>אפשור ניפוי תקלים מריץ השאילתות של SQLiteStudio.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Lists plugins installed in the SQLiteStudio and quits.</source>
-        <translation>הצגת רשימת מתקעים מותקנים ב־SQLiteStudio ויציאה.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Lists plugins installed in the SQLiteStudio and quits.</source>
+      <translation>הצגת רשימת מתקעים מותקנים ב־SQLiteStudio ויציאה.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation>הפניה לקובץ התצורה הראשי. פרטים נוספים, ניתן למצוא במדריך למשתמש בעמוד הויקי.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>הפניה לקובץ התצורה הראשי. פרטים נוספים, ניתן למצוא במדריך למשתמש בעמוד הויקי.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>SQLiteStudio settings file</source>
-        <translation>קובץ הגדרות SQLiteStudio</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>SQLiteStudio settings file</source>
+      <translation>קובץ הגדרות SQLiteStudio</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="71"/>
-        <source>file</source>
-        <translation>קובץ</translation>
+      <location filename="../main.cpp" line="74"/>
+      <source>file</source>
+      <translation>קובץ</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="71"/>
-        <source>Database file to open</source>
-        <translation>קובץ מסד נתונים לפתיחה</translation>
+      <location filename="../main.cpp" line="74"/>
+      <source>Database file to open</source>
+      <translation>קובץ מסד נתונים לפתיחה</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="139"/>
-        <source>Select configuration directory</source>
-        <translation>בחירת מחיצת תצור</translation>
+      <location filename="../main.cpp" line="142"/>
+      <source>Select configuration directory</source>
+      <translation>בחירת מחיצת תצור</translation>
     </message>
-</context>
+  </context>
 </TS>
