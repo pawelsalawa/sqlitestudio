fe9d0ed82dcd7b36a887c9de156be12f719ebdd4
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-26 23:45:11 +0100

    New translations PdfExport.ts (Slovak)

--- Plugins/PdfExport/translations/PdfExport_sk_SK.ts
+++ Plugins/PdfExport/translations/PdfExport_sk_SK.ts
@@ -4,164 +4,164 @@
   <context>
     <name>PdfExport</name>
     <message>
-      <location filename="../pdfexport.cpp" line="28"/>
+      <location filename="../pdfexport.cpp" line="33"/>
       <source>SQLiteStudio v%1</source>
       <translation type="unfinished">SQLiteStudio v%1</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="61"/>
+      <location filename="../pdfexport.cpp" line="66"/>
       <source>SQL query results</source>
       <translation>Výsledky SQL dotazu</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="90"/>
-      <location filename="../pdfexport.cpp" line="144"/>
+      <location filename="../pdfexport.cpp" line="95"/>
+      <location filename="../pdfexport.cpp" line="149"/>
       <source>Exported table: %1</source>
       <translation>Vyexportovaná tabuľka: %1</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="93"/>
-      <location filename="../pdfexport.cpp" line="159"/>
+      <location filename="../pdfexport.cpp" line="98"/>
+      <location filename="../pdfexport.cpp" line="164"/>
       <source>Table: %1</source>
       <translation>Tabuľka: %1</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="95"/>
-      <location filename="../pdfexport.cpp" line="231"/>
+      <location filename="../pdfexport.cpp" line="100"/>
+      <location filename="../pdfexport.cpp" line="236"/>
       <source>Column</source>
       <translation>Stĺpec</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="95"/>
+      <location filename="../pdfexport.cpp" line="100"/>
       <source>Data type</source>
       <translation>Dátový typ</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="95"/>
+      <location filename="../pdfexport.cpp" line="100"/>
       <source>Constraints</source>
       <translation>Obmedzenia</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="126"/>
+      <location filename="../pdfexport.cpp" line="131"/>
       <source>Global table constraints</source>
       <translation>Globálne tabuľkové obmedzenia</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="215"/>
+      <location filename="../pdfexport.cpp" line="220"/>
       <source>Exported database: %1</source>
       <translation>Vyexportovaná databáza: %1</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="223"/>
+      <location filename="../pdfexport.cpp" line="228"/>
       <source>Index: %1</source>
       <translation type="unfinished">Index: %1</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="225"/>
+      <location filename="../pdfexport.cpp" line="230"/>
       <source>Property</source>
       <comment>index header</comment>
       <translation>Typ</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="225"/>
+      <location filename="../pdfexport.cpp" line="230"/>
       <source>Value</source>
       <comment>index header</comment>
       <translation>Hodnota</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="228"/>
+      <location filename="../pdfexport.cpp" line="233"/>
       <source>Indexed table</source>
       <translation>Indexovaná tabuľka</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="229"/>
+      <location filename="../pdfexport.cpp" line="234"/>
       <source>Unique index</source>
       <translation>Jedinečný index</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="229"/>
+      <location filename="../pdfexport.cpp" line="234"/>
       <source>Yes</source>
       <translation>Áno</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="229"/>
+      <location filename="../pdfexport.cpp" line="234"/>
       <source>No</source>
       <translation>Nie</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="231"/>
+      <location filename="../pdfexport.cpp" line="236"/>
       <source>Collation</source>
       <translation>Porovnávanie</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="231"/>
+      <location filename="../pdfexport.cpp" line="236"/>
       <source>Sort order</source>
       <translation>Zoradenie</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="247"/>
+      <location filename="../pdfexport.cpp" line="252"/>
       <source>Partial index condition</source>
       <translation>Podmienka parciálneho indexu</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="261"/>
+      <location filename="../pdfexport.cpp" line="266"/>
       <source>Trigger: %1</source>
       <translation>Spúšťač: %1</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="263"/>
+      <location filename="../pdfexport.cpp" line="268"/>
       <source>Property</source>
       <comment>trigger header</comment>
       <translation>Typ</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="263"/>
+      <location filename="../pdfexport.cpp" line="268"/>
       <source>Value</source>
       <comment>trigger header</comment>
       <translation>Hodnota</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="265"/>
+      <location filename="../pdfexport.cpp" line="270"/>
       <source>Activation time</source>
       <translation>Aktivačný čas</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="268"/>
+      <location filename="../pdfexport.cpp" line="273"/>
       <source>For action</source>
       <translation>Pre akciu</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="272"/>
+      <location filename="../pdfexport.cpp" line="277"/>
       <source>On view</source>
       <translation>Na pohľade</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="274"/>
+      <location filename="../pdfexport.cpp" line="279"/>
       <source>On table</source>
       <translation>Na tabuľke</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="279"/>
+      <location filename="../pdfexport.cpp" line="284"/>
       <source>Activation condition</source>
       <translation>Aktivačná podmienka</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="285"/>
+      <location filename="../pdfexport.cpp" line="290"/>
       <source>Code executed</source>
       <translation>Vykonaný kód</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="297"/>
+      <location filename="../pdfexport.cpp" line="302"/>
       <source>View: %1</source>
       <translation>Pohľad: %1</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="298"/>
+      <location filename="../pdfexport.cpp" line="303"/>
       <source>Query:</source>
       <translation>Dotaz:</translation>
     </message>
     <message>
-      <location filename="../pdfexport.cpp" line="654"/>
+      <location filename="../pdfexport.cpp" line="659"/>
       <source>Document generated with SQLiteStudio v%1</source>
       <translation>Dokument vygenerovaný programom SQLiteStudio v%1</translation>
     </message>
