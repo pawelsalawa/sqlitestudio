06a35d70ef849f04893ed8e0834cb3f81de797c8
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-26 23:48:19 +0100

    New translations ScriptingPython.ts (Italian)

--- Plugins/ScriptingPython/translations/ScriptingPython_it_IT.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_it_IT.ts
@@ -4,27 +4,27 @@
   <context>
     <name>QObject</name>
     <message>
-      <location filename="../scriptingpython.cpp" line="466"/>
+      <location filename="../scriptingpython.cpp" line="484"/>
       <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
       <translation>Uso non valido della funzione %1. Atteso %2 argomenti, ma ottenuto %3.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="475"/>
+      <location filename="../scriptingpython.cpp" line="493"/>
       <source>Unknown error from function %1.</source>
       <translation>Errore sconosciuto dalla funzione %1.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="516"/>
+      <location filename="../scriptingpython.cpp" line="534"/>
       <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
       <translation>Impossibile calcolare la rappresentazione della stringa dell'oggetto Python passato come argomento alla funzione %1.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="532"/>
+      <location filename="../scriptingpython.cpp" line="550"/>
       <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
       <translation>Impossibile trovare il contesto di esecuzione per la funzione %1. Questo è un bug del plugin Python. Si prega di segnalarlo.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="557"/>
+      <location filename="../scriptingpython.cpp" line="575"/>
       <source>Error from Python function %1: %2</source>
       <translation>Errore dalla funzione Python %1: %2</translation>
     </message>
