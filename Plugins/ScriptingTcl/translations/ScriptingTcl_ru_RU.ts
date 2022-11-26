b7f57834ca661099f0e695236dfe48e78a270946
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-26 23:46:49 +0100

    New translations ScriptingTcl.ts (Russian)

--- Plugins/ScriptingTcl/translations/ScriptingTcl_ru_RU.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_ru_RU.ts
@@ -4,18 +4,22 @@
   <context>
     <name>ScriptingTcl</name>
     <message>
-      <location filename="../scriptingtcl.cpp" line="411"/>
+      <location filename="../scriptingtcl.cpp" line="429"/>
       <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
       <translation>При вызове команды Tcl %1 в текущем контексте нет доступных баз данных.</translation>
     </message>
     <message>
-      <location filename="../scriptingtcl.cpp" line="433"/>
-      <source>Invalid &apos;%1&apos; command sytax. Should be: %2</source>
-      <translation>Некорректный синтаксис команды &apos;%1&apos;. Должно быть: %2</translation>
+      <location filename="../scriptingtcl.cpp" line="451"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation type="unfinished">Invalid &apos;%1&apos; command syntax. Should be: %2</translation>
     </message>
     <message>
-      <location filename="../scriptingtcl.cpp" line="445"/>
-      <location filename="../scriptingtcl.cpp" line="576"/>
+      <location filename="../scriptingtcl.cpp" line="463"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation type="unfinished">Error from Tcl&apos;s &apos;%1&apos; command: %2</translation>
+    </message>
+    <message>
+      <location filename="../scriptingtcl.cpp" line="594"/>
       <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
       <translation>Ошибка в команде Tcl &apos;%1&apos;: %2</translation>
     </message>
