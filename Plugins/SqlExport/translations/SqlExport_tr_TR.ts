31996ee30e104510623ec3251c95f9b5b00038d6
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-26 23:47:05 +0100

    New translations SqlExport.ts (Turkish)

--- Plugins/SqlExport/translations/SqlExport_tr_TR.ts
+++ Plugins/SqlExport/translations/SqlExport_tr_TR.ts
@@ -4,42 +4,42 @@
   <context>
     <name>SqlExport</name>
     <message>
-      <location filename="../sqlexport.cpp" line="57"/>
+      <location filename="../sqlexport.cpp" line="58"/>
       <source>-- Results of query:</source>
       <translation>-- Sorgu sonuçları:</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="122"/>
+      <location filename="../sqlexport.cpp" line="141"/>
       <source>-- Table: %1</source>
       <translation>-- Tablo: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="168"/>
+      <location filename="../sqlexport.cpp" line="195"/>
       <source>-- Index: %1</source>
       <translation>-- Indeks: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="185"/>
+      <location filename="../sqlexport.cpp" line="220"/>
       <source>-- Trigger: %1</source>
       <translation>-- Tetikleyici: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="202"/>
+      <location filename="../sqlexport.cpp" line="245"/>
       <source>-- View: %1</source>
       <translation>-- View: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="216"/>
+      <location filename="../sqlexport.cpp" line="265"/>
       <source>-- File generated with SQLiteStudio v%1 on %2</source>
       <translation>-- SQLiteStudio ile üretilen dosya %2 üzerinde v%1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="220"/>
+      <location filename="../sqlexport.cpp" line="269"/>
       <source>-- Text encoding used: %1</source>
       <translation>-- Kullanılan metin kodlaması: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="276"/>
+      <location filename="../sqlexport.cpp" line="339"/>
       <source>Table name for INSERT statements is mandatory.</source>
       <translation>INSERT komutlarında tablo adı zorunludur.</translation>
     </message>
@@ -47,17 +47,22 @@
   <context>
     <name>sqlExportCommonConfig</name>
     <message>
-      <location filename="../SqlExportCommon.ui" line="20"/>
+      <location filename="../SqlExportCommon.ui" line="30"/>
       <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
       <translation>&quot;CREATE&quot; komutundan önce &quot;DROP IF EXISTS&quot; oluştur</translation>
     </message>
     <message>
-      <location filename="../SqlExportCommon.ui" line="30"/>
+      <location filename="../SqlExportCommon.ui" line="40"/>
       <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
       <translation>Sadece DDL komutlarını biçimlendir(&quot;INSERT&quot; komutu hariç)</translation>
     </message>
     <message>
-      <location filename="../SqlExportCommon.ui" line="40"/>
+      <location filename="../SqlExportCommon.ui" line="50"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportCommon.ui" line="20"/>
       <source>Use SQL formatter to format exported SQL statements</source>
       <translation>Çıkartılan SQL komutları için SQL biçimlendirmesini kullan</translation>
     </message>
@@ -65,27 +70,27 @@
   <context>
     <name>sqlExportQueryConfig</name>
     <message>
-      <location filename="../SqlExportQuery.ui" line="20"/>
+      <location filename="../SqlExportQuery.ui" line="47"/>
       <source>Use SQL formatter to format exported SQL statements</source>
       <translation>Çıkartılan SQL komutları için SQL biçimlendirmesini kullan</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="30"/>
+      <location filename="../SqlExportQuery.ui" line="40"/>
       <source>Table name to use for INSERT statements:</source>
       <translation>INSERT komutları için kullanılacak tablo adı:</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="37"/>
+      <location filename="../SqlExportQuery.ui" line="64"/>
       <source>Generate &quot;CREATE TABLE&quot; statement at the begining</source>
       <translation>En başta &quot;CREATE TABLE&quot; komutlarını üret</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="47"/>
+      <location filename="../SqlExportQuery.ui" line="20"/>
       <source>Include the query in comments</source>
       <translation>Sorguyu yorumlara ekle</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="57"/>
+      <location filename="../SqlExportQuery.ui" line="30"/>
       <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
       <translation>&quot;CREATE&quot; komutundan önce &quot;DROP IF EXISTS&quot; komutunu üret</translation>
     </message>
@@ -93,6 +98,11 @@
       <location filename="../SqlExportQuery.ui" line="74"/>
       <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
       <translation>Sadece DDL komutlarını biçimlendir(&quot;INSERT&quot; komutları dışında)</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="84"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</translation>
     </message>
   </context>
 </TS>
