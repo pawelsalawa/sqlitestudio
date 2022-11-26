b952764939c1dd89b7e3ae173cc34b5571d58598
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-26 23:48:31 +0100

    New translations ScriptingPython.ts (Czech)

--- Plugins/ScriptingPython/translations/ScriptingPython_cs_CZ.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_cs_CZ.ts
@@ -4,27 +4,27 @@
   <context>
     <name>QObject</name>
     <message>
-      <location filename="../scriptingpython.cpp" line="466"/>
+      <location filename="../scriptingpython.cpp" line="484"/>
       <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
       <translation type="unfinished">Invalid use of %1 function. Expected %2 arguments, but got %3.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="475"/>
+      <location filename="../scriptingpython.cpp" line="493"/>
       <source>Unknown error from function %1.</source>
       <translation type="unfinished">Unknown error from function %1.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="516"/>
+      <location filename="../scriptingpython.cpp" line="534"/>
       <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
       <translation type="unfinished">Could not calculate string representation of the Python object passed as argument to the function %1.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="532"/>
+      <location filename="../scriptingpython.cpp" line="550"/>
       <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
       <translation type="unfinished">Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="557"/>
+      <location filename="../scriptingpython.cpp" line="575"/>
       <source>Error from Python function %1: %2</source>
       <translation type="unfinished">Error from Python function %1: %2</translation>
     </message>
