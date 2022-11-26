c781f2ce18a071b27e1ac989c07ca6dfaea01573
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-26 23:48:05 +0100

    New translations ScriptingPython.ts (Russian)

--- Plugins/ScriptingPython/translations/ScriptingPython_ru_RU.ts
+++ Plugins/ScriptingPython/translations/ScriptingPython_ru_RU.ts
@@ -4,27 +4,27 @@
   <context>
     <name>QObject</name>
     <message>
-      <location filename="../scriptingpython.cpp" line="466"/>
+      <location filename="../scriptingpython.cpp" line="484"/>
       <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
       <translation>Некорректное использование функции %1. Ожидается аргументов: %2, передано аргументов: %3.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="475"/>
+      <location filename="../scriptingpython.cpp" line="493"/>
       <source>Unknown error from function %1.</source>
       <translation>Неизвестная ошибка при выполнении функции %1.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="516"/>
+      <location filename="../scriptingpython.cpp" line="534"/>
       <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
       <translation>Невозможно сформировать строковое представление объекта Python, переданного в виде аргумента в функцию %1.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="532"/>
+      <location filename="../scriptingpython.cpp" line="550"/>
       <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
       <translation>Не найден контекст выполнения для функции %1. Это ошибка модуля Python. Пожалуйста, сообщите о ней.</translation>
     </message>
     <message>
-      <location filename="../scriptingpython.cpp" line="557"/>
+      <location filename="../scriptingpython.cpp" line="575"/>
       <source>Error from Python function %1: %2</source>
       <translation>Ошибка при выполнении функции Python %1: %2</translation>
     </message>
