17929966fe1cdd1b0ab3d96136e8f9f1a133f766
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-26 23:47:08 +0100

    New translations SqlExport.ts (Chinese Traditional)

--- Plugins/SqlExport/translations/SqlExport_zh_TW.ts
+++ Plugins/SqlExport/translations/SqlExport_zh_TW.ts
@@ -4,42 +4,42 @@
   <context>
     <name>SqlExport</name>
     <message>
-      <location filename="../sqlexport.cpp" line="57"/>
+      <location filename="../sqlexport.cpp" line="58"/>
       <source>-- Results of query:</source>
       <translation>-- 查詢的執行結果：</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="122"/>
+      <location filename="../sqlexport.cpp" line="141"/>
       <source>-- Table: %1</source>
       <translation>-- 表：%1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="168"/>
+      <location filename="../sqlexport.cpp" line="195"/>
       <source>-- Index: %1</source>
       <translation>-- 索引：%1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="185"/>
+      <location filename="../sqlexport.cpp" line="220"/>
       <source>-- Trigger: %1</source>
       <translation>-- 觸發器：%1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="202"/>
+      <location filename="../sqlexport.cpp" line="245"/>
       <source>-- View: %1</source>
       <translation>-- 檢視：%1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="216"/>
+      <location filename="../sqlexport.cpp" line="265"/>
       <source>-- File generated with SQLiteStudio v%1 on %2</source>
       <translation>-- SQLiteStudio v%1 生成的檔案，%2</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="220"/>
+      <location filename="../sqlexport.cpp" line="269"/>
       <source>-- Text encoding used: %1</source>
       <translation>-- 所用的文字編碼：%1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="276"/>
+      <location filename="../sqlexport.cpp" line="339"/>
       <source>Table name for INSERT statements is mandatory.</source>
       <translation>INSERT 語句的表名稱是強制性的。</translation>
     </message>
@@ -47,17 +47,22 @@
   <context>
     <name>sqlExportCommonConfig</name>
     <message>
-      <location filename="../SqlExportCommon.ui" line="20"/>
+      <location filename="../SqlExportCommon.ui" line="30"/>
       <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
       <translation>在 &quot;CREATE&quot; 語句前生成 &quot;DROP IF EXISTS&quot; 語句</translation>
     </message>
     <message>
-      <location filename="../SqlExportCommon.ui" line="30"/>
+      <location filename="../SqlExportCommon.ui" line="40"/>
       <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
       <translation>僅格式 DDL 語句 (排除 &quot;INSERT&quot; 語句)</translation>
     </message>
     <message>
-      <location filename="../SqlExportCommon.ui" line="40"/>
+      <location filename="../SqlExportCommon.ui" line="50"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportCommon.ui" line="20"/>
       <source>Use SQL formatter to format exported SQL statements</source>
       <translation>使用 SQL 格式化器格式化匯出的 SQL 語句</translation>
     </message>
@@ -65,27 +70,27 @@
   <context>
     <name>sqlExportQueryConfig</name>
     <message>
-      <location filename="../SqlExportQuery.ui" line="20"/>
+      <location filename="../SqlExportQuery.ui" line="47"/>
       <source>Use SQL formatter to format exported SQL statements</source>
       <translation>使用 SQL 格式化器格式化匯出的 SQL 語句</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="30"/>
+      <location filename="../SqlExportQuery.ui" line="40"/>
       <source>Table name to use for INSERT statements:</source>
       <translation>INSERT 語句的表名：</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="37"/>
+      <location filename="../SqlExportQuery.ui" line="64"/>
       <source>Generate &quot;CREATE TABLE&quot; statement at the begining</source>
       <translation>在匯出檔案開始部分生成 &quot;CREATE TABLE&quot; 語句</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="47"/>
+      <location filename="../SqlExportQuery.ui" line="20"/>
       <source>Include the query in comments</source>
       <translation>在註釋中包含查詢</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="57"/>
+      <location filename="../SqlExportQuery.ui" line="30"/>
       <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
       <translation>在 &quot;CREATE&quot; 語句前生成 &quot;DROP IF EXISTS&quot; 語句</translation>
     </message>
@@ -93,6 +98,11 @@
       <location filename="../SqlExportQuery.ui" line="74"/>
       <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
       <translation>僅格式 DDL 語句 (排除 &quot;INSERT&quot; 語句)</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="84"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</translation>
     </message>
   </context>
 </TS>
