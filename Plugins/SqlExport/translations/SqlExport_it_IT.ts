ee2a2cb53d3e4f11ff44163c0b4110b709c9ad3e
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-26 23:46:54 +0100

    New translations SqlExport.ts (Italian)

--- Plugins/SqlExport/translations/SqlExport_it_IT.ts
+++ Plugins/SqlExport/translations/SqlExport_it_IT.ts
@@ -4,42 +4,42 @@
   <context>
     <name>SqlExport</name>
     <message>
-      <location filename="../sqlexport.cpp" line="57"/>
+      <location filename="../sqlexport.cpp" line="58"/>
       <source>-- Results of query:</source>
       <translation>-- Risultati della query:</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="122"/>
+      <location filename="../sqlexport.cpp" line="141"/>
       <source>-- Table: %1</source>
       <translation>-- Tabella: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="168"/>
+      <location filename="../sqlexport.cpp" line="195"/>
       <source>-- Index: %1</source>
       <translation>-- Indice: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="185"/>
+      <location filename="../sqlexport.cpp" line="220"/>
       <source>-- Trigger: %1</source>
       <translation>-- Trigger: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="202"/>
+      <location filename="../sqlexport.cpp" line="245"/>
       <source>-- View: %1</source>
       <translation>-- Vista: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="216"/>
+      <location filename="../sqlexport.cpp" line="265"/>
       <source>-- File generated with SQLiteStudio v%1 on %2</source>
       <translation>-- File generato con SQLiteStudio v%1 su %2</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="220"/>
+      <location filename="../sqlexport.cpp" line="269"/>
       <source>-- Text encoding used: %1</source>
       <translation>-- Codifica del testo utilizzata: %1</translation>
     </message>
     <message>
-      <location filename="../sqlexport.cpp" line="276"/>
+      <location filename="../sqlexport.cpp" line="339"/>
       <source>Table name for INSERT statements is mandatory.</source>
       <translation>Il nome della tabella per le istruzioni INSERT è obbligatorio.</translation>
     </message>
@@ -47,17 +47,22 @@
   <context>
     <name>sqlExportCommonConfig</name>
     <message>
-      <location filename="../SqlExportCommon.ui" line="20"/>
+      <location filename="../SqlExportCommon.ui" line="30"/>
       <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
       <translation>Genera l&apos;istruzione &quot;DROP IF EXISTS&quot; prima dell&apos;istruzione &quot;CREATE&quot;</translation>
     </message>
     <message>
-      <location filename="../SqlExportCommon.ui" line="30"/>
+      <location filename="../SqlExportCommon.ui" line="40"/>
       <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
       <translation>Forma solo dichiarazioni DDL (escludendo dichiarazioni &quot;INSERT&quot;)</translation>
     </message>
     <message>
-      <location filename="../SqlExportCommon.ui" line="40"/>
+      <location filename="../SqlExportCommon.ui" line="50"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportCommon.ui" line="20"/>
       <source>Use SQL formatter to format exported SQL statements</source>
       <translation>Usa il formattatore SQL per formattare le istruzioni SQL esportate</translation>
     </message>
@@ -65,27 +70,27 @@
   <context>
     <name>sqlExportQueryConfig</name>
     <message>
-      <location filename="../SqlExportQuery.ui" line="20"/>
+      <location filename="../SqlExportQuery.ui" line="47"/>
       <source>Use SQL formatter to format exported SQL statements</source>
       <translation>Usa il formattatore SQL per formattare le istruzioni SQL esportate</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="30"/>
+      <location filename="../SqlExportQuery.ui" line="40"/>
       <source>Table name to use for INSERT statements:</source>
       <translation>Nome tabella da usare per le istruzioni INSERT:</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="37"/>
+      <location filename="../SqlExportQuery.ui" line="64"/>
       <source>Generate &quot;CREATE TABLE&quot; statement at the begining</source>
       <translation>Genera dichiarazione &quot;CREATE TABLE&quot; all&apos;inizio</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="47"/>
+      <location filename="../SqlExportQuery.ui" line="20"/>
       <source>Include the query in comments</source>
       <translation>Includi la query nei commenti</translation>
     </message>
     <message>
-      <location filename="../SqlExportQuery.ui" line="57"/>
+      <location filename="../SqlExportQuery.ui" line="30"/>
       <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
       <translation>Genera l&apos;istruzione &quot;DROP IF EXISTS&quot; prima dell&apos;istruzione &quot;CREATE&quot;</translation>
     </message>
@@ -93,6 +98,11 @@
       <location filename="../SqlExportQuery.ui" line="74"/>
       <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
       <translation>Forma solo dichiarazioni DDL (escludendo dichiarazioni &quot;INSERT&quot;)</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="84"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</translation>
     </message>
   </context>
 </TS>
