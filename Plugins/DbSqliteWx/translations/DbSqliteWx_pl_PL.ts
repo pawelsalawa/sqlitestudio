2d383dc7414f5f3153d7b31edf27e6cfae4e738d
Paweł Salawa <pawelsalawa@gmail.com>
2022-11-27 23:03:23 +0100

    New translations DbSqliteWx.ts (Polish)

--- Plugins/DbSqliteWx/translations/DbSqliteWx_pl_PL.ts
+++ Plugins/DbSqliteWx/translations/DbSqliteWx_pl_PL.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="pl" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteWx</name>
     <message>
-        <location filename="../dbsqlitewx.cpp" line="30"/>
-        <source>Password (key)</source>
-        <translation type="unfinished">Password (key)</translation>
+      <location filename="../dbsqlitewx.cpp" line="30"/>
+      <source>Password (key)</source>
+      <translation>Hasło (klucz)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitewx.cpp" line="31"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation type="unfinished">Leave empty to create or connect to decrypted database.</translation>
+      <location filename="../dbsqlitewx.cpp" line="31"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Pozostaw puste, aby utworzyć lub połączyć się z odszyfrowaną bazą danych.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitewx.cpp" line="32"/>
-        <source>Encryption password</source>
-        <translation type="unfinished">Encryption password</translation>
+      <location filename="../dbsqlitewx.cpp" line="32"/>
+      <source>Encryption password</source>
+      <translation>Hasło szyfrowania</translation>
     </message>
     <message>
-        <location filename="../dbsqlitewx.cpp" line="38"/>
-        <source>Cipher</source>
-        <translation type="unfinished">Cipher</translation>
+      <location filename="../dbsqlitewx.cpp" line="38"/>
+      <source>Cipher</source>
+      <translation>Szyfr</translation>
     </message>
     <message>
-        <location filename="../dbsqlitewx.cpp" line="39"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation type="unfinished">Cipher determines encryption algorithm used to encrypt the database.</translation>
+      <location filename="../dbsqlitewx.cpp" line="39"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Szyfr określa algorytm szyfrowania używany do szyfrowania bazy danych.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitewx.cpp" line="47"/>
-        <source>Cipher configuration (optional)</source>
-        <translation type="unfinished">Cipher configuration (optional)</translation>
+      <location filename="../dbsqlitewx.cpp" line="47"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Konfiguracja szyfru (opcjonalnie)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitewx.cpp" line="48"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitewx.cpp" line="48"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation type="unfinished">PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
-They will be executed upon each opening of the database.
-See documentation for SQLite3 Multiple Ciphers for details.</translation>
+      <translation>Instrukcje PRAGMA do dostosowywania konfiguracji SQLite3 Multiple Ciphers, takich jak iteracje KDF, tryb zgodności itp.
+Zostaną one wykonane po każdym otwarciu bazy danych.
+Zobacz dokumentację dla SQLite3 Multiple Ciphers, aby poznać szczegóły.</translation>
     </message>
-</context>
+  </context>
 </TS>
