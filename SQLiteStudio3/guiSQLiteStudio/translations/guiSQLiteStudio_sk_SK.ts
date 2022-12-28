d0952266c2e0a7c3a6a96555792cb1bd3dc82fdc
Paweł Salawa <pawelsalawa@gmail.com>
2022-12-28 06:46:20 +0100

    New translations guiSQLiteStudio.ts (Slovak)

--- SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_sk_SK.ts
+++ SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_sk_SK.ts
@@ -1523,95 +1523,95 @@
       <translation type="unfinished">Automatically trigger the assistant after a dot is typed after an object name</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="898"/>
+      <location filename="../dialogs/configdialog.cpp" line="899"/>
       <source>Description:</source>
       <comment>plugin details</comment>
       <translation>Popis:</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="899"/>
+      <location filename="../dialogs/configdialog.cpp" line="900"/>
       <source>Category:</source>
       <comment>plugin details</comment>
       <translation>Kategória:</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="900"/>
+      <location filename="../dialogs/configdialog.cpp" line="901"/>
       <source>Version:</source>
       <comment>plugin details</comment>
       <translation>Verzia:</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="901"/>
+      <location filename="../dialogs/configdialog.cpp" line="902"/>
       <source>Author:</source>
       <comment>plugin details</comment>
       <translation>Autor:</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="903"/>
+      <location filename="../dialogs/configdialog.cpp" line="904"/>
       <source>Internal name:</source>
       <comment>plugin details</comment>
       <translation>Interný názov:</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="904"/>
+      <location filename="../dialogs/configdialog.cpp" line="905"/>
       <source>Dependencies:</source>
       <comment>plugin details</comment>
       <translation>Závislosti:</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="905"/>
+      <location filename="../dialogs/configdialog.cpp" line="906"/>
       <source>Conflicts:</source>
       <comment>plugin details</comment>
       <translation>Konflikty:</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="909"/>
+      <location filename="../dialogs/configdialog.cpp" line="910"/>
       <source>Plugin details</source>
       <translation>Detaily pluginu</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="1445"/>
+      <location filename="../dialogs/configdialog.cpp" line="1452"/>
       <source>Plugins are loaded/unloaded immediately when checked/unchecked, but modified list of plugins to load at startup is not saved until you commit the whole configuration dialog.</source>
       <translation>Pluginy sú načítané/uvoľnené okamžite pri zaškrtnutí/odškrtnutí ale midifikovaný zoznam pluginov načítaných pri štarte nieje uložený až kým nepotvrdíte celý konfiguračný dialóg.</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="1509"/>
+      <location filename="../dialogs/configdialog.cpp" line="1516"/>
       <source>%1 (built-in)</source>
       <comment>plugins manager in configuration dialog</comment>
       <translation>%1 (interný)</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="1522"/>
+      <location filename="../dialogs/configdialog.cpp" line="1529"/>
       <source>Details</source>
       <translation>Detaily</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="1534"/>
+      <location filename="../dialogs/configdialog.cpp" line="1541"/>
       <source>No plugins in this category.</source>
       <translation type="unfinished">No plugins in this category.</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="1636"/>
+      <location filename="../dialogs/configdialog.cpp" line="1643"/>
       <source>Add new data type</source>
       <translation>Pridať nový datový typ</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="1640"/>
+      <location filename="../dialogs/configdialog.cpp" line="1647"/>
       <source>Rename selected data type</source>
       <translation>Premenovať vybraný datový typ</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="1644"/>
+      <location filename="../dialogs/configdialog.cpp" line="1651"/>
       <source>Delete selected data type</source>
       <translation>Vymazať vybraný datový typ</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="1648"/>
+      <location filename="../dialogs/configdialog.cpp" line="1655"/>
       <source>Help for configuring data type editors</source>
       <translation type="unfinished">Help for configuring data type editors</translation>
     </message>
     <message>
-      <location filename="../dialogs/configdialog.cpp" line="1777"/>
+      <location filename="../dialogs/configdialog.cpp" line="1784"/>
       <source>Here you can configure colors for code syntax highlighting.They are shared across different languages - not only for SQL, but also JavaScript and others.</source>
       <translation type="unfinished">Here you can configure colors for code syntax highlighting.They are shared across different languages - not only for SQL, but also JavaScript and others.</translation>
     </message>
@@ -3985,53 +3985,53 @@ Please enter new, unique name, or press &apos;%1&apos; to abort the operation:</
       <translation type="unfinished">&amp;Help</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="587"/>
+      <location filename="../mainwindow.cpp" line="586"/>
       <source>Could not set style: %1</source>
       <comment>main window</comment>
       <translation>Nemôžem nastaviť štýl: %1</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="718"/>
+      <location filename="../mainwindow.cpp" line="717"/>
       <source>Cannot export, because no export plugin is loaded.</source>
       <translation>Nemôžem exportovať, lebo nebol načítaný žiaden plugin na export.</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="734"/>
+      <location filename="../mainwindow.cpp" line="733"/>
       <source>Cannot import, because no import plugin is loaded.</source>
       <translation>Nemôžem importovať, lebo nebol načítaný žiaden plugin na import.</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="777"/>
+      <location filename="../mainwindow.cpp" line="776"/>
       <source>Rename window</source>
       <translation>Premenovať okno</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="777"/>
+      <location filename="../mainwindow.cpp" line="776"/>
       <source>Enter new name for the window:</source>
       <translation>Zadajte nový názov pre okno:</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="880"/>
+      <location filename="../mainwindow.cpp" line="879"/>
       <source>New updates are available. &lt;a href=&quot;%1&quot;&gt;Click here for details&lt;/a&gt;.</source>
       <translation>Nové aktualizácie sú dostupné. &lt;a href=&quot;%1&quot;&gt;Kliknite sem pre zobrazenie detailov&lt;/a&gt;.</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="888"/>
+      <location filename="../mainwindow.cpp" line="887"/>
       <source>You&apos;re running the most recent version. No updates are available.</source>
       <translation>Niesú dostupné žiadne aktualizácie. Používate aktuálnu verziu.</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="1018"/>
+      <location filename="../mainwindow.cpp" line="1017"/>
       <source>Database passed in command line parameters (%1) was already on the list under name: %2</source>
       <translation>Databáza prebratá z príkazového riadka (%1) už je v zozname pod názvom: %2</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="1025"/>
+      <location filename="../mainwindow.cpp" line="1024"/>
       <source>Database passed in command line parameters (%1) has been temporarily added to the list under name: %2</source>
       <translation>Databáza prebratá z príkazového riadka (%1) bola dočasne pridaná do zoznamu pod názvom: %2</translation>
     </message>
     <message>
-      <location filename="../mainwindow.cpp" line="1030"/>
+      <location filename="../mainwindow.cpp" line="1029"/>
       <source>Could not add database %1 to list.</source>
       <translation>Nemôžem pridať databázu %1 do zoznamu.</translation>
     </message>
