67b000d1e2c1039431a0128b6b30c3e76a5b4ddf
Paweł Salawa <pawelsalawa@gmail.com>
2023-01-03 06:37:25 +0100

    New translations guiSQLiteStudio.ts (Russian)

--- SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_ru_RU.ts
+++ SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_ru_RU.ts
@@ -3254,13 +3254,9 @@ Please enter new, unique name, or press &apos;%1&apos; to abort the operation:</
   <context>
     <name>FunctionsEditor</name>
     <message>
-      <source>Filter funtions</source>
-      <translation type="vanished">Отфильтровать функции</translation>
-    </message>
-    <message>
       <location filename="../windows/functionseditor.ui" line="81"/>
       <source>Filter functions</source>
-      <translation type="unfinished"/>
+      <translation type="unfinished">Filter functions</translation>
     </message>
     <message>
       <location filename="../windows/functionseditor.ui" line="157"/>
