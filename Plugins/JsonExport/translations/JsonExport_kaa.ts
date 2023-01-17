698ae6a42e5cc3d1cdbed1cc72ea32b8172a821e
Paweł Salawa <pawelsalawa@gmail.com>
2023-01-17 23:12:15 +0100

    New translations JsonExport.ts (Karakalpak)

---
+++ Plugins/JsonExport/translations/JsonExport_kaa.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kaa" sourcelanguage="en">
+  <context>
+    <name>JsonExportConfig</name>
+    <message>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation type="unfinished">Output format</translation>
+    </message>
+    <message>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation type="unfinished">Format document (new lines, indentation)</translation>
+    </message>
+    <message>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation type="unfinished">Compress (everything in one line)</translation>
+    </message>
+  </context>
+</TS>
