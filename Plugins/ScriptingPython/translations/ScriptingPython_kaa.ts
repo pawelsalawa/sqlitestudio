e310507dbdafcd775bbf25a216d36c91803fbace
Paweł Salawa <pawelsalawa@gmail.com>
2023-01-17 23:12:27 +0100

    New translations ScriptingPython.ts (Karakalpak)

---
+++ Plugins/ScriptingPython/translations/ScriptingPython_kaa.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kaa" sourcelanguage="en">
+  <context>
+    <name>QObject</name>
+    <message>
+      <location filename="../scriptingpython.cpp" line="484"/>
+      <source>Invalid use of %1 function. Expected %2 arguments, but got %3.</source>
+      <translation type="unfinished">Invalid use of %1 function. Expected %2 arguments, but got %3.</translation>
+    </message>
+    <message>
+      <location filename="../scriptingpython.cpp" line="493"/>
+      <source>Unknown error from function %1.</source>
+      <translation type="unfinished">Unknown error from function %1.</translation>
+    </message>
+    <message>
+      <location filename="../scriptingpython.cpp" line="534"/>
+      <source>Could not calculate string representation of the Python object passed as argument to the function %1.</source>
+      <translation type="unfinished">Could not calculate string representation of the Python object passed as argument to the function %1.</translation>
+    </message>
+    <message>
+      <location filename="../scriptingpython.cpp" line="550"/>
+      <source>Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</source>
+      <translation type="unfinished">Could not find execution context for function %1. This is a bug of Python plugin. Please report it.</translation>
+    </message>
+    <message>
+      <location filename="../scriptingpython.cpp" line="575"/>
+      <source>Error from Python function %1: %2</source>
+      <translation type="unfinished">Error from Python function %1: %2</translation>
+    </message>
+  </context>
+</TS>
