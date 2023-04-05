e4c63973f2ca80f9028895b49c952cfe30b4d92d
Paweł Salawa <pawelsalawa@gmail.com>
2023-04-05 19:43:42 +0200

    New translations sqlitestudiocli.ts (Korean)

--- SQLiteStudio3/sqlitestudiocli/translations/sqlitestudiocli_ko_KR.ts
+++ SQLiteStudio3/sqlitestudiocli/translations/sqlitestudiocli_ko_KR.ts
@@ -870,11 +870,7 @@ Expected one of: %2</translation>
     <message>
       <location filename="../main.cpp" line="114"/>
       <source>Could not open specified database for executing SQL file. You may try using -d option to find out more details.</source>
-      <translation type="unfinished"/>
-    </message>
-    <message>
-      <source>Could not open specified database for executing SQL file. You my try using -d option to find out more details.</source>
-      <translation type="obsolete">Could not open specified database for executing SQL file. You my try using -d option to find out more details.</translation>
+      <translation type="unfinished">Could not open specified database for executing SQL file. You may try using -d option to find out more details.</translation>
     </message>
   </context>
 </TS>
