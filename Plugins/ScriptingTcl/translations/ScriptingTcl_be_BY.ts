2fb660352330503b5b8d9c34907d7562a259d47b
Paweł Salawa <pawelsalawa@gmail.com>
2023-08-29 12:58:31 +0200

    New translations ScriptingTcl.ts (Belarusian)

---
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_be_BY.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be" sourcelanguage="en">
+  <context>
+    <name>ScriptingTcl</name>
+    <message>
+      <location filename="../scriptingtcl.cpp" line="429"/>
+      <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
+      <translation type="unfinished">No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</translation>
+    </message>
+    <message>
+      <location filename="../scriptingtcl.cpp" line="451"/>
+      <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
+      <translation type="unfinished">Invalid &apos;%1&apos; command syntax. Should be: %2</translation>
+    </message>
+    <message>
+      <location filename="../scriptingtcl.cpp" line="463"/>
+      <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
+      <translation type="unfinished">Error from Tcl&apos;s &apos;%1&apos; command: %2</translation>
+    </message>
+    <message>
+      <location filename="../scriptingtcl.cpp" line="594"/>
+      <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
+      <translation type="unfinished">Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</translation>
+    </message>
+  </context>
+</TS>
