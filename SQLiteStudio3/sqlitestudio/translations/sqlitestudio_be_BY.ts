df0a67cd7249f089dc2718a857e487f9d7cb71fa
Paweł Salawa <pawelsalawa@gmail.com>
2023-08-29 12:58:37 +0200

    New translations sqlitestudio.ts (Belarusian)

---
+++ SQLiteStudio3/sqlitestudio/translations/sqlitestudio_be_BY.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be" sourcelanguage="en">
+  <context>
+    <name>QObject</name>
+    <message>
+      <location filename="../main.cpp" line="49"/>
+      <source>GUI interface to SQLiteStudio, a SQLite manager.</source>
+      <translation type="unfinished">GUI interface to SQLiteStudio, a SQLite manager.</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="53"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation type="unfinished">Enables debug messages in console (accessible with F12).</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="54"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation type="unfinished">Redirects debug messages into standard output (forces debug mode).</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="55"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation type="unfinished">Redirects debug messages into given file (forces debug mode).</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="55"/>
+      <source>log file</source>
+      <translation type="unfinished">log file</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="56"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation type="unfinished">Enables Lemon parser debug messages for SQL code assistant.</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="57"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation type="unfinished">Enables debugging of every single SQL query being sent to any database.</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="58"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation type="unfinished">Limits SQL query messages to only the given &lt;database&gt;.</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="58"/>
+      <source>database</source>
+      <translation type="unfinished">database</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debugging of SQLiteStudio&apos;s query executor.</source>
+      <translation type="unfinished">Enables debugging of SQLiteStudio&apos;s query executor.</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="60"/>
+      <source>Lists plugins installed in the SQLiteStudio and quits.</source>
+      <translation type="unfinished">Lists plugins installed in the SQLiteStudio and quits.</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="61"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation type="unfinished">Points to the master configuration file. Read manual at wiki page for more details.</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="61"/>
+      <source>SQLiteStudio settings file</source>
+      <translation type="unfinished">SQLiteStudio settings file</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="72"/>
+      <source>file</source>
+      <translation type="unfinished">file</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="72"/>
+      <source>Database file to open</source>
+      <translation type="unfinished">Database file to open</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="142"/>
+      <source>Select configuration directory</source>
+      <translation type="unfinished">Select configuration directory</translation>
+    </message>
+  </context>
+</TS>
