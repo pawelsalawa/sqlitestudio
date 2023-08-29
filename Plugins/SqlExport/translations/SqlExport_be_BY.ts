4a8a040089ede4beaa67566beb66f9167c89f524
Paweł Salawa <pawelsalawa@gmail.com>
2023-08-29 12:58:32 +0200

    New translations SqlExport.ts (Belarusian)

---
+++ Plugins/SqlExport/translations/SqlExport_be_BY.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be" sourcelanguage="en">
+  <context>
+    <name>SqlExport</name>
+    <message>
+      <location filename="../sqlexport.cpp" line="58"/>
+      <source>-- Results of query:</source>
+      <translation type="unfinished">-- Results of query:</translation>
+    </message>
+    <message>
+      <location filename="../sqlexport.cpp" line="141"/>
+      <source>-- Table: %1</source>
+      <translation type="unfinished">-- Table: %1</translation>
+    </message>
+    <message>
+      <location filename="../sqlexport.cpp" line="195"/>
+      <source>-- Index: %1</source>
+      <translation type="unfinished">-- Index: %1</translation>
+    </message>
+    <message>
+      <location filename="../sqlexport.cpp" line="220"/>
+      <source>-- Trigger: %1</source>
+      <translation type="unfinished">-- Trigger: %1</translation>
+    </message>
+    <message>
+      <location filename="../sqlexport.cpp" line="245"/>
+      <source>-- View: %1</source>
+      <translation type="unfinished">-- View: %1</translation>
+    </message>
+    <message>
+      <location filename="../sqlexport.cpp" line="265"/>
+      <source>-- File generated with SQLiteStudio v%1 on %2</source>
+      <translation type="unfinished">-- File generated with SQLiteStudio v%1 on %2</translation>
+    </message>
+    <message>
+      <location filename="../sqlexport.cpp" line="269"/>
+      <source>-- Text encoding used: %1</source>
+      <translation type="unfinished">-- Text encoding used: %1</translation>
+    </message>
+    <message>
+      <location filename="../sqlexport.cpp" line="339"/>
+      <source>Table name for INSERT statements is mandatory.</source>
+      <translation type="unfinished">Table name for INSERT statements is mandatory.</translation>
+    </message>
+  </context>
+  <context>
+    <name>sqlExportCommonConfig</name>
+    <message>
+      <location filename="../SqlExportCommon.ui" line="30"/>
+      <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportCommon.ui" line="40"/>
+      <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
+      <translation type="unfinished">Format DDL statements only (excludes &quot;INSERT&quot; statements)</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportCommon.ui" line="50"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportCommon.ui" line="20"/>
+      <source>Use SQL formatter to format exported SQL statements</source>
+      <translation type="unfinished">Use SQL formatter to format exported SQL statements</translation>
+    </message>
+  </context>
+  <context>
+    <name>sqlExportQueryConfig</name>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="47"/>
+      <source>Use SQL formatter to format exported SQL statements</source>
+      <translation type="unfinished">Use SQL formatter to format exported SQL statements</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="40"/>
+      <source>Table name to use for INSERT statements:</source>
+      <translation type="unfinished">Table name to use for INSERT statements:</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="64"/>
+      <source>Generate &quot;CREATE TABLE&quot; statement at the begining</source>
+      <translation type="unfinished">Generate &quot;CREATE TABLE&quot; statement at the begining</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="20"/>
+      <source>Include the query in comments</source>
+      <translation type="unfinished">Include the query in comments</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="30"/>
+      <source>Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Generate &quot;DROP IF EXISTS&quot; statement before &quot;CREATE&quot; statement</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="74"/>
+      <source>Format DDL statements only (excludes &quot;INSERT&quot; statements)</source>
+      <translation type="unfinished">Format DDL statements only (excludes &quot;INSERT&quot; statements)</translation>
+    </message>
+    <message>
+      <location filename="../SqlExportQuery.ui" line="84"/>
+      <source>Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</source>
+      <translation type="unfinished">Add &quot;IF NOT EXISTS&quot; clause to &quot;CREATE&quot; statement</translation>
+    </message>
+  </context>
+</TS>
