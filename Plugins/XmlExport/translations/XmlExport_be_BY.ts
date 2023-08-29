bf0c002a8f9450cff61525f8697321d88632f206
Paweł Salawa <pawelsalawa@gmail.com>
2023-08-29 12:58:34 +0200

    New translations XmlExport.ts (Belarusian)

---
+++ Plugins/XmlExport/translations/XmlExport_be_BY.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be" sourcelanguage="en">
+  <context>
+    <name>XmlExport</name>
+    <message>
+      <location filename="../xmlexport.cpp" line="38"/>
+      <source>Enter the namespace to use (for example: http://my.namespace.org)</source>
+      <translation type="unfinished">Enter the namespace to use (for example: http://my.namespace.org)</translation>
+    </message>
+  </context>
+  <context>
+    <name>XmlExportConfig</name>
+    <message>
+      <location filename="../XmlExport.ui" line="20"/>
+      <source>Output format</source>
+      <translation type="unfinished">Output format</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation type="unfinished">Format document (new lines, indentation)</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation type="unfinished">Compress (everything in one line)</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="58"/>
+      <source>Special characters escaping</source>
+      <translation type="unfinished">Special characters escaping</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="64"/>
+      <source>&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</source>
+      <translation type="unfinished">&lt;p&gt;Ampersands will be used for shorter values and CDATA will be used for larger values. This applies only to values that require character escaping. Other values will be exported as they are.&lt;/p&gt;</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="67"/>
+      <source>Use CDATA and ampersands</source>
+      <translation type="unfinished">Use CDATA and ampersands</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="83"/>
+      <source>&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</source>
+      <translation type="unfinished">&lt;p&gt;Every value requiring character escepe will be enclosed in CDATA block.&lt;/p&gt;</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="86"/>
+      <source>Always use CDATA</source>
+      <translation type="unfinished">Always use CDATA</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="99"/>
+      <source>&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</source>
+      <translation type="unfinished">&lt;p&gt;Every character that require esceping will be replaced with its ampersand escape sequence. No CDATA blocks will be used.&lt;/p&gt;</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="102"/>
+      <source>Always use ampersand</source>
+      <translation type="unfinished">Always use ampersand</translation>
+    </message>
+    <message>
+      <location filename="../XmlExport.ui" line="118"/>
+      <source>Define XML namespace</source>
+      <translation type="unfinished">Define XML namespace</translation>
+    </message>
+  </context>
+</TS>
