d36509b69b26644fcbcd2a9db4331d6a4e810fd5
Paweł Salawa <pawelsalawa@gmail.com>
2023-08-29 12:58:21 +0200

    New translations CsvExport.ts (Belarusian)

---
+++ Plugins/CsvExport/translations/CsvExport_be_BY.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be" sourcelanguage="en">
+  <context>
+    <name>CsvExport</name>
+    <message>
+      <location filename="../CsvExport.ui" line="20"/>
+      <source>Column names in first row</source>
+      <translation type="unfinished">Column names in first row</translation>
+    </message>
+    <message>
+      <location filename="../CsvExport.ui" line="30"/>
+      <source>Column separator:</source>
+      <translation type="unfinished">Column separator:</translation>
+    </message>
+    <message>
+      <location filename="../CsvExport.ui" line="41"/>
+      <source>, (comma)</source>
+      <translation type="unfinished">, (comma)</translation>
+    </message>
+    <message>
+      <location filename="../CsvExport.ui" line="46"/>
+      <source>; (semicolon)</source>
+      <translation type="unfinished">; (semicolon)</translation>
+    </message>
+    <message>
+      <location filename="../CsvExport.ui" line="51"/>
+      <source>\t (tab)</source>
+      <translation type="unfinished">\t (tab)</translation>
+    </message>
+    <message>
+      <location filename="../CsvExport.ui" line="56"/>
+      <source>  (whitespace)</source>
+      <translation type="unfinished">  (whitespace)</translation>
+    </message>
+    <message>
+      <location filename="../CsvExport.ui" line="61"/>
+      <source>Custom:</source>
+      <translation type="unfinished">Custom:</translation>
+    </message>
+    <message>
+      <location filename="../CsvExport.ui" line="82"/>
+      <source>Export NULL values as:</source>
+      <translation type="unfinished">Export NULL values as:</translation>
+    </message>
+    <message>
+      <location filename="../CsvExport.ui" line="89"/>
+      <source>Empty string</source>
+      <translation type="unfinished">Empty string</translation>
+    </message>
+    <message>
+      <location filename="../csvexport.cpp" line="42"/>
+      <source>Enter the custom separator character.</source>
+      <translation type="unfinished">Enter the custom separator character.</translation>
+    </message>
+  </context>
+</TS>
