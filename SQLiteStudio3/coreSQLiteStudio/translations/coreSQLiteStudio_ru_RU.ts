3828b3c12bb3baa716608c0890af3191d460aba0
Paweł Salawa <pawelsalawa@gmail.com>
2024-01-13 16:48:14 +0100

    New translations coresqlitestudio.ts (Russian)

--- SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_ru_RU.ts
+++ SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_ru_RU.ts
@@ -764,7 +764,7 @@ Tables, indexes, triggers and views copied to database %3 will remain.</source>
   <context>
     <name>QObject</name>
     <message>
-      <location filename="../common/utils.cpp" line="1028"/>
+      <location filename="../common/utils.cpp" line="1030"/>
       <source>Could not open file &apos;%1&apos; for reading: %2</source>
       <translation>Невозможно открыть файл &apos;%1&apos; для чтения: %2</translation>
     </message>
