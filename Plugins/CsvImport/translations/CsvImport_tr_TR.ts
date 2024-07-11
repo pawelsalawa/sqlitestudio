f79818a302ed56ce35d37691364fe700f047dd64
Paweł Salawa <pawelsalawa@gmail.com>
2024-07-11 14:28:18 +0200

    New translations csvimport.ts (Turkish)

--- Plugins/CsvImport/translations/CsvImport_tr_TR.ts
+++ Plugins/CsvImport/translations/CsvImport_tr_TR.ts
@@ -54,7 +54,7 @@
     <message>
       <location filename="../CsvImportOptions.ui" line="65"/>
       <source>Column separator:</source>
-      <translation type="unfinished">Column separator:</translation>
+      <translation>Sütun ayracı:</translation>
     </message>
     <message>
       <location filename="../CsvImportOptions.ui" line="108"/>
