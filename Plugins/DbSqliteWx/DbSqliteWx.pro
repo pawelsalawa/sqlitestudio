pawelsalawa/sqlitestudio

commit e92d6dee481da80cec467c18bbc74e9d45b2febf

Fix non-x86_64 build with CLang 18+ which ignores no unknown flags anymore (#4981)

Plugins/DbSqliteWx/DbSqliteWx.pro
@@ -66,7 +66,11 @@ DEFINES += SQLITE_HAS_CODEC SQLITE_ALLOW_XTHREAD_CONNECT=1 SQLITE_THREADSAFE=1 S
     SQLITE_ENABLE_RTREE=1 \
     SQLITE_ENABLE_MATH_FUNCTIONS=1
 
-QMAKE_CFLAGS += -msse4.1 -msse4.2 -maes
+# We cannot reliably detect the target architecture, assume that host == target
+contains(QMAKE_HOST.arch, x86_64) | contains(QMAKE_HOST.arch, amd64): {
+    QMAKE_CFLAGS += -msse4.1 -msse4.2 -maes
+}
+
 QMAKE_CFLAGS_WARN_ON = -Wall -Wno-unused-parameter -Wno-sign-compare -Wno-unused-function -Wno-unused-but-set-variable \
     -Wno-parentheses -Wno-unused-variable -Wno-unknown-pragmas
 

commit a5be7f2328b55df075a1fae3a5568704355ebe32

Fix 32-bit x86 builds broken by e92d6dee

Plugins/DbSqliteWx/DbSqliteWx.pro
@@ -67,7 +67,7 @@ DEFINES += SQLITE_HAS_CODEC SQLITE_ALLOW_XTHREAD_CONNECT=1 SQLITE_THREADSAFE=1 S
     SQLITE_ENABLE_MATH_FUNCTIONS=1
 
 # We cannot reliably detect the target architecture, assume that host == target
-contains(QMAKE_HOST.arch, x86_64) | contains(QMAKE_HOST.arch, amd64): {
+contains(QMAKE_HOST.arch,x86) | contains(QMAKE_HOST.arch,amd64): {
     QMAKE_CFLAGS += -msse4.1 -msse4.2 -maes
 }
 