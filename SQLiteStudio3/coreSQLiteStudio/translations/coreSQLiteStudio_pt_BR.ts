9d420616491c3746f5898425bbd14f36067cea7b
Paweł Salawa <pawelsalawa@gmail.com>
2024-08-20 08:04:46 +0200

    New translations coresqlitestudio.ts (Portuguese, Brazilian)

--- SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_pt_BR.ts
+++ SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_pt_BR.ts
@@ -355,7 +355,7 @@ Tabelas, índices, trigger e visualizações copiadas para o banco de dados %3 p
     <message>
       <location filename="../services/impl/functionmanagerimpl.cpp" line="197"/>
       <source>Could not create scripting context, probably the plugin is not configured properly</source>
-      <translation type="unfinished"/>
+      <translation type="unfinished">Could not create scripting context, probably the plugin is not configured properly</translation>
     </message>
     <message>
       <location filename="../services/impl/functionmanagerimpl.cpp" line="290"/>
@@ -853,7 +853,7 @@ Tabelas, índices, trigger e visualizações copiadas para o banco de dados %3 p
     <message>
       <location filename="../plugins/populatescript.cpp" line="70"/>
       <source>Could not get evaluation context, probably the %1 scripting plugin is not configured properly</source>
-      <translation type="unfinished"/>
+      <translation type="unfinished">Could not get evaluation context, probably the %1 scripting plugin is not configured properly</translation>
     </message>
     <message>
       <location filename="../plugins/populatescript.cpp" line="84"/>
@@ -1105,7 +1105,7 @@ A visualização permanecerá como é.</translation>
     <message>
       <location filename="../db/abstractdb3.h" line="859"/>
       <source>Registered default collation on demand, under name: %1</source>
-      <translation type="unfinished"/>
+      <translation type="unfinished">Registered default collation on demand, under name: %1</translation>
     </message>
   </context>
   <context>
