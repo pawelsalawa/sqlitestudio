e0c2398d63ae655464b718159b840a4b73631b8b
Paweł Salawa <pawelsalawa@gmail.com>
2025-04-10 15:56:52 +0200

    New translations guisqlitestudio.ts (Turkish)

--- SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_tr_TR.ts
+++ SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_tr_TR.ts
@@ -2915,8 +2915,8 @@ Please enter new, unique name, or press &apos;%1&apos; to abort the operation:</
     </message>
     <message>
       <location filename="../dialogs/errorsconfirmdialog.ui" line="20"/>
-      <source>Following errors occured:</source>
-      <translation>Şu hatalar gerçekleşti:</translation>
+      <source>Following errors occurred:</source>
+      <translation type="unfinished">Following errors occurred:</translation>
     </message>
     <message>
       <location filename="../dialogs/errorsconfirmdialog.ui" line="34"/>
