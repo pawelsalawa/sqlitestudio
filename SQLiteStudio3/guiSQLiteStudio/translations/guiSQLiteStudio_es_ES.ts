91dedd3827226b91d4bf891eb77608e7da589d38
Paweł Salawa <pawelsalawa@gmail.com>
2025-04-10 15:55:48 +0200

    New translations guisqlitestudio.ts (Spanish)

--- SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_es_ES.ts
+++ SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_es_ES.ts
@@ -2916,8 +2916,8 @@ Please enter new, unique name, or press &apos;%1&apos; to abort the operation:</
     </message>
     <message>
       <location filename="../dialogs/errorsconfirmdialog.ui" line="20"/>
-      <source>Following errors occured:</source>
-      <translation>Se han producido los siguientes errores:</translation>
+      <source>Following errors occurred:</source>
+      <translation type="unfinished">Following errors occurred:</translation>
     </message>
     <message>
       <location filename="../dialogs/errorsconfirmdialog.ui" line="34"/>
