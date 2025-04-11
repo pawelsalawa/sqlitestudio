d0b1ac9facd098107c22d76a6cd3e23ebb428700
Paweł Salawa <pawelsalawa@gmail.com>
2025-04-11 07:51:39 +0200

    New translations coresqlitestudio.ts (Chinese Simplified)

--- SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_zh_CN.ts
+++ SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_zh_CN.ts
@@ -1024,8 +1024,8 @@ The view will remain as it is.</source>
       <location filename="../db/queryexecutor.cpp" line="344"/>
       <location filename="../db/queryexecutor.cpp" line="358"/>
       <location filename="../db/queryexecutor.cpp" line="608"/>
-      <source>An error occured while executing the count(*) query, thus data paging will be disabled. Error details from the database: %1</source>
-      <translation>执行 count(*) 查询时出错，因此将禁用数据分页。数据库错误详情：%1</translation>
+      <source>An error occurred while executing the count(*) query, thus data paging will be disabled. Error details from the database: %1</source>
+      <translation type="unfinished">An error occurred while executing the count(*) query, thus data paging will be disabled. Error details from the database: %1</translation>
     </message>
     <message>
       <location filename="../db/queryexecutor.cpp" line="527"/>
