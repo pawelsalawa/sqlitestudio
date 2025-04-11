8926e31080d03f3bdc78e0c4e4a45ea394460280
Paweł Salawa <pawelsalawa@gmail.com>
2025-04-11 07:51:36 +0200

    New translations coresqlitestudio.ts (Italian)

--- SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_it_IT.ts
+++ SQLiteStudio3/coreSQLiteStudio/translations/coreSQLiteStudio_it_IT.ts
@@ -1024,8 +1024,8 @@ La vista rimarrà così com&apos;è.</translation>
       <location filename="../db/queryexecutor.cpp" line="344"/>
       <location filename="../db/queryexecutor.cpp" line="358"/>
       <location filename="../db/queryexecutor.cpp" line="608"/>
-      <source>An error occured while executing the count(*) query, thus data paging will be disabled. Error details from the database: %1</source>
-      <translation>Si è verificato un errore durante l&apos;esecuzione della query count(*), quindi la pagina dei dati sarà disabilitata. Dettagli di errore dal database: %1</translation>
+      <source>An error occurred while executing the count(*) query, thus data paging will be disabled. Error details from the database: %1</source>
+      <translation type="unfinished">An error occurred while executing the count(*) query, thus data paging will be disabled. Error details from the database: %1</translation>
     </message>
     <message>
       <location filename="../db/queryexecutor.cpp" line="527"/>
