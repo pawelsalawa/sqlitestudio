8ad364880df851d5e98a34123a51aa637ee58047
Paweł Salawa <pawelsalawa@gmail.com>
2026-01-24 07:03:51 +0100

    New translations sqlitestudio.ts (Chinese Traditional)

--- SQLiteStudio3/sqlitestudio/translations/sqlitestudio_zh_TW.ts
+++ SQLiteStudio3/sqlitestudio/translations/sqlitestudio_zh_TW.ts
@@ -4,87 +4,92 @@
   <context>
     <name>QObject</name>
     <message>
-      <location filename="../main.cpp" line="49"/>
+      <location filename="../main.cpp" line="51"/>
       <source>GUI interface to SQLiteStudio, a SQLite manager.</source>
       <translation>SQLiteStudio 圖形介面，一款 SQLite 管理軟體。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="53"/>
+      <location filename="../main.cpp" line="56"/>
       <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
       <translation type="unfinished">Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="54"/>
+      <location filename="../main.cpp" line="57"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation type="unfinished">Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</translation>
+    </message>
+    <message>
+      <location filename="../main.cpp" line="58"/>
       <source>Enables debug messages in console (accessible with F12).</source>
       <translation>啟用控制檯中的除錯資訊 (透過 F12 訪問)。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="55"/>
+      <location filename="../main.cpp" line="59"/>
       <source>Redirects debug messages into standard output (forces debug mode).</source>
       <translation>重定向除錯資訊到標準輸出 (強制除錯模式)。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="56"/>
+      <location filename="../main.cpp" line="60"/>
       <source>Redirects debug messages into given file (forces debug mode).</source>
       <translation>重定向除錯資訊到指定檔案 (強制除錯模式)。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="56"/>
+      <location filename="../main.cpp" line="60"/>
       <source>log file</source>
       <translation>日誌檔案</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="57"/>
+      <location filename="../main.cpp" line="61"/>
       <source>Enables Lemon parser debug messages for SQL code assistant.</source>
       <translation>啟用 SQL 程式碼助理的 Lemon 解析器除錯資訊。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="58"/>
+      <location filename="../main.cpp" line="62"/>
       <source>Enables debugging of every single SQL query being sent to any database.</source>
       <translation>啟用傳送到任何資料庫的每條 SQL 查詢的除錯。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="59"/>
+      <location filename="../main.cpp" line="63"/>
       <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
       <translation>將 SQL 查詢訊息限制為指定的&lt;資料庫&gt;。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="59"/>
+      <location filename="../main.cpp" line="63"/>
       <source>database</source>
       <translation>資料庫</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="60"/>
+      <location filename="../main.cpp" line="64"/>
       <source>Enables debugging of SQLiteStudio&apos;s query executor.</source>
       <translation>啟用 SQLiteStudio 的查詢執行器的除錯。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="61"/>
+      <location filename="../main.cpp" line="65"/>
       <source>Lists plugins installed in the SQLiteStudio and quits.</source>
       <translation>列出 SQLiteStudio 中已安裝的外掛並退出。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="62"/>
+      <location filename="../main.cpp" line="55"/>
       <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
       <translation>指向主設定檔檔案。更多細節見 Wiki 頁面上的手冊。</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="62"/>
+      <location filename="../main.cpp" line="55"/>
       <source>settings file</source>
       <translation type="unfinished">settings file</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="74"/>
+      <location filename="../main.cpp" line="78"/>
       <source>file</source>
       <translation>檔案</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="74"/>
+      <location filename="../main.cpp" line="78"/>
       <source>Database file to open</source>
       <translation>要開啟的資料庫檔案</translation>
     </message>
     <message>
-      <location filename="../main.cpp" line="141"/>
+      <location filename="../main.cpp" line="151"/>
       <source>Select configuration directory</source>
       <translation>選擇設定檔目錄</translation>
     </message>
