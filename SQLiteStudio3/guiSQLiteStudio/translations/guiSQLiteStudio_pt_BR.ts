04fc6d3379d8e21132a89c1498c094bce21cab92
Paweł Salawa <pawelsalawa@gmail.com>
2026-01-29 07:03:30 +0100

    New translations guisqlitestudio.ts (Portuguese, Brazilian)

--- SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_pt_BR.ts
+++ SQLiteStudio3/guiSQLiteStudio/translations/guiSQLiteStudio_pt_BR.ts
@@ -1545,23 +1545,23 @@ Key</translation>
     <message>
       <location filename="../dialogs/configdialog.ui" line="2088"/>
       <source>Toolbars</source>
-      <translation type="unfinished"/>
+      <translation type="unfinished">Toolbars</translation>
     </message>
     <message>
       <location filename="../dialogs/configdialog.ui" line="2094"/>
       <location filename="../dialogs/configdialog.ui" line="2104"/>
       <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Expressed in percentage of default size.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-      <translation type="unfinished"/>
+      <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Expressed in percentage of default size.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
       <location filename="../dialogs/configdialog.ui" line="2097"/>
       <source>Icon size</source>
-      <translation type="unfinished"/>
+      <translation type="unfinished">Icon size</translation>
     </message>
     <message>
       <location filename="../dialogs/configdialog.ui" line="2107"/>
       <source>General.ToolBarIconSize</source>
-      <translation type="unfinished"/>
+      <translation type="unfinished">General.ToolBarIconSize</translation>
     </message>
     <message>
       <location filename="../dialogs/configdialog.ui" line="2222"/>
@@ -4171,13 +4171,13 @@ Digite um nome novo, único ou pressione &apos;%1&apos; para abortar a operaçã
       <location filename="../mainwindow.cpp" line="1012"/>
       <source>Toolbar &amp;icons</source>
       <comment>menubar</comment>
-      <translation type="unfinished"/>
+      <translation type="unfinished">Toolbar &amp;icons</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="1024"/>
       <source>Size: %1%</source>
       <comment>toolbar icons</comment>
-      <translation type="unfinished"/>
+      <translation type="unfinished">Size: %1%</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="1117"/>
