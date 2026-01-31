e463bbf413616daffeeed10da806b3d708a555f1
Paweł Salawa <pawelsalawa@gmail.com>
2026-01-31 06:59:18 +0100

    New translations multieditorimage.ts (Chinese Simplified)

--- Plugins/MultiEditorImage/translations/MultiEditorImage_zh_CN.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_zh_CN.ts
@@ -4,14 +4,6 @@
   <context>
     <name>MultiEditorImage</name>
     <message>
-      <source>Load from file</source>
-      <translation type="vanished">从文件加载</translation>
-    </message>
-    <message>
-      <source>Store in a file</source>
-      <translation type="vanished">保存为文件</translation>
-    </message>
-    <message>
       <location filename="../multieditorimage.cpp" line="27"/>
       <source>Zoom in by 25%</source>
       <translation>放大25%</translation>
@@ -25,38 +17,6 @@
       <location filename="../multieditorimage.cpp" line="29"/>
       <source>Reset zoom</source>
       <translation>重置缩放</translation>
-    </message>
-    <message>
-      <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
-      <translation type="vanished">图片 (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;所有文件 (*)</translation>
-    </message>
-    <message>
-      <source>Open image</source>
-      <translation type="vanished">打开图片</translation>
-    </message>
-    <message>
-      <source>Could not open file %1 for reading.</source>
-      <translation type="vanished">无法以读模式打开文件 %1。</translation>
-    </message>
-    <message>
-      <source>All files (*)</source>
-      <translation type="vanished">所有文件 (*)</translation>
-    </message>
-    <message>
-      <source>Save image</source>
-      <translation type="vanished">保存图片</translation>
-    </message>
-    <message>
-      <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
-      <translation type="vanished">试图以与原始格式 (%2) 不同的格式 (%1) 保存图像，但应用程序转换失败。 图像将以未改变的格式 (%3) 保存在给定名称下 (%4)</translation>
-    </message>
-    <message>
-      <source>Could not open file %1 for writting.</source>
-      <translation type="vanished">无法以写模式打开文件 %1。</translation>
-    </message>
-    <message>
-      <source>Could not write image into the file %1</source>
-      <translation type="vanished">无法将图像写入文件 %1</translation>
     </message>
   </context>
   <context>
