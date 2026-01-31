c54b6db8c870ffc43877744f4aae518feeb75772
Paweł Salawa <pawelsalawa@gmail.com>
2026-01-31 06:57:18 +0100

    New translations multieditorimage.ts (Turkish)

--- Plugins/MultiEditorImage/translations/MultiEditorImage_tr_TR.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_tr_TR.ts
@@ -4,14 +4,6 @@
   <context>
     <name>MultiEditorImage</name>
     <message>
-      <source>Load from file</source>
-      <translation type="vanished">Dosyadan yükle</translation>
-    </message>
-    <message>
-      <source>Store in a file</source>
-      <translation type="vanished">Dosyada sakla</translation>
-    </message>
-    <message>
       <location filename="../multieditorimage.cpp" line="27"/>
       <source>Zoom in by 25%</source>
       <translation>%25 yakınlaştır</translation>
@@ -25,38 +17,6 @@
       <location filename="../multieditorimage.cpp" line="29"/>
       <source>Reset zoom</source>
       <translation>Zoom Sıfırla</translation>
-    </message>
-    <message>
-      <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
-      <translation type="vanished">Resimler (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;Diğer dosyalar (*)</translation>
-    </message>
-    <message>
-      <source>Open image</source>
-      <translation type="vanished">Resmi aç</translation>
-    </message>
-    <message>
-      <source>Could not open file %1 for reading.</source>
-      <translation type="vanished">%1 dosyası okumak için açılamadı.</translation>
-    </message>
-    <message>
-      <source>All files (*)</source>
-      <translation type="vanished">Tüm Dosyalar (*)</translation>
-    </message>
-    <message>
-      <source>Save image</source>
-      <translation type="vanished">Resmi kaydet</translation>
-    </message>
-    <message>
-      <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
-      <translation type="vanished">Görüntüyü orjinalinden (%2) farklı bir formatta kaydetmeye çalıştı (%1), fakat uygulama dönüştürürken hata aldı. Değiştirilmemiş formattaki görüntü (%3) verilen isimde kaydedilecek (%4)</translation>
-    </message>
-    <message>
-      <source>Could not open file %1 for writting.</source>
-      <translation type="vanished">%1 dosyası yazmak için açılamadı.</translation>
-    </message>
-    <message>
-      <source>Could not write image into the file %1</source>
-      <translation type="vanished">%1 dosyasına resim yazılamadı</translation>
     </message>
   </context>
   <context>
