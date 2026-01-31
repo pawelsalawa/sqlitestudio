cb82d8a0edfe7c1637d2124e33febdcf673de8f4
Paweł Salawa <pawelsalawa@gmail.com>
2026-01-31 06:56:58 +0100

    New translations multieditorimage.ts (Hebrew)

--- Plugins/MultiEditorImage/translations/MultiEditorImage_he_IL.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_he_IL.ts
@@ -4,14 +4,6 @@
   <context>
     <name>MultiEditorImage</name>
     <message>
-      <source>Load from file</source>
-      <translation type="vanished">טעינה מקובץ</translation>
-    </message>
-    <message>
-      <source>Store in a file</source>
-      <translation type="vanished">אחסון בקובץ</translation>
-    </message>
-    <message>
       <location filename="../multieditorimage.cpp" line="27"/>
       <source>Zoom in by 25%</source>
       <translation>קרוב מיקוד ב 25%</translation>
@@ -25,38 +17,6 @@
       <location filename="../multieditorimage.cpp" line="29"/>
       <source>Reset zoom</source>
       <translation>איפוס מיקוד</translation>
-    </message>
-    <message>
-      <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
-      <translation type="vanished">תמונות (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);כל הקבצים (*)</translation>
-    </message>
-    <message>
-      <source>Open image</source>
-      <translation type="vanished">פתיחת תמונה</translation>
-    </message>
-    <message>
-      <source>Could not open file %1 for reading.</source>
-      <translation type="vanished">לא ניתן לפתוח קובץ%1 לקריאה.</translation>
-    </message>
-    <message>
-      <source>All files (*)</source>
-      <translation type="vanished">כל הקבצים (*)</translation>
-    </message>
-    <message>
-      <source>Save image</source>
-      <translation type="vanished">שמירת תמונה</translation>
-    </message>
-    <message>
-      <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
-      <translation type="vanished">נעשה ניסה לשמירת תמונה בתבנית (%1) השונה מהמקור (%2), אך היישום לא הצליח להמיר אותה. התמונה ללא שינוי בתבנית (%3) תישמר בשם (%4)</translation>
-    </message>
-    <message>
-      <source>Could not open file %1 for writting.</source>
-      <translation type="vanished">לא ניתן לפתוח קובץ%1 לכתיבה.</translation>
-    </message>
-    <message>
-      <source>Could not write image into the file %1</source>
-      <translation type="vanished">לא ניתן לכתוב תמונה לתוך קובץ %1</translation>
     </message>
   </context>
   <context>
