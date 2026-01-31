3edb88edba877ce55bbc6608ccab30efae2794ee
Paweł Salawa <pawelsalawa@gmail.com>
2026-01-31 06:58:46 +0100

    New translations multieditorimage.ts (Italian)

--- Plugins/MultiEditorImage/translations/MultiEditorImage_it_IT.ts
+++ Plugins/MultiEditorImage/translations/MultiEditorImage_it_IT.ts
@@ -4,14 +4,6 @@
   <context>
     <name>MultiEditorImage</name>
     <message>
-      <source>Load from file</source>
-      <translation type="vanished">Carica da file</translation>
-    </message>
-    <message>
-      <source>Store in a file</source>
-      <translation type="vanished">Memorizza in un file</translation>
-    </message>
-    <message>
       <location filename="../multieditorimage.cpp" line="27"/>
       <source>Zoom in by 25%</source>
       <translation>Aumenta ingrandimento del 25%</translation>
@@ -25,38 +17,6 @@
       <location filename="../multieditorimage.cpp" line="29"/>
       <source>Reset zoom</source>
       <translation>Reimposta zoom</translation>
-    </message>
-    <message>
-      <source>Images (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;All files (*)</source>
-      <translation type="vanished">Immagini (*.jpeg *.jpg *.png *.bmp *.gif *.tiff *.jp2 *.svg *.tga *.icns *.webp *.wbmp *.mng);;Tutti i file (*)</translation>
-    </message>
-    <message>
-      <source>Open image</source>
-      <translation type="vanished">Apri immagine</translation>
-    </message>
-    <message>
-      <source>Could not open file %1 for reading.</source>
-      <translation type="vanished">Impossibile aprire il file %1 in lettura.</translation>
-    </message>
-    <message>
-      <source>All files (*)</source>
-      <translation type="vanished">Tutti i file (*)</translation>
-    </message>
-    <message>
-      <source>Save image</source>
-      <translation type="vanished">Salva immagine</translation>
-    </message>
-    <message>
-      <source>Tried to save image under different format (%1) than original (%2), but application failed to convert it. The image with unchanged format (%3) will be saved under the given name (%4)</source>
-      <translation type="vanished">Ho cercato di salvare l&apos;immagine in un formato diverso (%1) rispetto all&apos;originale (%2), ma l&apos;applicazione non è riuscita a convertirla. L&apos;immagine con formato immutato (%3) verrà salvata con il nome specificato (%4)</translation>
-    </message>
-    <message>
-      <source>Could not open file %1 for writting.</source>
-      <translation type="vanished">Impossibile aprire il file %1 in scrittura.</translation>
-    </message>
-    <message>
-      <source>Could not write image into the file %1</source>
-      <translation type="vanished">Impossibile scrivere l&apos;immagine nel file %1</translation>
     </message>
   </context>
   <context>
