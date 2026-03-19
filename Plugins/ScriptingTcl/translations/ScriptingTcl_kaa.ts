ae2129d25ad586507711003b98618aa87041d07c
Paweł Salawa <pawelsalawa@gmail.com>
2026-03-19 03:02:37 +0100

    New translations scriptingtcl.ts (Karakalpak)

--- Plugins/ScriptingTcl/translations/ScriptingTcl_kaa.ts
+++ Plugins/ScriptingTcl/translations/ScriptingTcl_kaa.ts
@@ -4,22 +4,22 @@
   <context>
     <name>ScriptingTcl</name>
     <message>
-      <location filename="../scriptingtcl.cpp" line="429"/>
+      <location filename="../scriptingtcl.cpp" line="428"/>
       <source>No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</source>
       <translation type="unfinished">No database available in current context, while called Tcl&apos;s &apos;%1&apos; command.</translation>
     </message>
     <message>
-      <location filename="../scriptingtcl.cpp" line="451"/>
+      <location filename="../scriptingtcl.cpp" line="450"/>
       <source>Invalid &apos;%1&apos; command syntax. Should be: %2</source>
       <translation type="unfinished">Invalid &apos;%1&apos; command syntax. Should be: %2</translation>
     </message>
     <message>
-      <location filename="../scriptingtcl.cpp" line="463"/>
+      <location filename="../scriptingtcl.cpp" line="462"/>
       <source>Error from Tcl&apos;s &apos;%1&apos; command: %2</source>
       <translation type="unfinished">Error from Tcl&apos;s &apos;%1&apos; command: %2</translation>
     </message>
     <message>
-      <location filename="../scriptingtcl.cpp" line="594"/>
+      <location filename="../scriptingtcl.cpp" line="593"/>
       <source>Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</source>
       <translation type="unfinished">Error from Tcl&apos;s&apos; &apos;%1&apos; command: %2</translation>
     </message>
